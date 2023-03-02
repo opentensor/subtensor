ebca60c1d5543b0449b623c2547ce8a63e80cb74
Ala Shaabana <ala@bittensor.com>
2023-03-02 14:36:47 -0500

    Fixed conflicts, added single validator for testing

--- node/src/command.rs
+++ node/src/command.rs
@@ -39,6 +39,7 @@ impl SubstrateCli for Cli {
 		Ok(match id {
 			"dev" => Box::new(chain_spec::development_config()?),
 			"" | "local" => Box::new(chain_spec::local_testnet_config()?),
+			"finney" =>Box::new(chain_spec::finney_config()?),
 			path =>
 				Box::new(chain_spec::ChainSpec::from_json_file(std::path::PathBuf::from(path))?),
 		})
