2807f9c36b5d3403e4226bf82d2f51aae5ca467a
Rubberbandits <dalegribble@riseup.net>
2023-03-06 15:07:31 -0500

    Use finney in chain command for nakamoto genesis

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -140,8 +140,67 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 	))
 }
 
+// Includes for nakamoto genesis
+use std::{fs::File, path::PathBuf};
+use serde::{Deserialize};
+use serde_json as json;
+
+// Configure storage from nakamoto data
+#[derive(Deserialize, Debug)]
+struct ColdkeyHotkeys {
+	stakes: std::collections::HashMap<String, std::collections::HashMap<String, u64>>,
+	balances: std::collections::HashMap<String, u64>
+}
+
 pub fn finney_config() -> Result<ChainSpec, String> {
+	let path: PathBuf = std::path::PathBuf::from("nakamoto_gen.json");
 	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
+
+	// We mmap the file into memory first, as this is *a lot* faster than using
+	// `serde_json::from_reader`. See https://github.com/serde-rs/json/issues/160
+	let file = File::open(&path)
+		.map_err(|e| format!("Error opening genesis file `{}`: {}", path.display(), e))?;
+
+	// SAFETY: `mmap` is fundamentally unsafe since technically the file can change
+	//         underneath us while it is mapped; in practice it's unlikely to be a problem
+	let bytes = unsafe {
+		memmap2::Mmap::map(&file)
+			.map_err(|e| format!("Error mmaping genesis file `{}`: {}", path.display(), e))?
+	};
+
+	let old_state: ColdkeyHotkeys =
+		json::from_slice(&bytes).map_err(|e| format!("Error parsing genesis file: {}", e))?;
+
+	let mut processed_stakes: Vec<(sp_runtime::AccountId32, Vec<(sp_runtime::AccountId32, u64)>)> = Vec::new();
+	for (coldkey_str, hotkeys) in old_state.stakes.iter() {
+		let coldkey = <sr25519::Public as Ss58Codec>::from_ss58check(&coldkey_str).unwrap();
+		let coldkey_account = sp_runtime::AccountId32::from(coldkey);
+
+		let mut processed_hotkeys: Vec<(sp_runtime::AccountId32, u64)> = Vec::new();
+
+		for (hotkey_str, amount) in hotkeys.iter() {
+			let hotkey = <sr25519::Public as Ss58Codec>::from_ss58check(&hotkey_str).unwrap();
+			let hotkey_account = sp_runtime::AccountId32::from(hotkey);
+
+			processed_hotkeys.push((hotkey_account, *amount));
+		}
+
+		processed_stakes.push((coldkey_account, processed_hotkeys));
+	}
+
+	let mut processed_balances: Vec<(sp_runtime::AccountId32, u64)> = Vec::new();
+	for (key_str, amount) in old_state.balances.iter() {
+		let key = <sr25519::Public as Ss58Codec>::from_ss58check(&key_str).unwrap();
+		let key_account = sp_runtime::AccountId32::from(key);
+
+		/*
+		if *amount < 1_000_000 {
+			continue
+		}
+		*/
+			
+		processed_balances.push((key_account, *amount))
+	}
 
 	Ok(ChainSpec::from_genesis(
 		// Name
@@ -150,7 +209,7 @@ pub fn finney_config() -> Result<ChainSpec, String> {
 		"Finney",
 		ChainType::Development,
 		move || {
-			testnet_genesis(
+			finney_genesis(
 				wasm_binary,
 				// Initial PoA authorities (Validators)
 				// aura | grandpa
@@ -182,6 +241,8 @@ pub fn finney_config() -> Result<ChainSpec, String> {
 				vec![
 				],
 				true,
+				processed_stakes.clone(),
+				processed_balances.clone()
 			)
 		},
 		// Bootnodes
@@ -235,8 +296,11 @@ fn finney_genesis(
 	wasm_binary: &[u8],
 	initial_authorities: Vec<(AuraId, GrandpaId)>,
 	root_key: AccountId,
-	endowed_accounts: Vec<AccountId>,
+	_endowed_accounts: Vec<AccountId>,
 	_enable_println: bool,
+	stakes: Vec<(AccountId, Vec<(AccountId, u64)>)>,
+	balances: Vec<(AccountId, u64)>
+
 ) -> GenesisConfig {
 	GenesisConfig {
 		system: SystemConfig {
@@ -245,153 +309,6 @@ fn finney_genesis(
 		},
 		balances: BalancesConfig {
 			// Configure endowed accounts with initial balance of 1 << 60.
-			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 60)).collect(),
-		},
-		aura: AuraConfig {
-			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
-		},
-		grandpa: GrandpaConfig {
-			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
-		},
-		sudo: SudoConfig {
-			// Assign network admin rights.
-			key: Some(root_key),
-		},
-		transaction_payment: Default::default(),
-		subtensor_module: Default::default(),
-	}
-}
-
-// Includes for nakamoto genesis
-use std::{fs::File, path::PathBuf};
-use serde::{Deserialize};
-use serde_json as json;
-
-// Configure storage from nakamoto data
-#[derive(Deserialize, Debug)]
-struct ColdkeyHotkeys {
-	stakes: std::collections::HashMap<String, std::collections::HashMap<String, u64>>,
-	balances: std::collections::HashMap<String, u64>
-}
-
-pub fn nakamoto_config(path: PathBuf) -> Result<ChainSpec, String> {
-	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
-
-		// We mmap the file into memory first, as this is *a lot* faster than using
-	// `serde_json::from_reader`. See https://github.com/serde-rs/json/issues/160
-	let file = File::open(&path)
-		.map_err(|e| format!("Error opening genesis file `{}`: {}", path.display(), e))?;
-
-	// SAFETY: `mmap` is fundamentally unsafe since technically the file can change
-	//         underneath us while it is mapped; in practice it's unlikely to be a problem
-	let bytes = unsafe {
-		memmap2::Mmap::map(&file)
-			.map_err(|e| format!("Error mmaping genesis file `{}`: {}", path.display(), e))?
-	};
-
-	let old_state: ColdkeyHotkeys =
-		json::from_slice(&bytes).map_err(|e| format!("Error parsing genesis file: {}", e))?;
-
-	let mut processed_stakes: Vec<(sp_runtime::AccountId32, Vec<(sp_runtime::AccountId32, u64)>)> = Vec::new();
-	for (coldkey_str, hotkeys) in old_state.stakes.iter() {
-		let coldkey = <sr25519::Public as Ss58Codec>::from_ss58check(&coldkey_str).unwrap();
-		let coldkey_account = sp_runtime::AccountId32::from(coldkey);
-
-		let mut processed_hotkeys: Vec<(sp_runtime::AccountId32, u64)> = Vec::new();
-
-		for (hotkey_str, amount) in hotkeys.iter() {
-			let hotkey = <sr25519::Public as Ss58Codec>::from_ss58check(&hotkey_str).unwrap();
-			let hotkey_account = sp_runtime::AccountId32::from(hotkey);
-
-			processed_hotkeys.push((hotkey_account, *amount));
-		}
-
-		processed_stakes.push((coldkey_account, processed_hotkeys));
-	}
-
-	let mut processed_balances: Vec<(sp_runtime::AccountId32, u64)> = Vec::new();
-	for (key_str, amount) in old_state.balances.iter() {
-		let key = <sr25519::Public as Ss58Codec>::from_ss58check(&key_str).unwrap();
-		let key_account = sp_runtime::AccountId32::from(key);
-
-		/*
-		if *amount < 1_000_000 {
-			continue
-		}
-		*/
-			
-		processed_balances.push((key_account, *amount))
-	}
-
-	Ok(ChainSpec::from_genesis(
-		// Name
-		"Finney",
-		// ID
-		"Finney",
-		ChainType::Development,
-		move || {
-			nakamoto_genesis(
-				wasm_binary,
-				// Initial PoA authorities (Validators)
-				// aura | grandpa
-				vec![
-					authority_keys_from_ss58("5EJUcFbe74FDQwPsZDbRVpdDxVZQQxjoGZA9ayJqJTbcRrGf", "5GRcfchgXZjkCfqgNvfjicjJw3vVGF4Ahqon2w8RfjXwyzy4"),// key 1
-					/*authority_keys_from_ss58("5H5oVSbQxDSw1TohAvLvp9CTAua6PN4yHme19UrG4c1ojS8J", "5FAEYaHLZmLRX4XFs2SBHbLhkysbSPrcTp51w6sQNaYLa7Tu"), // key 2
-					authority_keys_from_ss58("5CfBazEwCAsmscGj1J9rhXess9ZXZ5qYcuZvFWii9sxT977v", "5F6LgDAenzchE5tPmFHKGueYy1rj85oB2yxvm1xyKLVvk4gy"), // key 3
-					authority_keys_from_ss58("5HZDvVFWH3ifx1Sx8Uaaa7oiT6U4fAKrR3LKy9r1zFnptc1z", "5GJY6A1X8KNvqHcf42Cpr5HZzG95FZVJkTHJvnHSBGgshEWn"), // key 4
-					authority_keys_from_ss58("5H3v2VfQmsAAgj63EDaB1ZWmruTHHkJ4kci5wkt6SwMi2VW1", "5FXVk1gEsNweTB6AvS5jAWCivXQHTcyCWXs21wHvRU5UTZtb"), // key 5
-					authority_keys_from_ss58("5CPhKdvHmMqRmMUrpFnvLc6GUcduVwpNHsPPEhnYQ7QXjPdz", "5GAzG6PhVvpeoZVkKupa2uZDrhwsUmk5fCHgwq95cN9s3Dvi"), // key 6
-					authority_keys_from_ss58("5DZTjVhqVjHyhXLhommE4jqY9w1hJEKNQWJ8p6QnUWghRYS1", "5HmGN73kkcHaKNJrSPAxwiwAiiCkztDZ1AYi4gkpv6jaWaxi"), // key 7
-					authority_keys_from_ss58("5ETyBUhi3uVCzsk4gyTmtf41nheH7wALqQQxbUkmRPNqEMGS", "5Cq63ca5KM5qScJYmQi7PvFPhJ6Cxr6yw6Xg9dLYoRYg33rN"), // key 8
-					authority_keys_from_ss58("5DUSt6KiZWxA3tsiFkv3xYSNuox6PCfhyvqqM9x7N5kuHV2S", "5FF1kun4rb5B7C3tqh23XPVDDUJ3UchnaXxJeXu1i5n8KNHp"), // key 9
-					authority_keys_from_ss58("5GgsDz9yixsdHxFu52SN37f6TrUtU2RwmGJejbHVmN1ERXL4", "5EZiep2gMyV2cz9x54TQDb1cuyFYYcwGRGZ7J19Ua4YSAWCZ"), // key 10
-					authority_keys_from_ss58("5HjhkCMa89QJbFULs8WPZBgVg8kMq5qdX1nx7CnQpZgoyKAN", "5D5DL9sru2ep3AWoHvmEUbFLirVr7tJ6BxBWH5M8j3r9kUpe"), // key 11
-					authority_keys_from_ss58("5F257gHitacwDGvYm2Xm7dBE882auTU8wraG6w4T3r63wh9V", "5CovRCaioWENKejfaeccDQY4vCF8kTGtZ5fwagSCeDGmiSyh"), // key 12
-					authority_keys_from_ss58("5CtGLbiHWs6XVgNi9nW7oqSP4D4JMot7yHYuFokidZzAP6ny", "5DSxsR9aAiq33uSYXWt4zEibx6KT6xxtFGkT9S4GLaCavgDE"), // key 13
-					authority_keys_from_ss58("5DeVtxyiniPzoHo4iQiLhGfhED6RP3V73B5nGSYWr5Mgt82c", "5HaWL2AvLZHwyPXofWFTEZ6jHVmUG8U9cFATggKZonN1xZjm"), // key 14
-					authority_keys_from_ss58("5GF4a6pQ8TQuPhdkKqugzrZSW7YnpQtB4ihouKGZsVMwoTn6", "5DaEhFN8bWjvhDxavSWFBr962qoTAMB4b51QebdRZ75VA4h2"), // key 15
-					authority_keys_from_ss58("5DAC8Did2NgeVfZeNmEfZuU6t7UseJNf9J68XTvhLf5yCsBZ", "5G27pyXx9ieSRCTuDoqPgTvpCynH6yhum9HiQQ1iMj3rAeaP"), // key 16
-					authority_keys_from_ss58("5FmxaYznqMqiorPHQgKoRQgEHN7ud4yKsJWr6FvXuS6FS6be", "5Ch5XFMKETDiiPiuhUj9TumUtgsnVG1VzQRvBykP9bRdt4km"), // key 17
-					authority_keys_from_ss58("5GNAkfKYmFbVRAYm1tPr1yG6bHCapaY7WKRmzkEdendDXj1j", "5EC6JjwnE11qaRnjKM85eevQFV1EoaKPPtcBRmTp1XsR7Kx3"), // key 18
-					authority_keys_from_ss58("5GYk3B38R9F2TEcWoqCLojqPwx6AA1TsD3EovoTgggyRdzki", "5FjdhdAxujZVev6HYqQcTB6UBAKfKFKPoftgMLenoxbNWoe2"), // key 19
-					authority_keys_from_ss58("5D7fthS7zBDhwi2u2JYd74t7FpQuseDkUkTuaLZoenXNpXPK", "5DhAKQ4MFg39mQAYzndzbznLGqSV4VMUJUyRXe8QPDqD5G1D"), // key 20*/
-					], 
-				// Sudo account
-				Ss58Codec::from_ss58check("5G3rrAtAsZiZsRKhi9y7yckwhB6HL8AwD8K3J28YNX4n2tMZ").unwrap(), 
-				true,
-				processed_stakes.clone(),
-				processed_balances.clone(),
-			)
-		},
-		// Bootnodes
-		vec![],
-		// Telemetry
-		None,
-		// Protocol ID
-		None,
-		None,
-		// Properties
-		None,
-		// Extensions
-		None,
-	))
-}
-
-fn nakamoto_genesis(
-	wasm_binary: &[u8],
-	initial_authorities: Vec<(AuraId, GrandpaId)>,
-	root_key: AccountId,
-	_enable_println: bool,
-	stakes: Vec<(AccountId, Vec<(AccountId, u64)>)>,
-	balances: Vec<(AccountId, u64)>
-) -> GenesisConfig {
-	GenesisConfig {
-		system: SystemConfig {
-			// Add Wasm runtime to storage.
-			code: wasm_binary.to_vec(),
-		},
-		balances: BalancesConfig {
-			// Load balances from nakamoto data
 			balances: balances.iter().cloned().map(|k| k).collect(),
 		},
 		aura: AuraConfig {
