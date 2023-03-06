74abd3d0a0e6ea50cfeaf1120991901ea5cdd382
Ala Shaabana <ala@bittensor.com>
2023-03-06 12:39:30 -0500

    Revert "Fixed block time and endowed sudo with some tao to perform transactions"
    This reverts commit 794c382a5aa8bdb9afdcc80e96a759563ab0ab87.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -112,12 +112,12 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	state_version: 1,
 };
 
-// This determines the average expected block time that we are targeting.
-// Blocks will be produced at a minimum duration defined by `SLOT_DURATION`.
-// `SLOT_DURATION` is picked up by `pallet_timestamp` which is in turn picked
-// up by `pallet_aura` to implement `fn slot_duration()`.
-//
-// Change this to adjust the block time.
+/// This determines the average expected block time that we are targeting.
+/// Blocks will be produced at a minimum duration defined by `SLOT_DURATION`.
+/// `SLOT_DURATION` is picked up by `pallet_timestamp` which is in turn picked
+/// up by `pallet_aura` to implement `fn slot_duration()`.
+///
+/// Change this to adjust the block time.
 pub const MILLISECS_PER_BLOCK: u64 = 12000;
 
 // NOTE: Currently it is not possible to change the slot duration after the chain has started.
