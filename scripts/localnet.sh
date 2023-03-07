8b9a6d538059db9ae7401017b98ca0600ad22457
S0AndS0 <strangerthanbland@gmail.com>
2023-03-07 14:34:59 -0800

    Use Bash lists/arrays for more separator control

--- scripts/localnet.sh
+++ scripts/localnet.sh
@@ -10,7 +10,28 @@ echo "*** Purging previous state..."
 ./target/debug/node-subtensor purge-chain -y --base-path /tmp/alice --chain=$CHAIN.json 2>&1 > /dev/null
 
 echo "*** Starting localnet nodes..."
-alice_start="./target/debug/node-subtensor --base-path /tmp/alice --chain=$CHAIN.json --alice --port 30334 --ws-port 9946 --rpc-port 9934 --validator --rpc-cors=all"
-bob_start="./target/debug/node-subtensor --base-path /tmp/bob --chain=$CHAIN.json --bob --port 30335 --ws-port 9947 --rpc-port 9935 --validator --bootnodes /ip4/127.0.0.1/tcp/30334/p2p/12D3KooWBBUaVWE5SYj3UvnoXojfS8fvPorw5biRDaDQV7XXwCXm"
+alice_start=(
+	./target/debug/node-subtensor
+	--base-path /tmp/alice
+	--chain="$CHAIN.json"
+	--alice
+	--port 30334
+	--ws-port 9946
+	--rpc-port 9934
+	--validator
+	--rpc-cors=all
+)
 
-(trap 'kill 0' SIGINT; ($alice_start 2>&1) & ($bob_start 2>&1))
+bob_start=(
+	./target/debug/node-subtensor
+	--base-path /tmp/bob
+	--chain="$CHAIN.json"
+	--bob
+	--port 30335
+	--ws-port 9947
+	--rpc-port 9935
+	--validator
+	--bootnodes "/ip4/127.0.0.1/tcp/30334/p2p/12D3KooWBBUaVWE5SYj3UvnoXojfS8fvPorw5biRDaDQV7XXwCXm"
+)
+
+(trap 'kill 0' SIGINT; ("${alice_start[@]}" 2>&1) & ("${bob_start[@]}" 2>&1))
