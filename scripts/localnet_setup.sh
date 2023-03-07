0db4d25e19c19622e2e432e47b816ce45f70b40e
S0AndS0 <strangerthanbland@gmail.com>
2023-03-07 14:26:36 -0800

    Use variables for repeated/changing values

--- scripts/localnet_setup.sh
+++ scripts/localnet_setup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+_script_name='support_install.sh'
+_valid_hash='7296b9d45a89e973528c3ae31719ff08'
+
 echo "*** Local testnet installation"
 echo "*** Installing substrate support libraries"
 
 # Install support libraries for compiling substrate binaries
 # verify md5
-curl https://getsubstrate.io -sSf > support_install.sh
-if ! md5sum --status --check <<< "7296b9d45a89e973528c3ae31719ff08 support_install.sh"; then
+curl https://getsubstrate.io -sSf > "${_script_name:?Undfined script name}"
+if ! md5sum --status --check <<< "${_valid_hash:?Undfined hash} ${_script_name}"; then
 	_status="${?}"
 	printf >&2 "Substrate library script checksum not valid, exiting.\n"
 	exit "${_status}"
 fi
-chmod +rx support_install.sh
-bash support_install.sh
-rm support_install.sh
+chmod +rx "${_script_name}"
+bash "${_script_name}"
+rm "${_script_name}"
 
 echo "*** Building node binary..."
 
