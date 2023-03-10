c084af75fa2e1690b29d28baf96358bee96c86bf
S0AndS0 <strangerthanbland@gmail.com>
2023-03-10 15:14:23 -0800

    Track some chain spec tests

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -15,7 +15,7 @@ use sp_core::crypto::Ss58Codec;
 // Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
 
-// Generate a crypto pair from seed.
+/// Generate a crypto pair from seed.
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
 	TPublic::Pair::from_string(&format!("//{}", seed), None)
 		.expect("static values are valid; qed")
