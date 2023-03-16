3ad985be7112f15394b06bf0ddf63760feae8545
Cameron Fairchild <cameron@opentensor.ai>
2023-03-16 19:24:06 -0400

    use compact ints for custom rpc

--- pallets/subtensor/src/delegate_info.rs
+++ pallets/subtensor/src/delegate_info.rs
@@ -5,13 +5,14 @@ use frame_support::pallet_prelude::{Decode, Encode};
 extern crate alloc;
 use alloc::vec::Vec;
 use sp_core::hexdisplay::AsBytesRef;
+use codec::Compact;
 
 
 #[derive(Decode, Encode, PartialEq, Eq, Clone, Debug)]
 pub struct DelegateInfo<T: Config> {
     delegate_ss58: T::AccountId,
-    take: u16,
-    nominators: Vec<(T::AccountId, u64)>, // map of nominator_ss58 to stake amount
+    take: Compact<u16>,
+    nominators: Vec<(T::AccountId, Compact<u64>)>, // map of nominator_ss58 to stake amount
     owner_ss58: T::AccountId
 }
 
@@ -22,18 +23,22 @@ impl<T: Config> Pallet<T> {
         }
 
         let delegate: AccountIdOf<T> = T::AccountId::decode( &mut delegate_account_vec.as_bytes_ref() ).unwrap();
+        // Check delegate exists
+        if !<Delegates<T>>::contains_key( delegate.clone() ) {
+            return None;
+        }
 
-        let mut nominators = Vec::<(T::AccountId, u64)>::new();
+        let mut nominators = Vec::<(T::AccountId, Compact<u64>)>::new();
 
         for ( nominator, stake ) in < Stake<T> as IterableStorageDoubleMap<T::AccountId, T::AccountId, u64> >::iter_prefix( delegate.clone() ) {
-            nominators.push( ( nominator.clone(), stake ) );
+            nominators.push( ( nominator.clone(), stake.into() ) );
         }
 
         let owner = <Owner<T>>::get( delegate.clone() );
 
         return Some( DelegateInfo {
             delegate_ss58: delegate.clone(),
-            take: <Delegates<T>>::get( delegate.clone() ),
+            take: <Delegates<T>>::get( delegate.clone() ).into(),
             nominators,
             owner_ss58: owner.clone()
         });
@@ -42,16 +47,16 @@ impl<T: Config> Pallet<T> {
     pub fn get_delegates() -> Vec<DelegateInfo<T>> {
         let mut delegates = Vec::<DelegateInfo<T>>::new();
         for ( delegate, take ) in < Delegates<T> as IterableStorageMap<T::AccountId, u16> >::iter() {
-            let mut nominators = Vec::<(T::AccountId, u64)>::new();
+            let mut nominators = Vec::<(T::AccountId, Compact<u64>)>::new();
             for ( nominator, stake ) in < Stake<T> as IterableStorageDoubleMap<T::AccountId, T::AccountId, u64> >::iter_prefix( delegate.clone() ) {
-                nominators.push( ( nominator.clone(), stake ) );
+                nominators.push( ( nominator.clone(), stake.into() ) );
             }
 
             let owner = <Owner<T>>::get( delegate.clone() );
 
             delegates.push( DelegateInfo {
                 delegate_ss58: delegate.clone(),
-                take,
+                take: take.into(),
                 nominators,
                 owner_ss58: owner.clone()
             });
