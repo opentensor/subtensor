45df60f5d5915e245d814301ebb48e83cfab1d96
opentaco <opentaco@protonmail.com>
2023-03-20 17:13:04 +0200

    Update initial hyperparameters for launch

--- hyperparameters.md
+++ hyperparameters.md
@@ -2,11 +2,11 @@
 ```rust
 Rho: u16 = 10;
 Kappa: u16 = 32_767; // 0.5 = 65535/2 
-MaxAllowedUids: u16 = 512;
+MaxAllowedUids: u16 = 4096;
 Issuance: u64 = 0;
-MinAllowedWeights: u16 = 1024;
+MinAllowedWeights: u16 = 128;
 EmissionValue: u16 = 0;
-MaxWeightsLimit: u16 = 262;
+MaxWeightsLimit: u16 = 1000; // 1000/2^16 = 0.015
 ValidatorBatchSize: u16 = 10; // 32
 ValidatorSequenceLen: u16 = 10; // 256
 ValidatorEpochLen: u16 = 100;
@@ -16,21 +16,21 @@ ValidatorPruneLen: u64 = 1;
 ValidatorLogitsDivergence: u16 = 1310; // 2% of u16
 ScalingLawPower: u16 = 50; // 0.5
 SynergyScalingLawPower: u16 = 50; // 0.5
-MaxAllowedValidators: u16 = 100;
-Tempo: u16 = 0;
+MaxAllowedValidators: u16 = 128;
+Tempo: u16 = 99;
 Difficulty: u64 = 1;
 AdjustmentInterval: u16 = 100;
 TargetRegistrationsPerInterval: u16 = 2;
 ImmunityPeriod: u16 = 4096;
 ActivityCutoff: u16 = 5000;
-MaxRegistrationsPerBlock: u16 = 50;
+MaxRegistrationsPerBlock: u16 = 0;
 PruningScore : u16 = u16::MAX;
 BondsMovingAverage: u64 = 900_000;
 DefaultTake: u16 = 11_796; // 18% honest number.
 WeightsVersionKey: u64 = 0;
 MinDifficulty: u64 = 1;
 MaxDifficulty: u64 = 10;
-ServingRateLimit: u64 = 1000; 
+ServingRateLimit: u64 = 50; 
 Burn: u64 = 0; 
 MinBurn: u64 = 0; 
 MaxBurn: u64 = 1_000_000_000;
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -317,11 +317,11 @@ impl pallet_sudo::Config for Runtime {
 parameter_types! {
 	pub const SubtensorInitialRho: u16 = 10;
     pub const SubtensorInitialKappa: u16 = 32_767; // 0.5 = 65535/2 
-    pub const SubtensorInitialMaxAllowedUids: u16 = 512;
+    pub const SubtensorInitialMaxAllowedUids: u16 = 4096;
     pub const SubtensorInitialIssuance: u64 = 0;
-    pub const SubtensorInitialMinAllowedWeights: u16 = 1024;
+    pub const SubtensorInitialMinAllowedWeights: u16 = 128;
     pub const SubtensorInitialEmissionValue: u16 = 0;
-    pub const SubtensorInitialMaxWeightsLimit: u16 = 262;
+    pub const SubtensorInitialMaxWeightsLimit: u16 = 1000; // 1000/2^16 = 0.015
     pub const SubtensorInitialValidatorBatchSize: u16 = 10; // 32
     pub const SubtensorInitialValidatorSequenceLen: u16 = 10; // 256
     pub const SubtensorInitialValidatorEpochLen: u16 = 100;
@@ -331,21 +331,21 @@ parameter_types! {
     pub const SubtensorInitialValidatorLogitsDivergence: u16 = 1310; // 2% of u16
     pub const SubtensorInitialScalingLawPower: u16 = 50; // 0.5
     pub const SubtensorInitialSynergyScalingLawPower: u16 = 50; // 0.5
-    pub const SubtensorInitialMaxAllowedValidators: u16 = 100;
-    pub const SubtensorInitialTempo: u16 = 0;
+    pub const SubtensorInitialMaxAllowedValidators: u16 = 128;
+    pub const SubtensorInitialTempo: u16 = 99;
     pub const SubtensorInitialDifficulty: u64 = 1;
     pub const SubtensorInitialAdjustmentInterval: u16 = 100;
     pub const SubtensorInitialTargetRegistrationsPerInterval: u16 = 2;
     pub const SubtensorInitialImmunityPeriod: u16 = 4096;
     pub const SubtensorInitialActivityCutoff: u16 = 5000;
-    pub const SubtensorInitialMaxRegistrationsPerBlock: u16 = 50;
+    pub const SubtensorInitialMaxRegistrationsPerBlock: u16 = 0;
     pub const SubtensorInitialPruningScore : u16 = u16::MAX;
     pub const SubtensorInitialBondsMovingAverage: u64 = 900_000;
     pub const SubtensorInitialDefaultTake: u16 = 11_796; // 18% honest number.
     pub const SubtensorInitialWeightsVersionKey: u64 = 0;
     pub const SubtensorInitialMinDifficulty: u64 = 1;
     pub const SubtensorInitialMaxDifficulty: u64 = 10;
-    pub const SubtensorInitialServingRateLimit: u64 = 1000; 
+    pub const SubtensorInitialServingRateLimit: u64 = 50; 
 	pub const SubtensorInitialBurn: u64 = 0; 
 	pub const SubtensorInitialMinBurn: u64 = 0; 
 	pub const SubtensorInitialMaxBurn: u64 = 1_000_000_000;
