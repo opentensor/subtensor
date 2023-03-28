99f60b69d53dc3fe8a8776524a6ea02a38bea63d
S0AndS0 <strangerthanbland@gmail.com>
2023-03-28 14:23:59 -0700

    :shell: Track script for collecting code coverage

---
+++ scripts/code-coverage.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -TeE
+
+## Find true directory this script resides in
+__SOURCE__="${BASH_SOURCE[0]}"
+while [[ -h "${__SOURCE__}" ]]; do
+    __SOURCE__="$(find "${__SOURCE__}" -type l -ls | sed -n 's@^.* -> \(.*\)@\1@p')"
+done
+__DIR__="$(cd -P "$(dirname "${__SOURCE__}")" && pwd)"
+__G_DIR__="$(dirname "${__DIR__}")"
+
+## Sub-directory name under: ../target/
+_target_dir_name='tarpaulin'
+
+_tarpaulin_options=(
+	--skip-clean
+	--no-fail-fast
+)
+
+if (( VERBOSE )); then
+	_tarpaulin_options+=( --verbose )
+fi
+
+##
+# Do not fool around with contents of: ../target/debug
+# - https://lib.rs/crates/cargo-tarpaulin#readme-recompilation
+_tarpaulin_options+=(
+	--target-dir "${__G_DIR__}/target/${_target_dir_name}"
+)
+
+SKIP_WASM_BUILD=1 cargo +nightly tarpaulin "${_tarpaulin_options[@]}"
+
