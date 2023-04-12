44ce9463800993ed679126a7cdb23eba736103db
Cameron Fairchild <cameron@opentensor.ai>
2023-04-12 18:29:32 -0400

    Use cfg on more bench things

--- node/src/main.rs
+++ node/src/main.rs
@@ -4,10 +4,11 @@
 mod chain_spec;
 #[macro_use]
 mod service;
-mod benchmarking;
 mod cli;
 mod command;
 mod rpc;
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
 
 fn main() -> sc_cli::Result<()> {
 	command::run()
