404120278ffb5deb2630b5bb1bec3476a7e05b27
Rubberbandits <dalegribble@riseup.net>
2023-04-18 22:54:42 +0000

    Import OpaqueKeys from sp_runtime::traits

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -20,7 +20,7 @@ use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
 use sp_runtime::{
 	create_runtime_str, generic, impl_opaque_keys,
 	traits::{
-		AccountIdLookup, BlakeTwo256, Block as BlockT, IdentifyAccount, NumberFor, One, Verify,
+		AccountIdLookup, BlakeTwo256, Block as BlockT, IdentifyAccount, NumberFor, One, Verify, OpaqueKeys
 	},
 	transaction_validity::{TransactionSource, TransactionValidity},
 	ApplyExtrinsicResult, MultiSignature,
