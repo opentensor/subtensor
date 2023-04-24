6a90f39448ac116a21f46fc7ef808ef74fbafe39
Ala Shaabana <ala@bittensor.com>
2023-04-24 16:54:10 -0400

    Fixes toolchain installation

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,9 +4,12 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
+# remove old nightlies
+rustup toolchain remove nightly
+
 if ! (( ${#CI_PROJECT_NAME} )) ; then
-   rustup update nightly
+   rustup update nightly-2023-01-18
    rustup update stable
 fi
 
-rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-01-18
