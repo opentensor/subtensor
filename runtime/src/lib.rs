22195b3058601cfcba9e65d1e8a023c8889476fe
Rubberbandits <dalegribble@riseup.net>
2023-05-01 20:41:59 +0000

    Remove unused imports from subtensor/lib.rs runtime/lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -48,7 +48,7 @@ pub use frame_support::{
 pub use frame_system::Call as SystemCall;
 pub use pallet_balances::Call as BalancesCall;
 pub use pallet_timestamp::Call as TimestampCall;
-use pallet_transaction_payment::{ConstFeeMultiplier, CurrencyAdapter, Multiplier};
+use pallet_transaction_payment::{CurrencyAdapter, Multiplier};
 #[cfg(any(feature = "std", test))]
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
