7c44b8bf9b7332a6c73005a25ea3a14bcde06669
Rubberbandits <dalegribble@riseup.net>
2023-06-22 15:16:48 -0700

    Begin implementing subtensor-specific senate testing

---
+++ pallets/subtensor/tests/senate.rs
@@ -0,0 +1,44 @@
+use frame_system::Config;
+use sp_core::U256;
+mod mock;
+use mock::*;
+
+#[test]
+fn test_senate_join_works() {
+    
+}
+
+#[test]
+fn test_senate_join_fails_stake_req() {
+    
+}
+
+#[test]
+fn test_senate_vote_works() {
+    
+}
+
+#[test]
+fn test_senate_vote_not_member() {
+    
+}
+
+#[test]
+fn test_senate_leave_works() {
+    
+}
+
+#[test]
+fn test_senate_leave_vote_removal() {
+    
+}
+
+#[test]
+fn test_senate_leave_when_stake_removed() {
+    
+}
+
+#[test]
+fn test_senate_replace_lowest_member() {
+    
+}
