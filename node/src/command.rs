787377a70c69ed5651890088565f4ed5d293f05a
Ala Shaabana <ala@bittensor.com>
2023-08-08 14:09:38 -0700

    Added dev call

--- node/src/command.rs
+++ node/src/command.rs
@@ -46,6 +46,7 @@ impl SubstrateCli for Cli {
 		Ok(match id {
 			"local" => Box::new(chain_spec::localnet_config()?),
 			"finney" => Box::new(chain_spec::finney_mainnet_config()?),
+			"dev" => Box::new(chain_spec::development_config()?),
 			"" |"test_finney" => Box::new(chain_spec::finney_testnet_config()?),
 			path =>
 				Box::new(chain_spec::ChainSpec::from_json_file(std::path::PathBuf::from(path))?),
