3de6c83b461606bfe515f604bd5f6e6d473a3ebe
eduardo <garciaruiz.edu@gmail.com>
2023-08-21 20:45:43 +0200

    BIT-353: BIT-353 github-action; publish-tag; Use self hosted and exeucting script for dependencies installation

---
+++ scripts/install-deps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function install_deps()
+{
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        sudo apt upgrade -y && \
+        sudo apt install -y git build-essential make clang libssl-dev llvm libudev-dev protobuf-compiler
+    elif [[ "$OSTYPE" == "darwin22.0" ]]; then
+        brew upgrade &&
+        yes | brew install git cmake openssl protobuf
+    elif [[ "$OSTYPE" == "darwin22" ]]; then
+        echo "NOT IMPLEMENTED"
+        exit 1
+    elif [[ "$OSTYPE" == "darwin" ]]; then
+        echo "NOT IMPLEMENTED"
+        exit 1
+    elif [[ "$OSTYPE" == "darwin20" ]]; then
+        echo "NOT IMPLEMENTED"
+        exit 1
+    else
+        echo "NOT IMPLEMENTED"
+        exit 1
+    fi
+}
+
+install_deps
