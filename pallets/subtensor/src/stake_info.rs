1e453fdc14b01da992455e7267af954e3e4e55bb
Cameron Fairchild <cameron@opentensor.ai>
2023-08-23 17:38:00 -0400

    return tuples for stake info

--- pallets/subtensor/src/stake_info.rs
+++ pallets/subtensor/src/stake_info.rs
@@ -1,5 +1,4 @@
 use super::*;
-use frame_support::storage::IterableStorageDoubleMap;
 use frame_support::pallet_prelude::{Decode, Encode};
 extern crate alloc;
 use alloc::vec::Vec;
@@ -14,25 +13,32 @@ pub struct StakeInfo<T: Config> {
 }
 
 impl<T: Config> Pallet<T> {
-	fn _get_stake_info_for_coldkeys(coldkeys: Vec<T::AccountId>) -> Vec<StakeInfo<T>> {
+	fn _get_stake_info_for_coldkeys(coldkeys: Vec<T::AccountId>) -> Vec<(T::AccountId, Vec<StakeInfo<T>>)> {
 		if coldkeys.len() == 0 {
 			return Vec::new(); // No coldkeys to check
 		}
 
-		let mut stake_info: Vec<StakeInfo<T>> = Vec::new();
-        for (hotkey, coldkey, stake) in < Stake<T> as IterableStorageDoubleMap<T::AccountId, T::AccountId, u64> >::iter() {
-			if coldkeys.contains(&coldkey) {
-				stake_info.push(StakeInfo {
-					hotkey,
-					coldkey,
-					stake: Compact(stake),
-				});
-        	}
+		let mut stake_info: Vec<(T::AccountId, Vec<StakeInfo<T>>)> = Vec::new();
+		for coldkey_ in coldkeys {
+			let mut stake_info_for_coldkey: Vec<StakeInfo<T>> = Vec::new();
+
+			for (hotkey, coldkey, stake) in <Stake<T>>::iter() {
+				if coldkey == coldkey_ {
+					stake_info_for_coldkey.push(StakeInfo {
+						hotkey,
+						coldkey,
+						stake: stake.into(),
+					});
+				}
+			}
+
+			stake_info.push((coldkey_, stake_info_for_coldkey));
 		}
+
 		return stake_info;
 	}
 
-	pub fn get_stake_info_for_coldkeys(coldkey_account_vecs: Vec<Vec<u8>>) -> Vec<StakeInfo<T>> {
+	pub fn get_stake_info_for_coldkeys(coldkey_account_vecs: Vec<Vec<u8>>) -> Vec<(T::AccountId, Vec<StakeInfo<T>>)> {
 		let mut coldkeys: Vec<T::AccountId> = Vec::new();
 		for coldkey_account_vec in coldkey_account_vecs {
 			if coldkey_account_vec.len() != 32 {
@@ -59,7 +65,11 @@ impl<T: Config> Pallet<T> {
 		let coldkey: AccountIdOf<T> = T::AccountId::decode( &mut coldkey_account_vec.as_bytes_ref() ).unwrap();
 		let stake_info = Self::_get_stake_info_for_coldkeys(vec![coldkey]);
 
-        return stake_info;
+        if stake_info.len() == 0 {
+			return Vec::new(); // Invalid coldkey
+		} else {
+			return stake_info.get(0).unwrap().1.clone();
+		}
 	}
 }
 
