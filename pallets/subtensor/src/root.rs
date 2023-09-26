76a638a03a6e441e3257021b1b0fcc1d36cb5e08
Ayden Brewer <dalegribble@riseup.net>
2023-09-26 10:21:40 -0700

    Use float_kappa not float_rho

--- pallets/subtensor/src/root.rs
+++ pallets/subtensor/src/root.rs
@@ -305,8 +305,8 @@ impl<T: Config> Pallet<T> {
         let one = I64F64::from_num(1);
         let mut consensus = vec![I64F64::from_num(0); total_networks as usize];
         for (idx, trust_score) in trust.iter_mut().enumerate() {
-            let shifted_trust = *trust_score - I64F64::from_num(Self::get_kappa(0)); // Range( -kappa, 1 - kappa )
-            let temperatured_trust = shifted_trust * I64F64::from_num(Self::get_float_rho(0)); // Range( -rho * kappa, rho ( 1 - kappa ) )
+            let shifted_trust = *trust_score - I64F64::from_num(Self::get_float_kappa(0)); // Range( -kappa, 1 - kappa )
+            let temperatured_trust = shifted_trust * I64F64::from_num(Self::get_rho(0)); // Range( -rho * kappa, rho ( 1 - kappa ) )
             let exponentiated_trust: I64F64 = substrate_fixed::transcendental::exp(-temperatured_trust).expect("temperatured_trust is on range( -rho * kappa, rho ( 1 - kappa ) )");
 
             consensus[idx] = one / (one + exponentiated_trust);
