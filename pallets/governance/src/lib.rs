d7551431ca64db0db9b6b5d99337acd85d30ece0
Ayden Brewer <dalegribble@riseup.net>
2023-11-30 10:47:20 -0800

    Add empty extrinsics for each governance action

--- pallets/governance/src/lib.rs
+++ pallets/governance/src/lib.rs
@@ -12,7 +12,7 @@ pub use pallet::*;
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::{pallet_prelude::*};
+	use frame_support::pallet_prelude::*;
 	use frame_system::{pallet_prelude::*, WeightInfo};
 
 	#[pallet::pallet]
@@ -31,24 +31,69 @@ pub mod pallet {
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		
-	}
+	pub enum Event<T: Config> {}
 
-	#[pallet::error]
-	pub enum Error<T> {
-		
-	}
+	//#[pallet::error]
+	//pub enum Error<T> {}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+		/// Vote for a new coldkey to join the Triumvirate (Subnets)
 		#[pallet::call_index(0)]
 		#[pallet::weight((
-			0, //T::WeightInfo::set_identity(), 
+			0,
 			DispatchClass::Operational
 		))]
-		pub fn extrinsic(origin: OriginFor<T>) -> DispatchResult {
-			
+		pub fn nominate(origin: OriginFor<T>) -> DispatchResult {
+
+
+			Ok(().into())
+		}
+
+		/// Remove all emissions from a subnet (Triumvirate)
+		#[pallet::call_index(1)]
+		#[pallet::weight((
+			0,
+			DispatchClass::Operational
+		))]
+		pub fn censor(origin: OriginFor<T>) -> DispatchResult {
+
+
+			Ok(().into())
+		}
+
+		/// Vote on a proposal created by the Triumvirate (Root)
+		#[pallet::call_index(2)]
+		#[pallet::weight((
+			0,
+			DispatchClass::Operational
+		))]
+		pub fn trenchant(origin: OriginFor<T>) -> DispatchResult {
+
+
+			Ok(().into())
+		}
+
+		/// Propose an administrative action (Triumvirate)
+		#[pallet::call_index(3)]
+		#[pallet::weight((
+			0,
+			DispatchClass::Operational
+		))]
+		pub fn sudo(origin: OriginFor<T>) -> DispatchResult {
+
+
+			Ok(().into())
+		}
+
+		/// Punish a delegate validator (Subnets)
+		#[pallet::call_index(4)]
+		#[pallet::weight((
+			0,
+			DispatchClass::Operational
+		))]
+		pub fn ostraca(origin: OriginFor<T>) -> DispatchResult {
+
 
 			Ok(().into())
 		}
