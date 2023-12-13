e78a5d2ee83c3161ac622ee1602e5c894e548c34
ifrit98 <ifrit98@gmail.com>
2023-12-13 16:06:08 +0000

    actually 100 tao

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -625,7 +625,7 @@ parameter_types! {
     pub const SubtensorInitialSenateRequiredStakePercentage: u64 = 1; // 1 percent of total stake
     pub const SubtensorInitialNetworkImmunity: u64 = 7 * 7200;
     pub const SubtensorInitialMinAllowedUids: u16 = 128;
-    pub const SubtensorInitialMinLockCost: u64 = 1_000_000_000; // 100 TAO
+    pub const SubtensorInitialMinLockCost: u64 = 100_000_000_000; // 100 TAO
     pub const SubtensorInitialSubnetOwnerCut: u16 = 11_796; // 18 percent
     pub const SubtensorInitialSubnetLimit: u16 = 12;
     pub const SubtensorInitialNetworkLockReductionInterval: u64 = 14 * 7200;
