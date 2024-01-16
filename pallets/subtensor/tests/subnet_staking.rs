c6c47acf44f822e178b0edf45ddf335875566a40
welikethestock <cherrymaraschino@protonmail.com>
2024-01-16 19:28:50 +0000

    port part of staking tests to subnet staking

--- pallets/subtensor/tests/subnet_staking.rs
+++ pallets/subtensor/tests/subnet_staking.rs
@@ -112,6 +112,392 @@ fn test_subnet_dividends_with_run_to_block() {
 }
 
 #[test]
+fn test_subnet_add_stake_err_signature()
+{
+    new_test_ext().execute_with(|| 
+    {
+        let hotkey_account_id:  U256    = U256::from(654); // bogus
+        let amount:             u64     = 20000; // Not used
+
+        let result = Subtensor::add_subnet_stake(
+            <<Test as Config>::RuntimeOrigin>::none(),
+            hotkey_account_id,
+            1,
+            amount,
+        );
+        assert_eq!(result, DispatchError::BadOrigin.into());
+    });
+}
+
+#[test]
+fn test_add_subnet_stake_network_doesnt_exist() 
+{
+    new_test_ext().execute_with(|| 
+    {
+        let coldkey_account_id: U256    = U256::from(435445);
+        let hotkey_account_id:  U256    = U256::from(54544);
+        let amount:             u64     = 1337;
+
+        Subtensor::add_balance_to_coldkey_account(&coldkey_account_id, 1800);
+
+        assert_eq!(
+            Subtensor::add_subnet_stake(
+                <<Test as Config>::RuntimeOrigin>::signed(coldkey_account_id),
+                hotkey_account_id,
+                1,
+                amount
+            ),
+            Err(Error::<Test>::NetworkDoesNotExist.into())
+        );
+    });
+}
+
+#[test]
+fn test_add_subnet_stake_not_registered_key_pair() 
+{
+    new_test_ext().execute_with(|| 
+    {
+        let coldkey_account_id: U256    = U256::from(435445);
+        let hotkey_account_id:  U256    = U256::from(54544);
+        let amount:             u64     = 1337;
+        let netuid:             u16     = 1;
+
+        add_network(netuid, 1, 0);
+
+        Subtensor::add_balance_to_coldkey_account(&coldkey_account_id, 1800);
+
+        assert_eq!(
+            Subtensor::add_subnet_stake(
+                <<Test as Config>::RuntimeOrigin>::signed(coldkey_account_id),
+                hotkey_account_id,
+                netuid,
+                amount
+            ),
+            Err(Error::<Test>::NotRegistered.into())
+        );
+    });
+}
+
+#[test]
+fn test_add_subnet_stake_err_neuron_does_not_belong_to_coldkey() 
+{
+    new_test_ext().execute_with(|| 
+    {
+        let coldkey_id:     U256    = U256::from(544);
+        let hotkey_id:      U256    = U256::from(54544);
+        let other_cold_key: U256    = U256::from(99498);
+        let netuid:         u16     = 1;
+        let tempo:          u16     = 13;
+        let start_nonce:    u64     = 0;
+
+        //add network
+        add_network(netuid, tempo, 0);
+
+        register_ok_neuron(netuid, hotkey_id, coldkey_id, start_nonce);
+        // Give it some $$$ in his coldkey balance
+        Subtensor::add_balance_to_coldkey_account(&other_cold_key, 100000);
+
+        // Perform the request which is signed by a different cold key
+        let result = Subtensor::add_subnet_stake(
+            <<Test as Config>::RuntimeOrigin>::signed(other_cold_key),
+            hotkey_id,
+            netuid,
+            1000
+        );
+        assert_eq!(result, Err(Error::<Test>::NonAssociatedColdKey.into()));
+    });
+}
+
+
+#[test]
+fn test_add_subnet_stake_err_not_enough_belance() 
+{
+    new_test_ext().execute_with(|| 
+    {
+        let coldkey_id:     U256    = U256::from(544);
+        let hotkey_id:      U256    = U256::from(54544);
+        let netuid:         u16     = 1;
+        let tempo:          u16     = 13;
+        let start_nonce:    u64     = 0;
+
+        //add network
+        add_network(netuid, tempo, 0);
+
+        register_ok_neuron(netuid, hotkey_id, coldkey_id, start_nonce);
+
+        // Lets try to stake with 0 balance in cold key account
+        assert_eq!(Subtensor::get_coldkey_balance(&coldkey_id), 0);
+        let result = Subtensor::add_subnet_stake(
+            <<Test as Config>::RuntimeOrigin>::signed(coldkey_id),
+            hotkey_id,
+            1,
+            60000,
+        );
+
+        assert_eq!(result, Err(Error::<Test>::NotEnoughBalanceToStake.into()));
+    });
+}
+
+#[test]
+fn test_remove_subnet_stake_ok_no_emission() 
+{
+    new_test_ext().execute_with(|| 
+    {
+        let coldkey_account_id: U256    = U256::from(4343);
+        let hotkey_account_id:  U256    = U256::from(4968585);
+        let amount:             u64     = 10000;
+        let netuid:             u16     = 1;
+        let tempo:              u16     = 13;
+        let start_nonce:        u64     = 0;
+
+        //add network
+        add_network(netuid, tempo, 0);
+
+        // Let's spin up a neuron
+        register_ok_neuron(netuid, hotkey_account_id, coldkey_account_id, start_nonce);
+
+        // Some basic assertions
+        assert_eq!(Subtensor::get_subnet_total_stake(netuid), 0);
+        assert_eq!(
+            Subtensor::get_subnet_total_stake_for_hotkey(netuid, &hotkey_account_id),
+            0
+        );
+        assert_eq!(Subtensor::get_coldkey_balance(&coldkey_account_id), 0);
+
+        // Give the neuron some stake to remove
+        Subtensor::inc_subnet_stake_for_coldkey_hotkey(netuid, &coldkey_account_id, &hotkey_account_id, amount);
+
+        // Do the magic
+        assert_ok!(Subtensor::remove_subnet_stake(
+            <<Test as Config>::RuntimeOrigin>::signed(coldkey_account_id),
+            hotkey_account_id,
+            netuid,
+            amount
+        ));
+
+        assert_eq!(
+            Subtensor::get_coldkey_balance(&coldkey_account_id),
+            amount
+        );
+        assert_eq!(
+            Subtensor::get_subnet_total_stake_for_hotkey(netuid, &hotkey_account_id),
+            0
+        );
+        assert_eq!(Subtensor::get_subnet_total_stake(netuid), 0);
+    });
+}
+
+
+#[test]
+fn test_remove_subnet_stake_amount_zero() 
+{
+    new_test_ext().execute_with(|| 
+    {
+        let coldkey_account_id: U256    = U256::from(4343);
+        let hotkey_account_id:  U256    = U256::from(4968585);
+        let amount:             u64     = 10000;
+        let netuid:             u16     = 1;
+        let tempo:              u16     = 13;
+        let start_nonce:        u64     = 0;
+
+        //add network
+        add_network(netuid, tempo, 0);
+
+        // Let's spin up a neuron
+        register_ok_neuron(netuid, hotkey_account_id, coldkey_account_id, start_nonce);
+
+        // Some basic assertions
+        assert_eq!(Subtensor::get_subnet_total_stake(netuid), 0);
+        assert_eq!(
+            Subtensor::get_subnet_stake_for_coldkey_hotkey(netuid, &coldkey_account_id, &hotkey_account_id),
+            0
+        );
+        assert_eq!(Subtensor::get_coldkey_balance(&coldkey_account_id), 0);
+
+        // Give the neuron some stake to remove
+        Subtensor::inc_subnet_stake_for_coldkey_hotkey(netuid, &coldkey_account_id, &hotkey_account_id, amount);
+
+        // Do the magic
+        assert_noop!(
+            Subtensor::remove_subnet_stake(
+                <<Test as Config>::RuntimeOrigin>::signed(coldkey_account_id),
+                hotkey_account_id,
+                netuid,
+                0
+            ),
+            Error::<Test>::NotEnoughStaketoWithdraw
+        );
+    });
+}
+
+#[test]
+fn test_remove_subnet_stake_err_signature() 
+{
+    new_test_ext().execute_with(|| 
+    {
+        let hotkey_account_id:  U256    = U256::from(4968585);
+        let amount:             u64     = 10000; // Amount to be removed
+
+        let result = Subtensor::remove_subnet_stake(
+            <<Test as Config>::RuntimeOrigin>::none(),
+            hotkey_account_id,
+            1,
+            amount,
+        );
+        assert_eq!(result, DispatchError::BadOrigin.into());
+    });
+}
+
+#[test]
+fn test_remove_subnet_stake_err_hotkey_does_not_belong_to_coldkey() 
+{
+    new_test_ext().execute_with(|| 
+    {
+        let coldkey_id:     U256    = U256::from(544);
+        let hotkey_id:      U256    = U256::from(54544);
+        let other_cold_key: U256    = U256::from(99498);
+        let netuid:         u16     = 1;
+        let tempo:          u16     = 13;
+        let start_nonce:    u64     = 0;
+
+        //add network
+        add_network(netuid, tempo, 0);
+
+        register_ok_neuron(netuid, hotkey_id, coldkey_id, start_nonce);
+
+        // Perform the request which is signed by a different cold key
+        let result = Subtensor::remove_subnet_stake(
+            <<Test as Config>::RuntimeOrigin>::signed(other_cold_key),
+            hotkey_id,
+            netuid,
+            1000,
+        );
+        assert_eq!(result, Err(Error::<Test>::NonAssociatedColdKey.into()));
+    });
+}
+
+
+#[test]
+fn test_remove_subnet_stake_no_enough_stake() 
+{
+    new_test_ext().execute_with(|| 
+    {
+        let coldkey_id:     U256    = U256::from(544);
+        let hotkey_id:      U256    = U256::from(54544);
+        let amount:         u64     = 10000;
+        let netuid:         u16     = 1;
+        let tempo:          u16     = 13;
+        let start_nonce:    u64     = 0;
+
+        //add network
+        add_network(netuid, tempo, 0);
+
+        register_ok_neuron(netuid, hotkey_id, coldkey_id, start_nonce);
+
+        assert_eq!(Subtensor::get_subnet_stake_for_coldkey_hotkey(netuid, &coldkey_id, &hotkey_id), 0);
+
+        let result = Subtensor::remove_subnet_stake(
+            <<Test as Config>::RuntimeOrigin>::signed(coldkey_id),
+            hotkey_id,
+            netuid,
+            amount,
+        );
+        assert_eq!(result, Err(Error::<Test>::NotEnoughStaketoWithdraw.into()));
+    });
+}
+
+#[test]
+fn test_remove_subnet_stake_total_balance_no_change() 
+{
+    // When we remove stake, the total balance of the coldkey account should not change
+    //    this is because the stake should be part of the coldkey account balance (reserved/locked)
+    //    then the removed stake just becomes free balance
+    new_test_ext().execute_with(|| 
+    {
+        let hotkey_account_id:  U256    = U256::from(571337);
+        let coldkey_account_id: U256    = U256::from(71337);
+        let netuid:             u16     = 1;
+        let tempo:              u16     = 13;
+        let start_nonce:        u64     = 0;
+        let amount:             u64     = 10000;
+
+        //add network
+        add_network(netuid, tempo, 0);
+
+        // Register neuron
+        register_ok_neuron(netuid, hotkey_account_id, coldkey_account_id, start_nonce);
+
+        // Some basic assertions
+        assert_eq!(Subtensor::get_subnet_total_stake(netuid), 0);
+        assert_eq!(
+            Subtensor::get_subnet_stake_for_coldkey_hotkey(netuid, &coldkey_account_id, &hotkey_account_id),
+            0
+        );
+        assert_eq!(Subtensor::get_coldkey_balance(&coldkey_account_id), 0);
+        let initial_total_balance = Balances::total_balance(&coldkey_account_id);
+        assert_eq!(initial_total_balance, 0);
+
+        // Give the neuron some stake to remove
+        Subtensor::inc_subnet_stake_for_coldkey_hotkey(netuid, &coldkey_account_id, &hotkey_account_id, amount);
+
+        // Do the magic
+        assert_ok!(Subtensor::remove_subnet_stake(
+            <<Test as Config>::RuntimeOrigin>::signed(coldkey_account_id),
+            hotkey_account_id,
+            netuid,
+            amount
+        ));
+
+        assert_eq!(
+            Subtensor::get_coldkey_balance(&coldkey_account_id),
+            amount
+        );
+        assert_eq!(
+            Subtensor::get_subnet_stake_for_coldkey_hotkey(netuid, &coldkey_account_id, &hotkey_account_id),
+            0
+        );
+        assert_eq!(Subtensor::get_subnet_total_stake(netuid), 0);
+
+        // Check total balance is equal to the added stake. Even after remove stake (no fee, includes reserved/locked balance)
+        let total_balance = Balances::total_balance(&coldkey_account_id);
+        assert_eq!(total_balance, amount);
+    });
+}
+
+#[test]
+fn test_add_stake_to_hotkey_account_ok() 
+{
+    new_test_ext().execute_with(|| 
+    {
+        let hotkey_id:      U256    = U256::from(5445);
+        let coldkey_id:     U256    = U256::from(5443433);
+        let amount:         u64     = 10000;
+        let netuid:         u16     = 1;
+        let tempo:          u16     = 13;
+        let start_nonce:    u64     = 0;
+
+        //add network
+        add_network(netuid, tempo, 0);
+
+        register_ok_neuron(netuid, hotkey_id, coldkey_id, start_nonce);
+
+        // There is not stake in the system at first, so result should be 0;
+        assert_eq!(Subtensor::get_subnet_total_stake(netuid), 0);
+
+        Subtensor::inc_subnet_stake_for_coldkey_hotkey(netuid, &coldkey_id, &hotkey_id, amount);
+
+        // The stake that is now in the account, should equal the amount
+        assert_eq!(
+            Subtensor::get_subnet_total_stake_for_hotkey(netuid, &hotkey_id),
+            amount
+        );
+
+        // The total stake should have been increased by the amount -> 0 + amount = amount
+        assert_eq!(Subtensor::get_subnet_total_stake(netuid), amount);
+    });
+}
+
+#[test]
 fn test_subnet_full_block_emission_occurs() {
     new_test_ext().execute_with(|| {
         let netuid:     u16     = 1;
