8b3e9467ab5a519586312ca7152c6a58ee0d99f8
Sam Johnson <sam@durosoft.com>
2024-03-11 13:29:01 -0400

    fix GenesisBuild re-export issue

--- pallets/collective/src/lib.rs
+++ pallets/collective/src/lib.rs
@@ -42,6 +42,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![recursion_limit = "128"]
 
+use frame_support::traits::GenesisBuild;
 use scale_info::TypeInfo;
 use sp_io::storage;
 use sp_runtime::{traits::Hash, RuntimeDebug};
