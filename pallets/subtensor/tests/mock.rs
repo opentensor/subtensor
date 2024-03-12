352b25fc3a023dfc81de4ba4551a2028dce80038
Sam Johnson <sam@durosoft.com>
2024-03-12 17:56:09 -0400

    fix subtensor pallet test mock config

--- pallets/subtensor/tests/mock.rs
+++ pallets/subtensor/tests/mock.rs
@@ -93,13 +93,10 @@ impl system::Config for Test {
     type DbWeight = ();
     type RuntimeOrigin = RuntimeOrigin;
     type RuntimeCall = RuntimeCall;
-    type Index = u64;
-    type BlockNumber = u64;
     type Hash = H256;
     type Hashing = BlakeTwo256;
     type AccountId = U256;
     type Lookup = IdentityLookup<Self::AccountId>;
-    type Header = Header;
     type RuntimeEvent = RuntimeEvent;
     type BlockHashCount = BlockHashCount;
     type Version = ();
@@ -111,6 +108,8 @@ impl system::Config for Test {
     type SS58Prefix = SS58Prefix;
     type OnSetCode = ();
     type MaxConsumers = frame_support::traits::ConstU32<16>;
+    type Nonce = u64;
+    type Block = Block;
 }
 
 parameter_types! {
