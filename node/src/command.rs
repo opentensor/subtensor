1b2b5269bfd8a83ff25fa3a44dddad59ce9db0cb
Keith Yeung <keith@opentensor.dev>
2024-03-20 00:29:56 +0800

    Remove try-runtime subcommand

--- node/src/command.rs
+++ node/src/command.rs
@@ -13,12 +13,6 @@ pub use node_subtensor_runtime::EXISTENTIAL_DEPOSIT;
 #[cfg(feature = "runtime-benchmarks")]
 pub use sp_keyring::Sr25519Keyring;
 
-#[cfg(feature = "try-runtime")]
-use {
-    kitchensink_runtime::constants::time::SLOT_DURATION,
-    try_runtime_cli::block_building_info::substrate_info,
-};
-
 use node_subtensor_runtime::Block;
 use sc_cli::SubstrateCli;
 use sc_service::PartialComponents;
@@ -207,33 +201,6 @@ pub fn run() -> sc_cli::Result<()> {
                 }
             })
         }
-        #[cfg(feature = "try-runtime")]
-        Some(Subcommand::TryRuntime(cmd)) => {
-            use crate::service::ExecutorDispatch;
-            use sc_executor::{sp_wasm_interface::ExtendedHostFunctions, NativeExecutionDispatch};
-            let runner = cli.create_runner(cmd)?;
-            runner.async_run(|config| {
-                // we don't need any of the components of new_partial, just a runtime, or a task
-                // manager to do `async_run`.
-                let registry = config.prometheus_config.as_ref().map(|cfg| &cfg.registry);
-                let task_manager =
-                    sc_service::TaskManager::new(config.tokio_handle.clone(), registry)
-                        .map_err(|e| sc_cli::Error::Service(sc_service::Error::Prometheus(e)))?;
-                let info_provider = substrate_info(SLOT_DURATION);
-
-                Ok((
-                    cmd.run::<Block, ExtendedHostFunctions<
-                        sp_io::SubstrateHostFunctions,
-                        <ExecutorDispatch as NativeExecutionDispatch>::ExtendHostFunctions,
-                    >, _>(Some(info_provider)),
-                    task_manager,
-                ))
-            })
-        }
-        #[cfg(not(feature = "try-runtime"))]
-        Some(Subcommand::TryRuntime) => Err("TryRuntime wasn't enabled when building the node. \
-				You can enable it with `--features try-runtime`."
-            .into()),
         Some(Subcommand::ChainInfo(cmd)) => {
             let runner = cli.create_runner(cmd)?;
             runner.sync_run(|config| cmd.run::<Block>(&config))
