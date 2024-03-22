835a0f526b2d43b2b0b7c5b3a41798c396a40233
Sam Johnson <sam@durosoft.com>
2024-03-22 15:54:46 -0400

    add missing unwraps to tests

--- pallets/subtensor/src/migration.rs
+++ pallets/subtensor/src/migration.rs
@@ -113,8 +113,8 @@ pub fn migrate_create_root_network<T: Config>() -> Weight {
     // Empty senate members entirely, they will be filled by by registrations
     // on the subnet.
     for hotkey_i in T::SenateMembers::members().iter() {
-        T::TriumvirateInterface::remove_votes(&hotkey_i);
-        T::SenateMembers::remove_member(&hotkey_i);
+        T::TriumvirateInterface::remove_votes(&hotkey_i).unwrap();
+        T::SenateMembers::remove_member(&hotkey_i).unwrap();
 
         weight.saturating_accrue(T::DbWeight::get().reads_writes(2, 2));
     }
