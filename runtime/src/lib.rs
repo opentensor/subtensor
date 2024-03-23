b995ce3820150606fbd2bd928a9f56c0c0a280d8
Cameron Fairchild <cameron@opentensor.ai>
2024-03-23 00:39:39 -0400

    account for 32 + size of AccountId

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -474,8 +474,9 @@ impl pallet_sudo::Config for Runtime {
 }
 
 parameter_types! {
-    // One storage item; key size is 32; value is size 4+4+16+32 bytes = 56 bytes.
-    pub const DepositBase: Balance = (1) as Balance * 2_000 * 10_000 + (88 as Balance) * 100 * 10_000;
+    // One storage item; key size is 32 + sizeof(AccountId) = 64; value is size 4 + sizeof((BlockNumber, Balance, AccountId)) = 56;
+	// = 120 bytes total.
+    pub const DepositBase: Balance = (1) as Balance * 2_000 * 10_000 + (120 as Balance) * 100 * 10_000;
     // Additional storage item size of 32 bytes.
     pub const DepositFactor: Balance = (0) as Balance * 2_000 * 10_000 + (32 as Balance) * 100 * 10_000;
     pub const MaxSignatories: u32 = 100;
