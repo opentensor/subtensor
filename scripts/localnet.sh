619f8e477d0736ad0d9e1699e2decb1a1bf6f4f9
Greg Zaitsev <greg.zaitsev@gmail.com>
2024-04-12 11:54:51 -0400

    Remove ws-port from launch parameters, update rpc-port on both nodes

--- scripts/localnet.sh
+++ scripts/localnet.sh
@@ -35,8 +35,7 @@ alice_start=(
     --chain="$FULL_PATH"
     --alice
     --port 30334
-    --ws-port 9946
-    --rpc-port 9934
+    --rpc-port 9946
     --validator
     --rpc-cors=all
     --allow-private-ipv4
@@ -49,8 +48,7 @@ bob_start=(
     --chain="$FULL_PATH"
     --bob
     --port 30335
-    --ws-port 9947
-    --rpc-port 9935
+    --rpc-port 9947
     --validator
     --allow-private-ipv4
     --discover-local
