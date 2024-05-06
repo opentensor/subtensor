f4be4fd1f68d4752dd3b064aba162ab1b9ceabb5
open-junius <zhou@opentensor.dev>
2024-05-06 22:06:21 +0800

    correct one comment

--- pallets/subtensor/src/errors.rs
+++ pallets/subtensor/src/errors.rs
@@ -1,6 +1,6 @@
 use frame_support::pallet_macros::pallet_section;
 
-/// A [`pallet_section`] that defines the events for a pallet.
+/// A [`pallet_section`] that defines the errors for a pallet.
 /// This can later be imported into the pallet using [`import_section`].
 #[pallet_section]
 mod errors {
