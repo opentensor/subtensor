f7653212e1b0e9e6d1ec0ff722dc31bf92f034e5
open-junius <zhou@opentensor.dev>
2024-05-09 23:21:40 +0800

    remove commented code

--- runtime/src/check_nonce.rs
+++ runtime/src/check_nonce.rs
@@ -1,20 +1,3 @@
-// This file is part of Substrate.
-
-// Copyright (C) Parity Technologies (UK) Ltd.
-// SPDX-License-Identifier: Apache-2.0
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// 	http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 use codec::{Decode, Encode};
 use frame_support::dispatch::{DispatchInfo, Pays};
 use frame_system::Config;
@@ -79,7 +62,6 @@ where
         info: &DispatchInfoOf<Self::Call>,
         _len: usize,
     ) -> Result<(), TransactionValidityError> {
-        // let dispatch_info = <Call as GetDispatchInfo>::get_dispatch_info(call);
         let mut account = frame_system::Account::<T>::get(who);
         match info.pays_fee {
             Pays::Yes => {
