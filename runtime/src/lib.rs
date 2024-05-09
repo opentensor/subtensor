dc6ff91d405e7af05f8d8a424571adea4612f602
open-junius <zhou@opentensor.dev>
2024-05-09 22:41:31 +0800

    fix pays no check

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -6,6 +6,7 @@
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
+mod check_nonce;
 mod migrations;
 
 use codec::{Decode, Encode, MaxEncodedLen};
@@ -1177,7 +1178,7 @@ pub type SignedExtra = (
     frame_system::CheckTxVersion<Runtime>,
     frame_system::CheckGenesis<Runtime>,
     frame_system::CheckEra<Runtime>,
-    frame_system::CheckNonce<Runtime>,
+    check_nonce::CheckNonce<Runtime>,
     frame_system::CheckWeight<Runtime>,
     pallet_transaction_payment::ChargeTransactionPayment<Runtime>,
     pallet_subtensor::SubtensorSignedExtension<Runtime>,
