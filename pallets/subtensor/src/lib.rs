de987e32bccdd3e8cc15141cf030eaa6245d1dcd
Greg Zaitsev <gregz@opentensor.dev>
2024-05-17 19:38:10 -0400

    Add migration to populate SubnetCreator

--- pallets/subtensor/src/lib.rs
+++ pallets/subtensor/src/lib.rs
@@ -1363,7 +1363,9 @@ pub mod pallet {
                 // Storage version v6 -> v7
                 .saturating_add(migration::migrate_stake_to_substake::<T>())
                 // Storage version v7 -> v8
-                .saturating_add(migration::migrate_remove_deprecated_stake_variables::<T>());
+                .saturating_add(migration::migrate_remove_deprecated_stake_variables::<T>())
+                // Storage version v8 -> v9
+                .saturating_add(migration::migrate_populate_subnet_creator::<T>());
 
             log::info!(
                 "Runtime upgrade migration in subtensor pallet, total weight = ({})",
