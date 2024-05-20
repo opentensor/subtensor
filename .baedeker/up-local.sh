55e6160aa1bab07b1a41e90df7699a027a69b30f
Greg Zaitsev <gregz@opentensor.dev>
2024-05-20 14:13:58 -0400

    Revert up.sh to original, add up-local.sh that adds Alice balance and makes network development

---
+++ .baedeker/up-local.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+BDK_DIR=$(dirname $(readlink -f "$0"))
+RUST_LOG=info baedeker --spec=docker -J$BDK_DIR/vendor/ --generator=docker_compose=$BDK_DIR/.bdk-env --generator=docker_compose_discover=$BDK_DIR/.bdk-env/discover.env --secret=file=$BDK_DIR/.bdk-env/secret  --tla-str=relay_spec=rococo-local --input-modules='lib:baedeker-library/ops/nginx.libsonnet' --input-modules='lib:baedeker-library/ops/devtools.libsonnet' --tla-str=repoDir=$(realpath $BDK_DIR/..) $@ $BDK_DIR/rewrites.jsonnet
+cd $BDK_DIR/.bdk-env
+
+# Change the network type to development
+sed -i 's#"chainType": "Live"#"chainType": "Development"#' ./specs/subtensor.json
+
+# Give Alice some balance
+sed -i 's#^"0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9de1e86a9a8c739864cf3cc5ec2bea59fd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d": "0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9de1e86a9a8c739864cf3cc5ec2bea59fd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"#"0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9de1e86a9a8c739864cf3cc5ec2bea59fd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d": "0x0000000000000000010000000000000000407a10f35a0000000000000000000000000000000000000000000000000000"#' ./specs/subtensor.json
+
+#docker compose up -d --wait --remove-orphans
