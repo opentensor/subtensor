a96b41d93f65227b2f96cdaa3693ab8d7d1a7dbe
Liam <liam@opentensor.dev>
2024-05-31 19:12:40 +0300

    feat: build_all_chainspecs.sh script
    test shasum

---
+++ scripts/build_all_chainspecs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "*** Building node..."
+cargo +nightly-2024-03-05 build
+
+echo "*** Building chainspecs..."
+./target/debug/node-subtensor build-spec --raw --chain finney >raw_spec_finney.json
+./target/debug/node-subtensor build-spec --chain finney >plain_spec_finney.json
+
+./target/debug/node-subtensor build-spec --raw --chain test_finney >raw_spec_testfinney.json
+./target/debug/node-subtensor build-spec --chain test_finney >plain_spec_testfinney.json
+
+echo "*** Done!"
