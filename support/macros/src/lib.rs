54f31ce0b8deef4765b763990b38da43093e7beb
Sam Johnson <sam@durosoft.com>
2024-06-07 16:11:46 -0400

    working

--- support/macros/src/lib.rs
+++ support/macros/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
-use quote::quote;
-use syn::{parse::Nothing, parse2, visit_mut::visit_item_struct_mut, ItemStruct, LitStr, Result};
+use quote::ToTokens;
+use syn::{parse2, visit_mut::visit_item_struct_mut, Error, ItemStruct, LitStr, Result};
 
 mod visitor;
 use visitor::*;
@@ -9,7 +9,7 @@ use visitor::*;
 #[proc_macro_attribute]
 pub fn freeze_struct(attr: TokenStream, tokens: TokenStream) -> TokenStream {
     match freeze_struct_impl(attr, tokens) {
-        Ok(tokens) => tokens.into(),
+        Ok(item_struct) => item_struct.to_token_stream().into(),
         Err(err) => err.to_compile_error().into(),
     }
 }
@@ -17,18 +17,31 @@ pub fn freeze_struct(attr: TokenStream, tokens: TokenStream) -> TokenStream {
 fn freeze_struct_impl(
     attr: impl Into<TokenStream2>,
     tokens: impl Into<TokenStream2>,
-) -> Result<TokenStream2> {
+) -> Result<ItemStruct> {
     let attr = attr.into();
     let tokens = tokens.into();
 
-    let mut item = parse2::<ItemStruct>(tokens)?;
+    let item = parse2::<ItemStruct>(tokens)?;
+    let mut item_clone = item.clone();
     let hash_lit = parse2::<LitStr>(attr)?;
+    let provided_hash_hex = hash_lit.value().to_lowercase();
 
     let mut visitor = CleanDocComments::new();
-    visit_item_struct_mut(&mut visitor, &mut item);
+    visit_item_struct_mut(&mut visitor, &mut item_clone);
 
     let calculated_hash = generate_hash(&item);
     let calculated_hash_hex = format!("{:x}", calculated_hash);
 
-    todo!()
+    if provided_hash_hex != calculated_hash_hex {
+        return Err(Error::new_spanned(item,
+            format!(
+                "You have made a non-trivial change to this struct and the provided hashcode no longer matches:\n{} != {}\n\n\
+                If this was intentional, please update the hashcode in the `freeze_struct` attribute to:\n\
+                {}\n\nNote that if you are changing a storage struct in any way, including simply re-ordering fields, \
+                you will need a migration to prevent data corruption.",
+                provided_hash_hex, calculated_hash_hex, calculated_hash_hex
+            ),
+        ));
+    }
+    Ok(item)
 }
--- support/macros/tests/tests.rs
+++ support/macros/tests/tests.rs
@@ -1,7 +1,7 @@
 use subtensor_macros::freeze_struct;
 
-#[freeze_struct("1f29b57a32dbb617")]
-struct MyStruct {
-    ip: u32,
-    port: u32,
+#[freeze_struct("18ec48d6e1ccaa1b")]
+pub struct MyStruct {
+    pub ip: u32,
+    pub port: u32,
 }
