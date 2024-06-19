e2282e45d14c055a3d7a7ab40facc823ccbd9b06
Liam <liam@opentensor.dev>
2024-06-19 19:44:37 +0800

    disallow changing genesis

--- scripts/build_all_chainspecs.sh
+++ scripts/build_all_chainspecs.sh
@@ -5,11 +5,39 @@ set -e
 echo "*** Building node..."
 cargo build
 
-echo "*** Building chainspecs..."
-./target/debug/node-subtensor build-spec --raw --chain finney >raw_spec_finney.json
+echo "*** Building new chainspecs..."
+
+finney_genesis_temp=$(mktemp)
+testfinney_genesis_temp=$(mktemp)
+raw_spec_finney_temp=$(mktemp)
+raw_spec_testfinney_temp=$(mktemp)
+
+# Save old genesis state before doing anything
+jq -r ".genesis" /tmp/raw_spec_finney.json.old >"$finney_genesis_temp"
+jq -r ".genesis" /tmp/raw_spec_testfinney.json.old >"$testfinney_genesis_temp"
+
+# Build new chainspecs
+./target/debug/node-subtensor build-spec --raw --chain finney >"$raw_spec_finney_temp"
 ./target/debug/node-subtensor build-spec --chain finney >plain_spec_finney.json
 
-./target/debug/node-subtensor build-spec --raw --chain test_finney >raw_spec_testfinney.json
+./target/debug/node-subtensor build-spec --raw --chain test_finney >"$raw_spec_testfinney_temp"
 ./target/debug/node-subtensor build-spec --chain test_finney >plain_spec_testfinney.json
+
+echo "*** Updating genesis..."
+
+# The genesis is not allowed to change. Since the wasm genesis will change depending on the system
+# architecture used, we need to extract the genesis from the old chain specs and insert them into
+# the new chain specs to ensure there are no genesis mismatch issues.
+
+# Update genesis in new chainspecs using the extracted genesis data from the temporary files
+jq --slurpfile genesis "$finney_genesis_temp" '.genesis = $genesis[0]' "$raw_spec_finney_temp" >raw_spec_finney_temp_updated
+jq --slurpfile genesis "$testfinney_genesis_temp" '.genesis = $genesis[0]' "$raw_spec_testfinney_temp" >raw_spec_testfinney_temp_updated
+
+# Success! Actually replace the raw spec files.
+mv raw_spec_finney_temp_updated raw_spec_finney.json
+mv raw_spec_testfinney_temp_updated raw_spec_testfinney.json
+
+# Cleanup
+rm -f "$finney_genesis_temp" "$testfinney_genesis_temp" "$raw_spec_finney_temp" "$raw_spec_testfinney_temp"
 
 echo "*** Done!"
