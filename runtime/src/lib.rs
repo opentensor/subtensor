d147d728c917e17514d248a26885609f14074344
Greg Zaitsev <gregz@opentensor.dev>
2024-06-25 19:33:39 -0400

    Add pallet registry migration to populate delegates initially from GitHub data

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -153,7 +153,7 @@ pub const MILLISECS_PER_BLOCK: u64 = 12000;
 
 /// Fast blocks for development
 #[cfg(feature = "fast-blocks")]
-pub const MILLISECS_PER_BLOCK: u64 = 250;
+pub const MILLISECS_PER_BLOCK: u64 = 12000;
 
 // NOTE: Currently it is not possible to change the slot duration after the chain has started.
 //       Attempting to do so will brick block production.
