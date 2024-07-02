6dbcbffd66f25cb42bcf1914f38687cf46ea3dd2
Sam Johnson <sam@durosoft.com>
2024-07-02 19:43:11 -0400

    whitelist commit_weights and add_stake

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -305,7 +305,14 @@ impl Contains<RuntimeCall> for SafeModeWhitelistedCalls {
                 | RuntimeCall::System(_)
                 | RuntimeCall::SafeMode(_)
                 | RuntimeCall::Timestamp(_)
-                | RuntimeCall::SubtensorModule(_)
+                | RuntimeCall::SubtensorModule(pallet_subtensor::Call::commit_weights {
+                    netuid,
+                    commit_hash
+                })
+                | RuntimeCall::SubtensorModule(pallet_subtensor::Call::add_stake {
+                    hotkey,
+                    amount_staked
+                })
         )
     }
 }
