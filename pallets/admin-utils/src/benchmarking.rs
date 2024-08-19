bb66f09f38a3fb4d0a06906c53b833cb3057f392
Keith <keith@opentensor.dev>
2024-08-19 10:01:03 -0400

    cargo fmt

--- pallets/admin-utils/src/benchmarking.rs
+++ pallets/admin-utils/src/benchmarking.rs
@@ -251,7 +251,10 @@ mod benchmarks {
 
     #[benchmark]
     fn sudo_set_hotkey_emission_tempo() {
-        pallet_subtensor::Pallet::<T>::init_new_network(1u16 /*netuid*/, 1u16 /*sudo_tempo*/);
+        pallet_subtensor::Pallet::<T>::init_new_network(
+            1u16, /*netuid*/
+            1u16, /*sudo_tempo*/
+        );
 
         #[extrinsic_call]
         _(RawOrigin::Root, 1u64/*emission_tempo*/)/*set_hotkey_emission_tempo*/;
