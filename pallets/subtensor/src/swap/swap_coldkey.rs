93233fcb303a6e83e30db52699c5cb1345ed97dc
John Reed <87283488+JohnReedV@users.noreply.github.com>
2024-08-21 10:21:35 -1000

    clippy

--- pallets/subtensor/src/swap/swap_coldkey.rs
+++ pallets/subtensor/src/swap/swap_coldkey.rs
@@ -50,7 +50,7 @@ impl<T: Config> Pallet<T> {
         weight = weight.saturating_add(T::DbWeight::get().reads(1));
 
         // 5. Swap the identity if the old coldkey has one
-        if let Some(identity) = Identities::<T>::take(&old_coldkey) {
+        if let Some(identity) = Identities::<T>::take(old_coldkey) {
             Identities::<T>::insert(new_coldkey, identity);
         }
 
