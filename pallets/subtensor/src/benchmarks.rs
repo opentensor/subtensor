936cea75d70ae41d72a43032436d39aea1b7e927
John Reed <87283488+JohnReedV@users.noreply.github.com>
2024-08-23 08:32:33 -1000

    fix benchmark_dissolve_network

--- pallets/subtensor/src/benchmarks.rs
+++ pallets/subtensor/src/benchmarks.rs
@@ -312,7 +312,7 @@ benchmarks! {
     let amount_to_be_staked = 100_000_000_000_000u64;
     Subtensor::<T>::add_balance_to_coldkey_account(&coldkey.clone(), amount_to_be_staked);
     assert_ok!(Subtensor::<T>::register_network(RawOrigin::Signed(coldkey.clone()).into(), None));
-  }: dissolve_network(RawOrigin::Signed(coldkey), coldkey.clone(), 1)
+  }: dissolve_network(RawOrigin::Root, coldkey.clone(), 1)
 
 
   // swap_hotkey {
