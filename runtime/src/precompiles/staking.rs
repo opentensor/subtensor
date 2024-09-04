8285c39889bc42904c3a5ea1f29e6fbc8c8b2f7a
DedicatedDev <topdev323@gmail.com>
2024-09-04 20:26:28 +0300

    add add staking logic

--- runtime/src/precompiles/staking.rs
+++ runtime/src/precompiles/staking.rs
@@ -1,25 +1,83 @@
-/// Staking precompile's goal is to allow interaction between EVM users and smart contracts and 
-/// subtensor staking functionality, namely add_stake, and remove_stake extrinsics.
-/// 
-/// Additional requirement is to preserve compatibility with Ethereum indexers, which requires 
-/// no balance transfers from EVM accounts without a corresponding transaction that can be 
-/// parsed by an indexer.
-/// 
-/// Implementation of add_stake:
-///   - User transfers balance that will be staked to the precompile address with a payable 
-///     method addStake. This method also takes hotkey public key (bytes32) of the hotkey
-///     that the stake should be assigned to.
-///   - Precompile transfers the balance back to the signing address, and then invokes 
-///     do_add_stake from subtensor pallet with signing origin that mmatches to HashedAddressMapping
-///     of the message sender, which will effectively withdraw and stake balance from the message 
-///     sender.
-///   - Precompile checks the result of do_add_stake and, in case of a failure, reverts the transaction, 
-///     and leaves the balance on the message sender account.
-/// 
-/// Implementation of remove_stake:
-///   - User involkes removeStake method and specifies hotkey public key (bytes32) of the hotkey
-///     to remove stake from, and the amount to unstake.
-///   - Precompile calls do_remove_stake method of the subtensor pallet with the signing origin of message 
-///     sender, which effectively unstakes the specified amount and credits it to the message sender
-///   - Precompile checks the result of do_remove_stake and, in case of a failure, reverts the transaction.
-/// 
+use frame_system::RawOrigin;
+use pallet_evm::{
+    ExitError, ExitSucceed, PrecompileFailure, PrecompileHandle, PrecompileOutput, PrecompileResult,
+};
+use pallet_evm::{HashedAddressMapping,AddressMapping};
+use sp_core::{H160, U256};
+use sp_runtime::traits::BlakeTwo256;
+use sp_runtime::traits::Dispatchable;
+use sp_runtime::AccountId32;
+use sp_std::vec;
+use crate::precompiles::{bytes_to_account_id, get_method_id, get_slice};
+
+
+use crate::{Runtime, RuntimeCall};
+pub const STAKING_PRECOMPILE_INDEX: u64 = 2049;
+
+pub struct StakingPrecompile;
+
+impl StakingPrecompile {
+    pub fn execute(handle: &mut impl PrecompileHandle) -> PrecompileResult {
+        let txdata = handle.input();
+        let method_id = get_slice(txdata, 0, 4)?;
+        let method_input = txdata[4..].to_vec(); // Avoiding borrowing conflicts
+
+        if method_id ==  get_method_id("addStake(bytes32)") {
+           return  Self::add_stake(handle, &method_input);
+        } else if method_id == get_method_id("removeStake(bytes32,uint64)") {
+           return  Self::remove_stake(handle, &method_input);
+        }else {
+            return Err(PrecompileFailure::Error {
+                exit_status: ExitError::InvalidRange,
+            })
+        }
+    }
+
+    fn add_stake(handle: &mut impl PrecompileHandle, data: &[u8]) -> PrecompileResult {
+        let hotkey = Self::parse_hotkey(data)?.into();
+        let amount: U256 = handle.context().apparent_value;
+        let call = RuntimeCall::SubtensorModule(pallet_subtensor::Call::<Runtime>::add_stake {
+            hotkey,
+            amount_staked: amount.as_u64(),
+        });
+
+        Self::dispatch(handle, call)
+    }
+
+    fn remove_stake(handle: &mut impl PrecompileHandle, data: &[u8]) -> PrecompileResult {
+        let hotkey = Self::parse_hotkey(data)?.into();
+        let amount = U256::from_big_endian(&data[32..40]).as_u64(); // Assuming the next 8 bytes represent the amount
+        let call = RuntimeCall::SubtensorModule(pallet_subtensor::Call::<Runtime>::remove_stake {
+            hotkey,
+            amount_unstaked: amount,
+        });
+        Self::dispatch(handle, call)
+    }
+
+    fn parse_hotkey(data: &[u8]) -> Result<[u8; 32], PrecompileFailure> {
+        if data.len() < 32 {
+            return Err(PrecompileFailure::Error {
+                exit_status: ExitError::InvalidRange,
+            });
+        }
+        let mut hotkey = [0u8; 32];
+        hotkey.copy_from_slice(&data[0..32]);
+        Ok(hotkey)
+    }
+
+    fn dispatch(handle: &mut impl PrecompileHandle, call: RuntimeCall) -> PrecompileResult {
+        let account_id = <HashedAddressMapping<BlakeTwo256> as AddressMapping<AccountId32>>::into_account_id(handle.context().caller);
+        log::info!("Mapped account_id: {:?}", account_id);
+        //let account_id = bytes_to_account_id(&address_bytes_src)?;
+        let result = call.dispatch(RawOrigin::Signed(account_id).into());
+        match result {
+            Ok(_) => Ok(PrecompileOutput {
+                exit_status: ExitSucceed::Returned,
+                output: vec![],
+            }),
+            Err(_) => Err(PrecompileFailure::Error {
+                exit_status: ExitError::Other("Subtensor call failed".into()),
+            }),
+        }
+    }
+}
