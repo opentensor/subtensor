d8c63b14842da7e4658656f617198c9752f8ffc9
open-junius <zhou@opentensor.dev>
2024-09-06 11:18:51 +0800

    fix lint in test files

--- pallets/subtensor/tests/emission.rs
+++ pallets/subtensor/tests/emission.rs
@@ -1,3 +1,9 @@
+#![allow(
+    clippy::arithmetic_side_effects,
+    clippy::unwrap_used,
+    clippy::indexing_slicing
+)]
+
 mod mock;
 use crate::mock::*;
 use pallet_subtensor::*;
--- pallets/subtensor/tests/lock.rs
+++ pallets/subtensor/tests/lock.rs
@@ -1,3 +1,8 @@
+#![allow(
+    clippy::arithmetic_side_effects,
+    clippy::unwrap_used,
+    clippy::indexing_slicing
+)]
 mod mock;
 use frame_support::{assert_noop, assert_ok};
 use mock::*;
