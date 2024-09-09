c9bfdd24534560ddac308822991bd5522ed56ad7
Sam Johnson <sam@durosoft.com>
2024-09-09 17:48:42 -0400

    branch selection, fallible eval method

--- scripts/release_notes.rs
+++ scripts/release_notes.rs
@@ -4,13 +4,23 @@
 use core::{fmt::Display, str::FromStr};
 use std::{env, process::Command};
 
-fn eval(cmd: impl Display) -> String {
+fn eval(cmd: impl Display, print: bool) -> Result<String, String> {
+    if print {
+        println!("$ {}", cmd);
+    }
     let output = Command::new("sh")
         .arg("-c")
         .arg(cmd.to_string())
         .output()
         .expect("failed to execute process");
-    String::from_utf8(output.stdout).unwrap().trim().to_string()
+    if print {
+        println!("{}", String::from_utf8(output.stdout.clone()).unwrap());
+        eprintln!("{}", String::from_utf8(output.stderr.clone()).unwrap());
+    }
+    if !output.status.success() {
+        return Err(String::from_utf8(output.stderr).unwrap());
+    }
+    Ok(String::from_utf8(output.stdout).unwrap().trim().to_string())
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
@@ -39,7 +49,7 @@ fn main() {
     println!("Network: {:?}", network);
 
     let all_tags = env::var("PREVIOUS_TAG")
-        .unwrap_or_else(|_| eval("git tag --sort=-creatordate"))
+        .unwrap_or_else(|_| eval("git tag --sort=-creatordate", false).unwrap())
         .split("\n")
         .map(|s| s.trim().to_string())
         .collect::<Vec<String>>();
@@ -56,16 +66,29 @@ fn main() {
     };
     println!("Previous release tag: {}", previous_tag);
 
+    let branch = env::var("BRANCH").unwrap_or(
+        match network {
+            Network::Mainnet => "main",
+            Network::Testnet => "testnet",
+        }
+        .to_string(),
+    );
+    println!("Branch: {}", branch);
+    eval(format!("git checkout {}", branch), true).unwrap();
+
     println!("");
     println!(
         "Generating release notes for all merges since {}...",
         previous_tag,
     );
-
-    let merges = eval(format!(
-        "git log --merges --pretty=format:'%s' {}..HEAD",
-        previous_tag
-    ))
+    let merges = eval(
+        format!(
+            "git log --merges --pretty=format:'%s' {}..HEAD",
+            previous_tag
+        ),
+        false,
+    )
+    .unwrap()
     .split("\n")
     .map(|s| s.trim().to_string())
     .filter(|s| {
