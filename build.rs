90ba835222708d5829bb9541e401754678976566
Sam Johnson <sam@durosoft.com>
2024-09-10 17:43:14 -0400

    fix workspace lint error

--- build.rs
+++ build.rs
@@ -75,7 +75,9 @@ fn collect_rust_files(dir: &Path) -> Vec<PathBuf> {
     let mut rust_files = Vec::new();
 
     for entry in WalkDir::new(dir) {
-        let entry = entry.unwrap();
+        let Ok(entry) = entry else {
+            continue;
+        };
         let path = entry.path();
 
         // Skip any path that contains "target" directory
