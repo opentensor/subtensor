1c234dd2bae17d53964e1aa7e190f00289602902
Sam Johnson <sam@durosoft.com>
2024-10-01 16:01:21 -0400

    revert

--- examples/basic/src/lib.rs
+++
@@ -1,4 +0,0 @@
-#![doc(hidden)]
-
-// this crate is simply a stub to get the `procedural-fork` tests to pass since they expect a
-// valid `Cargo.toml` at this path with `frame-system` and `frame-support` as dependencies.
