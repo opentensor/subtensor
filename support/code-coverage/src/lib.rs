43e343bc24f77884badacbf83cb6c23b6b45f409
Sam Johnson <sam@durosoft.com>
2024-10-02 00:50:49 -0400

    document a path forward for resolving #[import_section(..)]

--- support/code-coverage/src/lib.rs
+++ support/code-coverage/src/lib.rs
@@ -1,5 +1,6 @@
 use proc_macro2::TokenStream as TokenStream2;
 use procedural_fork::{exports::pallet::parse::Def, simulate_manifest_dir};
+use quote::ToTokens;
 use std::{
     collections::HashMap,
     fs,
@@ -14,7 +15,7 @@ pub struct PalletCoverageInfo {
     pub extrinsics: HashMap<String, usize>,
 }
 
-pub fn try_parse_pallet(item_mod: &ItemMod) -> Option<Def> {
+pub fn try_parse_pallet(item_mod: &ItemMod, file_path: &Path) -> Option<Def> {
     simulate_manifest_dir("pallets/subtensor", || -> Option<Def> {
         if item_mod.content.is_none() || item_mod.ident != "pallet" {
             build_print::info!(
@@ -22,6 +23,17 @@ pub fn try_parse_pallet(item_mod: &ItemMod) -> Option<Def> {
                 item_mod.ident.to_string()
             );
             return None;
+        }
+        let item_mod = item_mod.clone();
+        // manually import foreign sections defined by the `#[import_section]` attribute
+        for attr in item_mod.attrs.iter() {
+            if attr.meta.path().segments.last().unwrap().ident == "import_section" {
+                build_print::note!("Importing section: {}", attr.to_token_stream().to_string());
+                build_print::note!("path: {:?}", file_path);
+                // TODO: in parallel, recursively search all files in the parent dir of `file_path` until we
+                // find a module with a `#[pallet_section]` attribute whose name also matches
+                // that of the `#[import_section]` attribute
+            }
         }
         build_print::info!("Parsing module: {}", item_mod.ident.to_string());
         if let Ok(pallet) = Def::try_from(item_mod.clone(), false) {
@@ -51,7 +63,7 @@ pub fn analyze_file(path: &Path) -> Vec<PalletCoverageInfo> {
     };
     let mut infos = Vec::new();
     build_print::info!("Analyzing file: {}", path.display());
-    PalletVisitor::for_each_pallet(&file, |_item_mod, _pallet: &Def| {
+    PalletVisitor::for_each_pallet(&file, &path, |_item_mod, _pallet: &Def| {
         let mut info = PalletCoverageInfo::default();
         info.path = path.to_path_buf();
         infos.push(info);
@@ -62,14 +74,18 @@ pub fn analyze_file(path: &Path) -> Vec<PalletCoverageInfo> {
 #[derive(Default)]
 pub struct PalletVisitor {
     pub pallets: Vec<(ItemMod, Def)>,
+    pub file_path: PathBuf,
 }
 
 impl PalletVisitor {
-    pub fn for_each_pallet<F>(file: &File, mut f: F) -> Self
+    pub fn for_each_pallet<F>(file: &File, file_path: &Path, mut f: F) -> Self
     where
         F: FnMut(&ItemMod, &Def),
     {
-        let mut visitor = PalletVisitor::default();
+        let mut visitor = PalletVisitor {
+            pallets: Vec::new(),
+            file_path: file_path.to_path_buf(),
+        };
         visitor.visit_file(file);
         for (item_mod, pallet) in &visitor.pallets {
             f(item_mod, pallet);
@@ -80,7 +96,7 @@ impl PalletVisitor {
 
 impl<'ast> Visit<'ast> for PalletVisitor {
     fn visit_item_mod(&mut self, item_mod: &'ast ItemMod) {
-        if let Some(pallet) = try_parse_pallet(item_mod) {
+        if let Some(pallet) = try_parse_pallet(item_mod, &self.file_path) {
             self.pallets.push((item_mod.clone(), pallet));
         }
         syn::visit::visit_item_mod(self, item_mod);
