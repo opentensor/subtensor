b5f4b2773c6bcabff41b3ee48b628395e6009c97
open-junius <zhou@opentensor.dev>
2024-10-18 23:28:05 +0800

    fix clippy

--- node/src/chain_spec/localnet.rs
+++ node/src/chain_spec/localnet.rs
@@ -114,7 +114,6 @@ fn localnet_genesis(
             get_account_id_from_seed::<sr25519::Public>("Wendy"),
             1500000000000u128,
         ),
-        
     ];
 
     // Check if the environment variable is set
