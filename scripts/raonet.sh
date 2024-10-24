e6ee132d66e2a9b1de7f00c76677612c90ad4904
Cameron Fairchild <cameron@opentensor.ai>
2024-10-24 18:09:11 -0400

    oops

--- scripts/raonet.sh
+++ scripts/raonet.sh
@@ -48,7 +48,7 @@ fi
 
 if [[ $BUILD_BINARY == "1" ]]; then
   echo "*** Building substrate binary..."
-  cargo build --production --features "$FEATURES" --manifest-path "$BASE_DIR/Cargo.toml"
+  cargo build --profile production --features "$FEATURES" --manifest-path "$BASE_DIR/Cargo.toml"
   echo "*** Binary compiled"
 fi
 
