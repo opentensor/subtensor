1d6c938770529724c825d76678e445e6b79ab681
Julian Eager <eagr@tutanota.com>
2024-11-12 17:12:03 +0800

    allow Keys::<T>::remove() in replace_neuron()

--- support/linting/src/forbid_keys_remove.rs
+++ support/linting/src/forbid_keys_remove.rs
@@ -1,7 +1,7 @@
 use super::*;
 use syn::{
     punctuated::Punctuated, spanned::Spanned, token::Comma, visit::Visit, Expr, ExprCall, ExprPath,
-    File,
+    File, Path,
 };
 
 pub struct ForbidKeysRemoveCall;
@@ -26,25 +26,30 @@ struct KeysRemoveVisitor {
 
 impl<'ast> Visit<'ast> for KeysRemoveVisitor {
     fn visit_expr_call(&mut self, node: &'ast syn::ExprCall) {
-        let ExprCall { func, args, .. } = node;
-        if is_keys_remove_call(func, args) {
+        let ExprCall {
+            func, args, attrs, ..
+        } = node;
+
+        if is_keys_remove_call(func, args) && !is_allowed(attrs) {
             let msg = "Keys::<T>::remove()` is banned to prevent accidentally breaking \
-                the neuron sequence. If you need to replace neuron, try `SubtensorModule::replace_neuron()`";
+                the neuron sequence. If you need to replace neurons, try `SubtensorModule::replace_neuron()`";
             self.errors.push(syn::Error::new(node.func.span(), msg));
         }
     }
 }
 
 fn is_keys_remove_call(func: &Expr, args: &Punctuated<Expr, Comma>) -> bool {
-    let Expr::Path(ExprPath { path, .. }) = func else {
+    let Expr::Path(ExprPath {
+        path: Path { segments: func, .. },
+        ..
+    }) = func
+    else {
         return false;
     };
-    let func = &path.segments;
-    if func.len() != 2 || args.len() != 2 {
-        return false;
-    }
 
-    func[0].ident == "Keys"
+    func.len() == 2
+        && args.len() == 2
+        && func[0].ident == "Keys"
         && !func[0].arguments.is_none()
         && func[1].ident == "remove"
         && func[1].arguments.is_none()
@@ -89,6 +94,25 @@ mod tests {
         let input = r#"ParentKeys::remove(netuid, uid_to_replace)"#;
         assert!(lint(input).is_ok());
         let input = r#"ChildKeys::<T>::remove(netuid, uid_to_replace)"#;
+        assert!(lint(input).is_ok());
+    }
+
+    #[test]
+    fn test_keys_remove_allowed() {
+        let input = r#"
+        #[allow(unknown_lints)]
+        Keys::<T>::remove(netuid, uid_to_replace)
+        "#;
+        assert!(lint(input).is_ok());
+        let input = r#"
+        #[allow(unknown_lints)]
+        Keys::<U>::remove(netuid, uid_to_replace)
+        "#;
+        assert!(lint(input).is_ok());
+        let input = r#"
+        #[allow(unknown_lints)]
+        Keys::<U>::remove(1, "2".parse().unwrap(),)
+        "#;
         assert!(lint(input).is_ok());
     }
 }
