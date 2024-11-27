4640bf815e5e3814d4a8bd44f5155af4501a5b94
Aliaksandr Tsurko <ales@opentensor.dev>
2024-11-27 16:33:04 +0100

    Fix build_all_chainspecs rewrite plain specs genesis

--- scripts/build_all_chainspecs.sh
+++ scripts/build_all_chainspecs.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+# The genesis is not allowed to change. Since the wasm genesis will change
+# depending on the system architecture used, we need to extract the genesis from
+# the old chain specs and insert them into the new chain specs to ensure there
+# are no genesis mismatch issues.
+
+# This script updates the chain spec files keeping the genesis unchanged.
+
 set -e
 
 raw_finney="chainspecs/raw_spec_finney.json"
-raw_testfinney="chainspecs/raw_spec_testfinney.json"
 plain_finney="chainspecs/plain_spec_finney.json"
+raw_testfinney="chainspecs/raw_spec_testfinney.json"
 plain_testfinney="chainspecs/plain_spec_testfinney.json"
-
-finney_genesis_temp=$(mktemp)
-testfinney_genesis_temp=$(mktemp)
-raw_spec_finney_temp=$(mktemp)
-raw_spec_testfinney_temp=$(mktemp)
 
 save_genesis() {
   jq -r ".genesis" "$1" >"$2"
@@ -28,41 +30,43 @@ update_genesis() {
   jq --slurpfile genesis "$1" '.genesis = $genesis[0]' "$2" >"$3"
 }
 
-cleanup() {
-  rm -f "$finney_genesis_temp" \
-    "$testfinney_genesis_temp" \
-    "$raw_spec_finney_temp" \
-    "$raw_spec_testfinney_temp"
+update_spec() {
+  local chain="$1"
+  local raw_path="$2"
+  local plain_path="$3"
+
+  raw_genesis_temp=$(mktemp)
+  plain_genesis_temp=$(mktemp)
+  raw_spec_temp=$(mktemp)
+  plain_spec_temp=$(mktemp)
+
+  echo "*** Backing up genesis for '$chain'..."
+
+  save_genesis "$raw_path" "$raw_genesis_temp"
+  save_genesis "$plain_path" "$plain_genesis_temp"
+
+  echo "*** Building new chainspec for '$chain'..."
+
+  # Build new chainspecs
+  buildspec "$chain" >"$plain_spec_temp"
+  buildspec "$chain" --raw >"$raw_spec_temp"
+
+  echo "*** Restoring genesis in '$chain'..."
+
+  update_genesis "$raw_genesis_temp" "$raw_spec_temp" "$raw_path"
+  update_genesis "$plain_genesis_temp" "$plain_spec_temp" "$plain_path"
+
+  # cleanup
+  rm -f "$raw_genesis_temp" "$plain_genesis_temp" "$raw_spec_temp" \
+    "$plain_spec_temp"
 }
 
 # SCRIPT
 
-echo "*** Building new chainspecs..."
-
 echo "*** Building node..."
 cargo build -p node-subtensor
 
-# Save old genesis state before doing anything
-save_genesis "$raw_finney" "$finney_genesis_temp"
-save_genesis "$raw_testfinney" "$testfinney_genesis_temp"
-
-# Build new chainspecs
-buildspec finney --raw >"$raw_spec_finney_temp"
-buildspec finney >"$plain_finney"
-
-buildspec test_finney --raw  >"$raw_spec_testfinney_temp"
-buildspec test_finney >"$plain_testfinney"
-
-echo "*** Updating genesis..."
-
-# The genesis is not allowed to change. Since the wasm genesis will change
-# depending on the system architecture used, we need to extract the genesis from
-# the old chain specs and insert them into the new chain specs to ensure there
-# are no genesis mismatch issues.
-update_genesis "$finney_genesis_temp" "$raw_spec_finney_temp" "$raw_finney"
-update_genesis "$testfinney_genesis_temp" "$raw_spec_testfinney_temp" \
-  "$raw_testfinney"
-
-cleanup
+update_spec finney "$raw_finney" "$plain_finney"
+update_spec test_finney "$raw_testfinney" "$plain_testfinney"
 
 echo "*** Done!"
