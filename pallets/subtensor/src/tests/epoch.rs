3e013c8aa8a470221d79c09313f46a62bfc3dabd
Aliaksandr Tsurko <ales@opentensor.dev>
2024-12-17 20:01:26 +0100

    Remove get_subnet_emission_value from pallet-subtensor::Pallet

--- pallets/subtensor/src/tests/epoch.rs
+++ pallets/subtensor/src/tests/epoch.rs
@@ -564,10 +564,7 @@ fn test_1_graph() {
         // SubtensorModule::set_weights_for_testing( netuid, i as u16, vec![ ( 0, u16::MAX )]); // doesn't set update status
         // SubtensorModule::set_bonds_for_testing( netuid, uid, vec![ ( 0, u16::MAX )]); // rather, bonds are calculated in epoch
         SubtensorModule::set_emission_values(&[netuid], vec![1_000_000_000]).unwrap();
-        assert_eq!(
-            SubtensorModule::get_subnet_emission_value(netuid),
-            1_000_000_000
-        );
+        assert_eq!(EmissionValues::<Test>::get(netuid), 1_000_000_000);
         SubtensorModule::epoch(netuid, 1_000_000_000);
         assert_eq!(
             SubtensorModule::get_total_stake_for_hotkey(&hotkey),
