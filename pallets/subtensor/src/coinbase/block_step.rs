372ddc6b9adfd0a2b66f3feced4dc2ced4c89b42
Aliaksandr Tsurko <ales@opentensor.dev>
2024-12-19 18:01:35 +0100

    Remove set_last_adjustment_block from pallet-subtensor::Pallet

--- pallets/subtensor/src/coinbase/block_step.rs
+++ pallets/subtensor/src/coinbase/block_step.rs
@@ -160,7 +160,7 @@ impl<T: Config + pallet_drand::Config> Pallet<T> {
                 }
 
                 // --- 6. Drain all counters for this network for this interval.
-                Self::set_last_adjustment_block(netuid, current_block);
+                LastAdjustmentBlock::<T>::insert(netuid, current_block);
                 Self::set_registrations_this_interval(netuid, 0);
                 Self::set_pow_registrations_this_interval(netuid, 0);
                 BurnRegistrationsThisInterval::<T>::insert(netuid, 0);
