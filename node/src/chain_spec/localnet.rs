e76519b1204be21e40fec7bec3c7d9d69d509c5a
Aliaksandr Tsurko <ales@opentensor.dev>
2025-01-06 17:02:06 +0100

    Comment eth addresses for alith and baltathar

--- node/src/chain_spec/localnet.rs
+++ node/src/chain_spec/localnet.rs
@@ -79,12 +79,12 @@ fn localnet_genesis(
         ),
         // ETH
         (
-            // Alith
+            // Alith - 0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac
             AccountId::from_str("5Fghzk1AJt88PeFEzuRfXzbPchiBbsVGTTXcdx599VdZzkTA").unwrap(),
             2000000000000u128,
         ),
         (
-            // Baltathar
+            // Baltathar - 0x3Cd0A705a2DC65e5b1E1205896BaA2be8A07c6e0
             AccountId::from_str("5GeqNhKWj1KG78VHzbmo3ZjZgUTrCuWeamdgiA114XHGdaEr").unwrap(),
             2000000000000u128,
         ),
