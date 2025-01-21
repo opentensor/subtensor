8144ed0bc77c1da70aa6a1daf8462bd7a0b811ab
open-junius <zhou@opentensor.dev>
2025-01-21 23:55:50 +0800

    update neuron precompile

--- runtime/src/precompiles/neuron.rs
+++ runtime/src/precompiles/neuron.rs
@@ -23,6 +23,23 @@ impl NeuronPrecompile {
             id if id == get_method_id("burnedRegister(uint16,bytes32)") => {
                 Self::burned_register(handle, &method_input)
             }
+            id if id
+                == get_method_id(
+                    "serveAxon(uint16,uint32,uint128,uint16,uint8,uint8,uint8,uint8)",
+                ) =>
+            {
+                Self::serve_axon(handle, &method_input)
+            }
+            id if id
+                == get_method_id(
+                    "serveAxonTls(uint16,uint32,uint128,uint16,uint8,uint8,uint8,uint8,bytes)",
+                ) =>
+            {
+                Self::serve_axon_tls(handle, &method_input)
+            }
+            id if id == get_method_id("servePrometheus(uint16,uint32,uint128,uint16,uint8)") => {
+                Self::serve_prometheus(handle, &method_input)
+            }
 
             _ => Err(PrecompileFailure::Error {
                 exit_status: ExitError::InvalidRange,
@@ -36,6 +53,53 @@ impl NeuronPrecompile {
             RuntimeCall::SubtensorModule(pallet_subtensor::Call::<Runtime>::burned_register {
                 netuid,
                 hotkey: hotkey.into(),
+            });
+        dispatch(handle, call, NEURON_CONTRACT_ADDRESS)
+    }
+
+    pub fn serve_axon(handle: &mut impl PrecompileHandle, data: &[u8]) -> PrecompileResult {
+        let (netuid, version, ip, port, ip_type, protocol, placeholder1, placeholder2) =
+            Self::parse_serve_axon_parameters(data)?;
+        let call = RuntimeCall::SubtensorModule(pallet_subtensor::Call::<Runtime>::serve_axon {
+            netuid,
+            version,
+            ip,
+            port,
+            ip_type,
+            protocol,
+            placeholder1,
+            placeholder2,
+        });
+        dispatch(handle, call, NEURON_CONTRACT_ADDRESS)
+    }
+
+    pub fn serve_axon_tls(handle: &mut impl PrecompileHandle, data: &[u8]) -> PrecompileResult {
+        let (netuid, version, ip, port, ip_type, protocol, placeholder1, placeholder2, certificate) =
+            Self::parse_serve_axon_tls_parameters(data)?;
+        let call =
+            RuntimeCall::SubtensorModule(pallet_subtensor::Call::<Runtime>::serve_axon_tls {
+                netuid,
+                version,
+                ip,
+                port,
+                ip_type,
+                protocol,
+                placeholder1,
+                placeholder2,
+                certificate,
+            });
+        dispatch(handle, call, NEURON_CONTRACT_ADDRESS)
+    }
+
+    pub fn serve_prometheus(handle: &mut impl PrecompileHandle, data: &[u8]) -> PrecompileResult {
+        let (netuid, version, ip, port, ip_type) = Self::parse_serve_prometheus_parameters(data)?;
+        let call =
+            RuntimeCall::SubtensorModule(pallet_subtensor::Call::<Runtime>::serve_prometheus {
+                netuid,
+                version,
+                ip,
+                port,
+                ip_type,
             });
         dispatch(handle, call, NEURON_CONTRACT_ADDRESS)
     }
@@ -54,5 +118,114 @@ impl NeuronPrecompile {
         parameter.copy_from_slice(get_slice(data, 32, 64)?);
 
         Ok((netuid, parameter))
+    }
+
+    fn parse_serve_axon_parameters(
+        data: &[u8],
+    ) -> Result<(u16, u32, u128, u16, u8, u8, u8, u8), PrecompileFailure> {
+        if data.len() < 256 {
+            return Err(PrecompileFailure::Error {
+                exit_status: ExitError::InvalidRange,
+            });
+        }
+        let mut netuid_vec = [0u8; 2];
+        netuid_vec.copy_from_slice(get_slice(data, 30, 32)?);
+        let netuid = u16::from_be_bytes(netuid_vec);
+
+        let mut version_vec = [0u8; 4];
+        version_vec.copy_from_slice(get_slice(data, 60, 64)?);
+        let version = u32::from_be_bytes(version_vec);
+
+        let mut ip_vec = [0u8; 16];
+        ip_vec.copy_from_slice(get_slice(data, 80, 96)?);
+        let ip = u128::from_be_bytes(ip_vec);
+
+        let mut port_vec = [0u8; 2];
+        port_vec.copy_from_slice(get_slice(data, 126, 128)?);
+        let port = u16::from_be_bytes(port_vec);
+
+        let ip_type = data[160];
+        let protocol = data[192];
+        let placeholder1 = data[224];
+        let placeholder2 = data[256];
+        Ok((
+            netuid,
+            version,
+            ip,
+            port,
+            ip_type,
+            protocol,
+            placeholder1,
+            placeholder2,
+        ))
+    }
+
+    fn parse_serve_axon_tls_parameters(
+        data: &[u8],
+    ) -> Result<(u16, u32, u128, u16, u8, u8, u8, u8, vec::Vec<u8>), PrecompileFailure> {
+        if data.len() < 256 {
+            return Err(PrecompileFailure::Error {
+                exit_status: ExitError::InvalidRange,
+            });
+        }
+        let mut netuid_vec = [0u8; 2];
+        netuid_vec.copy_from_slice(get_slice(data, 30, 32)?);
+        let netuid = u16::from_be_bytes(netuid_vec);
+
+        let mut version_vec = [0u8; 4];
+        version_vec.copy_from_slice(get_slice(data, 60, 64)?);
+        let version = u32::from_be_bytes(version_vec);
+
+        let mut ip_vec = [0u8; 16];
+        ip_vec.copy_from_slice(get_slice(data, 80, 96)?);
+        let ip = u128::from_be_bytes(ip_vec);
+
+        let mut port_vec = [0u8; 2];
+        port_vec.copy_from_slice(get_slice(data, 126, 128)?);
+        let port = u16::from_be_bytes(port_vec);
+
+        let ip_type = data[160];
+        let protocol = data[192];
+        let placeholder1 = data[224];
+        let placeholder2 = data[256];
+        Ok((
+            netuid,
+            version,
+            ip,
+            port,
+            ip_type,
+            protocol,
+            placeholder1,
+            placeholder2,
+            vec![],
+        ))
+    }
+
+    fn parse_serve_prometheus_parameters(
+        data: &[u8],
+    ) -> Result<(u16, u32, u128, u16, u8), PrecompileFailure> {
+        if data.len() < 160 {
+            return Err(PrecompileFailure::Error {
+                exit_status: ExitError::InvalidRange,
+            });
+        }
+        let mut netuid_vec = [0u8; 2];
+        netuid_vec.copy_from_slice(get_slice(data, 30, 32)?);
+        let netuid = u16::from_be_bytes(netuid_vec);
+
+        let mut version_vec = [0u8; 4];
+        version_vec.copy_from_slice(get_slice(data, 60, 64)?);
+        let version = u32::from_be_bytes(version_vec);
+
+        let mut ip_vec = [0u8; 16];
+        ip_vec.copy_from_slice(get_slice(data, 80, 96)?);
+        let ip = u128::from_be_bytes(ip_vec);
+
+        let mut port_vec = [0u8; 2];
+        port_vec.copy_from_slice(get_slice(data, 126, 128)?);
+        let port = u16::from_be_bytes(port_vec);
+
+        let ip_type = data[160];
+        Ok((netuid, version, ip, port, ip_type))
     }
 }
