fba58909afc2d4a3ea62b8007245faa18d8aea23
Keith <keith@opentensor.dev>
2025-02-05 21:56:41 +0800

    Complete migration of RAO

--- pallets/subtensor/src/migrations/multiblock/migrate_rao.rs
+++ pallets/subtensor/src/migrations/multiblock/migrate_rao.rs
@@ -21,8 +21,8 @@ pub mod migrate_rao {
     use super::*;
     use codec::MaxEncodedLen;
     use frame_support::{
-        migrations::{MigrationId, SteppedMigration, SteppedMigrationError},
-        traits::{Get, GetStorageVersion},
+        migrations::{SteppedMigration, SteppedMigrationError},
+        traits::Get,
         weights::{Weight, WeightMeter},
     };
     use substrate_fixed::types::U64F64;
@@ -32,7 +32,7 @@ pub mod migrate_rao {
         DynamicBlockSet,
         Stake(S),
         FinishedStake,
-        ConvertSubnets(C),
+        ConvertSubnet(C),
         Finished,
     }
 
@@ -40,21 +40,26 @@ pub mod migrate_rao {
 
     impl<T: Config> SteppedMigration for Migration<T> {
         type Cursor = MigrationState<(T::AccountId, T::AccountId), u16>;
-        type Identifier = MigrationId<16>;
+        type Identifier = [u8; 11];
 
         fn id() -> Self::Identifier {
-            MigrationId { pallet_id: *PALLET_MIGRATIONS_ID, version_from: 6, version_to: 7 }
+            *b"migrate_rao"
         }
 
         fn max_steps() -> Option<u32> {
-            Some(10) // TODO: Make sure to change this to something that makes sense
+            Some(32) // TODO: Make sure to change this to something that makes sense
         }
 
         fn step(
             mut cursor: Option<Self::Cursor>,
             meter: &mut WeightMeter,
         ) -> Result<Option<Self::Cursor>, SteppedMigrationError> {
-            if Pallet::<T>::on_chain_storage_version() != Self::id().version_from as u16 {
+            let migration_name = Self::id();
+            if HasMigrationRun::<T>::get(migration_name.as_ref()) {
+                log::info!(
+                    "Migration '{:?}' has already run. Skipping.",
+                    String::from_utf8_lossy(migration_name.as_ref())
+                );
                 return Ok(None);
             }
 
@@ -65,7 +70,20 @@ pub mod migrate_rao {
                     None => Self::dynamic_block_step(),
                     Some(MigrationState::DynamicBlockSet) => Self::stake_step(None),
                     Some(MigrationState::Stake(key)) => Self::stake_step(Some(key)),
-                    _ => todo!(),
+                    Some(MigrationState::FinishedStake) => {
+                        TaoWeight::<T>::set(332_041_393_326_771_929);
+                        Self::convert_subnet_step(None)
+                    }
+                    Some(MigrationState::ConvertSubnet(key)) => Self::convert_subnet_step(Some(key)),
+                    Some(MigrationState::Finished) => {
+                        HasMigrationRun::<T>::insert(migration_name.as_ref(), true);
+
+                        log::info!(
+                            "Migration '{:?}' completed.",
+                            String::from_utf8_lossy(migration_name.as_ref())
+                        );
+                        return Ok(None);
+                    }
                 };
 
                 cursor = Some(next);
@@ -77,11 +95,15 @@ pub mod migrate_rao {
     }
 
     impl<T: Config> Migration<T> {
-        fn required_weight(step: Option<&MigrationState<(T::AccountId, T::AccountId), u16>>) -> Weight {
+        fn required_weight(
+            step: Option<&MigrationState<(T::AccountId, T::AccountId), u16>>,
+        ) -> Weight {
             match step {
                 None => T::DbWeight::get().writes(1),
                 Some(MigrationState::DynamicBlockSet | MigrationState::Stake(_)) => T::DbWeight::get().reads_writes(6, 6),
-                _ => todo!(),
+                Some(MigrationState::FinishedStake) => T::DbWeight::get().reads_writes(1, 4),
+                Some(MigrationState::ConvertSubnet(_)) => T::DbWeight::get().reads_writes(22, 30),
+                Some(MigrationState::Finished) => T::DbWeight::get().writes(1),
             }
         }
 
@@ -90,7 +112,9 @@ pub mod migrate_rao {
             MigrationState::DynamicBlockSet
         }
 
-        fn stake_step(maybe_last_key: Option<&(T::AccountId, T::AccountId)>) -> MigrationState<(T::AccountId, T::AccountId), u16> {
+        fn stake_step(
+            maybe_last_key: Option<&(T::AccountId, T::AccountId)>,
+        ) -> MigrationState<(T::AccountId, T::AccountId), u16> {
             let mut iter = if let Some((last_key1, last_key2)) = maybe_last_key {
                 deprecated_stake_format::Stake::<T>::iter_from(
                     deprecated_stake_format::Stake::<T>::hashed_key_for(last_key1, last_key2)
@@ -123,6 +147,68 @@ pub mod migrate_rao {
                 MigrationState::Stake((hotkey, coldkey))
             } else {
                 MigrationState::FinishedStake
+            }
+        }
+
+        fn convert_subnet_step(
+            maybe_last_key: Option<&u16>,
+        ) -> MigrationState<(T::AccountId, T::AccountId), u16> {
+            let mut iter = if let Some(last_key) = maybe_last_key {
+                NetworksAdded::<T>::iter_from(NetworksAdded::<T>::hashed_key_for(last_key))
+            } else {
+                NetworksAdded::<T>::iter()
+            };
+            
+            if let Some((netuid, _)) = iter.next() {
+                if netuid == 0 {
+                    // Give root a single RAO in pool to avoid any catestrophic division by zero.
+                    SubnetAlphaIn::<T>::insert(netuid, 1);
+                    SubnetMechanism::<T>::insert(netuid, 0); // Set to zero mechanism.
+                    TokenSymbol::<T>::insert(netuid, Pallet::<T>::get_symbol_for_subnet(0));
+                    return MigrationState::ConvertSubnet(0);
+                }
+                let owner: T::AccountId = SubnetOwner::<T>::get(netuid);
+                let lock: u64 = SubnetLocked::<T>::get(netuid);
+                let initial_liquidity: u64 = 100_000_000_000; // 100 TAO.
+                let remaining_lock: u64 = lock.saturating_sub(initial_liquidity);
+                Pallet::<T>::add_balance_to_coldkey_account(&owner, remaining_lock);
+                SubnetTAO::<T>::insert(netuid, initial_liquidity); // Set TAO to the lock.
+                SubnetAlphaIn::<T>::insert(netuid, initial_liquidity); // Set AlphaIn to the initial alpha distribution.
+                SubnetAlphaOut::<T>::insert(netuid, 0); // Set zero subnet alpha out.
+                SubnetMechanism::<T>::insert(netuid, 1); // Convert to dynamic immediately with initialization.
+                Tempo::<T>::insert(netuid, DefaultTempo::<T>::get());
+                // Set the token symbol for this subnet using Self instead of Pallet::<T>
+                TokenSymbol::<T>::insert(netuid, Pallet::<T>::get_symbol_for_subnet(netuid));
+                SubnetTAO::<T>::insert(netuid, initial_liquidity); // Set TAO to the lock.
+                TotalStakeAtDynamic::<T>::insert(netuid, 0);
+        
+                if let Ok(owner_coldkey) = SubnetOwner::<T>::try_get(netuid) {
+                    // Set Owner as the coldkey.
+                    SubnetOwnerHotkey::<T>::insert(netuid, owner_coldkey.clone());
+                    // Associate the coldkey to coldkey.
+                    Pallet::<T>::create_account_if_non_existent(&owner_coldkey, &owner_coldkey);
+                    // Register the owner_coldkey as neuron to the network.
+                    let _neuron_uid: u16 = Pallet::<T>::register_neuron(netuid, &owner_coldkey);
+                    // Register the neuron immediately.
+                    if !Identities::<T>::contains_key(owner_coldkey.clone()) {
+                        // Set the identitiy for the Owner coldkey if non existent.
+                        let identity = ChainIdentityOf {
+                            name: format!("Owner{}", netuid).as_bytes().to_vec(),
+                            url: Vec::new(),
+                            image: Vec::new(),
+                            discord: Vec::new(),
+                            description: Vec::new(),
+                            additional: Vec::new(),
+                        };
+                        // Validate the created identity and set it.
+                        if Pallet::<T>::is_valid_identity(&identity) {
+                            Identities::<T>::insert(owner_coldkey.clone(), identity.clone());
+                        }
+                    }
+                }
+                MigrationState::ConvertSubnet(netuid)
+            } else {
+                MigrationState::Finished
             }
         }
     }
