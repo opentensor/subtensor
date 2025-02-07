f65df97cac5150935175dcd3a08ace86b64da671
Keith <keith@opentensor.dev>
2025-02-07 23:52:37 +0800

    Fixes

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -387,7 +387,10 @@ parameter_types! {
 
 impl pallet_migrations::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
+    #[cfg(not(feature = "runtime-benchmarks"))]
     type Migrations = (); // Add to this tuple to schedule migrations for the next runtime upgrade
+    #[cfg(feature = "runtime-benchmarks")]
+    type Migrations = pallet_migrations::mock_helpers::MockedMigrations;
     type CursorMaxLen = ConstU32<65_536>;
     type IdentifierMaxLen = ConstU32<256>;
     type MigrationStatusHandler = ();
