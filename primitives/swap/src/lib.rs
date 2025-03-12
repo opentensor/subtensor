52c2ef3628cccc01fc99a1fcaab89b168ff95b7f
Aliaksandr Tsurko <ales@opentensor.dev>
2025-03-12 17:06:34 +0100

    Refactor safe-math

--- primitives/swap/src/lib.rs
+++ primitives/swap/src/lib.rs
@@ -1,19 +1,21 @@
 use safe_math::*;
+use sp_arithmetic::traits::UniqueSaturatedInto;
+use substrate_fixed::types::U64F64;
 
 /// The width of a single price tick. Expressed in rao units.
 pub const TICK_SPACING: u64 = 10_000;
 
-/// Position designates one liquidity position. 
-/// 
-/// Alpha price is expressed in rao units per one 10^9 unit. For example, 
+/// Position designates one liquidity position.
+///
+/// Alpha price is expressed in rao units per one 10^9 unit. For example,
 /// price 1_000_000 is equal to 0.001 TAO per Alpha.
-/// 
+///
 /// tick_low - tick index for lower boundary of price
 /// tick_high - tick index for higher boundary of price
 /// liquidity - position liquidity
 /// fees_tao - fees accrued by the position in quote currency (TAO)
 /// fees_alpha - fees accrued by the position in base currency (Alpha)
-/// 
+///
 struct Position {
     tick_low: u64,
     tick_high: u64,
@@ -24,72 +26,76 @@ struct Position {
 
 impl Position {
     /// Converts tick index into SQRT of price
-    /// 
-    /// python: (1 + self.tick_spacing) ** (i / 2)
-    /// 
     pub fn tick_index_to_sqrt_price(tick_idx: u64) -> U64F64 {
-        // TODO: implement
+        // python: (1 + self.tick_spacing) ** (i / 2)
+        let tick_spacing_tao = U64F64::from_num(TICK_SPACING).saturating_div(U64F64::from_num(1e9))
+            + U64F64::from_num(1.0);
+
+        tick_spacing_tao
+            .checked_pow(tick_idx / 2)
+            .unwrap_or_default()
     }
 
     /// Converts SQRT price to tick index
-    /// 
-    /// python: math.floor(math.log(sqrt_p) / math.log(1 + self.tick_spacing)) * 2
-    /// 
     pub fn sqrt_price_to_tick_index(sqrt_price: U64F64) -> u64 {
-        // TODO: implement
+        let tick_spacing_tao = U64F64::from_num(TICK_SPACING).saturating_div(U64F64::from_num(1e9))
+            + U64F64::from_num(1.0);
+        // python: math.floor(math.log(sqrt_p) / math.log(1 + self.tick_spacing)) * 2
+        todo!()
     }
 
     /// Converts position to token amounts
-    /// 
+    ///
     /// returns tuple of (TAO, Alpha)
-    /// 
+    ///
     pub fn to_token_amounts(self, current_tick: u64) -> (u64, u64) {
         let one = U64F64::saturating_from_num(1);
 
-        let sqrt_price_curr = self.tick_index_to_sqrt_price(current_tick);
-        let sqrt_pa = self.tick_index_to_sqrt_price(self.tick_low);
-        let sqrt_pb = self.tick_index_to_sqrt_price(self.tick_high);
+        let sqrt_price_curr = Self::tick_index_to_sqrt_price(current_tick);
+        let sqrt_pa = Self::tick_index_to_sqrt_price(self.tick_low);
+        let sqrt_pb = Self::tick_index_to_sqrt_price(self.tick_high);
 
         if sqrt_price_curr < sqrt_pa {
             (
-                liquidity.saturating_mul(one.safe_div(sqrt_pa).saturating_sub(one.safe_div(sqrt_pb))),
-                0
+                liquidity
+                    .saturating_mul(one.safe_div(sqrt_pa).saturating_sub(one.safe_div(sqrt_pb))),
+                0,
             )
         } else if sqrt_price_curr > sqrt_pb {
-            (
-                0,
-                liquidity.saturating_mul(sqrt_pb.saturating_sub(sqrt_pa))
-            )
+            (0, liquidity.saturating_mul(sqrt_pb.saturating_sub(sqrt_pa)))
         } else {
             (
-                liquidity.saturating_mul(one.save_div(sqrt_price_curr).saturating_sub(one.safe_div(sqrt_pb))),
-                liquidity.saturating_mul(sqrt_price_curr.saturating_sub(sqrt_pa))
+                liquidity.saturating_mul(
+                    one.save_div(sqrt_price_curr)
+                        .saturating_sub(one.safe_div(sqrt_pb)),
+                ),
+                liquidity.saturating_mul(sqrt_price_curr.saturating_sub(sqrt_pa)),
             )
         }
     }
 }
 
-/// Tick is the price range determined by tick index (not part of this struct, 
-/// but is the key at which the Tick is stored in state hash maps). Tick struct 
+/// Tick is the price range determined by tick index (not part of this struct,
+/// but is the key at which the Tick is stored in state hash maps). Tick struct
 /// stores liquidity and fee information.
-/// 
+///
 ///   - Net liquidity
 ///   - Gross liquidity
 ///   - Fees (above global) in both currencies
-/// 
+///
 struct Tick {
     liquidity_net: i128,
-    liquidity_gross: u64, 
+    liquidity_gross: u64,
     fees_out_tao: u64,
     fees_out_alpha: u64,
 }
 
-/// This trait implementation depends on Runtime and it needs to be implemented 
-/// in the pallet to be able to work with chain state and per subnet. All subnet 
+/// This trait implementation depends on Runtime and it needs to be implemented
+/// in the pallet to be able to work with chain state and per subnet. All subnet
 /// swaps are independent and hence netuid is abstracted away from swap implementation.
-/// 
+///
 pub trait SwapDataOperations {
-    /// Tells if v3 swap is initialized in the state. v2 only provides base and quote 
+    /// Tells if v3 swap is initialized in the state. v2 only provides base and quote
     /// reserves, while v3 also stores ticks and positions, which need to be initialized
     /// at the first pool creation.
     fn is_v3_initialized() -> bool;
@@ -108,9 +114,9 @@ pub trait SwapDataOperations {
     fn deposit_balances(tao: u64, alpha: u64);
 }
 
-/// All main swapping logic abstracted from Runtime implementation is concentrated 
+/// All main swapping logic abstracted from Runtime implementation is concentrated
 /// in this struct
-/// 
+///
 #[derive(Debug)]
 pub struct Swap<Ops>
 where
@@ -129,35 +135,29 @@ where
             // Set price, set initial (protocol owned) liquidity and positions, etc.
         }
 
-        Swap {
-            state_ops: ops,
-        }
+        Swap { state_ops: ops }
     }
 
-    /// Auxiliary method to calculate Alpha amount to match given TAO 
+    /// Auxiliary method to calculate Alpha amount to match given TAO
     /// amount at the current price for liquidity.
-    /// 
+    ///
     /// Returns (Alpha, Liquidity) tuple
-    /// 
+    ///
     pub fn get_tao_based_liquidity(tao: u64) -> (u64, u64) {
         // TODO
     }
 
-    /// Auxiliary method to calculate TAO amount to match given Alpha 
+    /// Auxiliary method to calculate TAO amount to match given Alpha
     /// amount at the current price for liquidity.
-    /// 
+    ///
     /// Returns (TAO, Liquidity) tuple
-    /// 
+    ///
     pub fn get_alpha_based_liquidity(alpha: u64) -> (u64, u64) {
         // TODO
     }
 
-    /// Add 
-    pub fn add_liquidity(
-        tick_low: u64,
-        tick_high: u64,
-        liquidity: u64
-    ) -> u64 {
+    /// Add
+    pub fn add_liquidity(tick_low: u64, tick_high: u64, liquidity: u64) -> u64 {
         // TODO
     }
 
