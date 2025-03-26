a77ea67126003a7593cbba724f12c2cab33fae9e
John Reed <87283488+JohnReedV@users.noreply.github.com>
2025-03-26 08:07:38 -0700

    add error UnexpectedUnreserveLeftover

--- pallets/commitments/src/lib.rs
+++ pallets/commitments/src/lib.rs
@@ -120,6 +120,8 @@ pub mod pallet {
         CommitmentSetRateLimitExceeded,
         /// Space Limit Exceeded for the current interval
         SpaceLimitExceeded,
+        /// Indicates that unreserve returned a leftover, which is unexpected.
+        UnexpectedUnreserveLeftover,
     }
 
     #[pallet::type_value]
@@ -264,9 +266,7 @@ pub mod pallet {
                 T::Currency::reserve(&who, id.deposit.saturating_sub(old_deposit))?;
             }
             if old_deposit > id.deposit {
-                let err_amount =
-                    T::Currency::unreserve(&who, old_deposit.saturating_sub(id.deposit));
-                debug_assert!(err_amount.is_zero());
+                return Err(Error::<T>::UnexpectedUnreserveLeftover.into());
             }
 
             <CommitmentOf<T>>::insert(netuid, &who, id);
