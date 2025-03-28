f5ddec8a6020063f0c30a20502e04f4ed74a086e
open-junius <zhou@opentensor.dev>
2025-03-28 21:09:33 +0800

    add test file

--- evm-tests/test/staking.precompile.stake-get.test.ts
+++ evm-tests/test/staking.precompile.stake-get.test.ts
@@ -5,59 +5,26 @@ import { TypedApi } from "polkadot-api";
 import { convertPublicKeyToSs58 } from "../src/address-utils"
 import { tao } from "../src/balance-math"
 import {
-    forceSetBalanceToSs58Address, addNewSubnetwork, burnedRegister,
-    setTxRateLimit, setTempo, setWeightsSetRateLimit, setSubnetOwnerCut, setMaxAllowedUids,
-    setMinDelegateTake, becomeDelegate, setActivityCutoff, addStake, setWeight, rootRegister
+    forceSetBalanceToSs58Address, addNewSubnetwork, addStake,
 } from "../src/subtensor"
-import { PublicClient } from "viem";
-import { generateRandomEthersWallet, getPublicClient } from "../src/utils"
-import { ISTAKING_ADDRESS, ISTAKING_V2_ADDRESS, IStakingABI, IStakingV2ABI } from "../src/contracts/staking"
-import { ETH_LOCAL_URL } from "../src/config";
+import { ethers } from "ethers";
+import { generateRandomEthersWallet } from "../src/utils"
+import { ISTAKING_V2_ADDRESS, IStakingV2ABI } from "../src/contracts/staking"
+import { log } from "console";
 
-describe("Test neuron precompile reveal weights", () => {
+describe("Test staking precompile get methods", () => {
     const hotkey = getRandomSubstrateKeypair();
     const coldkey = getRandomSubstrateKeypair();
-    let publicClient: PublicClient;
-
-    // const validator = getRandomSubstrateKeypair();
-    // const miner = getRandomSubstrateKeypair();
-    // const nominator = getRandomSubstrateKeypair();
+    const wallet1 = generateRandomEthersWallet();
 
     let api: TypedApi<typeof devnet>
 
     before(async () => {
-        const root_netuid = 0;
-        const root_tempo = 1; // neet root epoch to happen before subnet tempo
-        const subnet_tempo = 1;
-        publicClient = await getPublicClient(ETH_LOCAL_URL)
         api = await getDevnetApi()
-
-        // await forceSetBalanceToSs58Address(api, convertPublicKeyToSs58(alice.publicKey))
         await forceSetBalanceToSs58Address(api, convertPublicKeyToSs58(hotkey.publicKey))
         await forceSetBalanceToSs58Address(api, convertPublicKeyToSs58(coldkey.publicKey))
-        // await forceSetBalanceToSs58Address(api, convertPublicKeyToSs58(validator.publicKey))
-        // await forceSetBalanceToSs58Address(api, convertPublicKeyToSs58(miner.publicKey))
-        // await forceSetBalanceToSs58Address(api, convertPublicKeyToSs58(nominator.publicKey))
-        // await forceSetBalanceToEthAddress(api, wallet1.address)
-        // await forceSetBalanceToEthAddress(api, wallet2.address)
         let netuid = await addNewSubnetwork(api, hotkey, coldkey)
-
-        console.log("test the case on subnet ", netuid)
-
-        await setTxRateLimit(api, BigInt(0))
-        await setTempo(api, root_netuid, root_tempo)
-        await setTempo(api, netuid, subnet_tempo)
-        await setWeightsSetRateLimit(api, netuid, BigInt(0))
-
-        // await burnedRegister(api, netuid, convertPublicKeyToSs58(validator.publicKey), coldkey)
-        // await burnedRegister(api, netuid, convertPublicKeyToSs58(miner.publicKey), coldkey)
-        // await burnedRegister(api, netuid, convertPublicKeyToSs58(nominator.publicKey), coldkey)
-        await setSubnetOwnerCut(api, 0)
-        await setActivityCutoff(api, netuid, 65535)
-        await setMaxAllowedUids(api, netuid, 65535)
-        await setMinDelegateTake(api, 0)
-        // await becomeDelegate(api, convertPublicKeyToSs58(validator.publicKey), coldkey)
-        // await becomeDelegate(api, convertPublicKeyToSs58(miner.publicKey), coldkey)
+        console.log("will test in subnet: ", netuid)
     })
 
     it("Staker receives rewards", async () => {
@@ -65,16 +32,26 @@ describe("Test neuron precompile reveal weights", () => {
 
         await addStake(api, netuid, convertPublicKeyToSs58(hotkey.publicKey), tao(1), coldkey)
 
-        const value = await publicClient.readContract({
-            address: ISTAKING_ADDRESS,
-            abi: IStakingABI,
-            functionName: "getStake",
-            args: [hotkey.publicKey, // Convert to bytes32 format
-            hotkey.publicKey,
-                netuid]
-        })
+        const contract = new ethers.Contract(
+            ISTAKING_V2_ADDRESS,
+            IStakingV2ABI,
+            wallet1
+        );
 
-        console.log(value)
+        const stake = BigInt(
+            await contract.getStake(hotkey.publicKey, coldkey.publicKey, netuid)
+        );
+
+        // validator returned as bigint now. 
+        const validators =
+            await contract.getAlphaStakedValidators(hotkey.publicKey, netuid)
+
+        const alpha = BigInt(
+            await contract.getTotalAlphaStaked(hotkey.publicKey, netuid)
+        );
+        assert.ok(stake > 0)
+        assert.equal(validators.length, 1)
+        assert.ok(alpha > 0)
 
     })
 })
