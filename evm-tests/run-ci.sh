f3a29315d4629e07b0dfbf4e4bfba9fe64c83d35
open-junius <zhou@opentensor.dev>
2025-04-01 18:25:29 +0800

    add localnet back

--- evm-tests/run-ci.sh
+++ evm-tests/run-ci.sh
@@ -3,26 +3,26 @@
 echo "start run-ci.sh"
 pwd
 
-# scripts/localnet.sh &>/dev/null &
+scripts/localnet.sh &>/dev/null &
 
-# i=1
-# while [ $i -le 1000 ]; do
-#   if nc -z localhost 9944; then
-#     echo "node subtensor is running after $i seconds"
-#     break
-#   fi
-#   sleep 1
-#   i=$((i + 1))
-# done
+i=1
+while [ $i -le 1000 ]; do
+  if nc -z localhost 9944; then
+    echo "node subtensor is running after $i seconds"
+    break
+  fi
+  sleep 1
+  i=$((i + 1))
+done
 
-# echo "port is available"
-# pwd
+echo "port is available"
+pwd
 
 
-# # port not available exit with error
-# if [ "$i" -eq 1000 ]; then
-#     exit 1
-# fi
+# port not available exit with error
+if [ "$i" -eq 1000 ]; then
+    exit 1
+fi
 
 echo "go to evm-tests"
 cd evm-tests
