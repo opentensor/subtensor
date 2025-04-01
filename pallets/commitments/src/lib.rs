76b3fd8942cc6ef11a307d06026661a90f06033f
John Reed <87283488+JohnReedV@users.noreply.github.com>
2025-04-01 11:31:16 -0700

    magic number => variable

--- pallets/commitments/src/lib.rs
+++ pallets/commitments/src/lib.rs
@@ -221,12 +221,13 @@ pub mod pallet {
 
             let cur_block = <frame_system::Pallet<T>>::block_number();
 
+            let min_used_space: u64 = 100;
             let required_space: u64 = info
                 .fields
                 .iter()
                 .map(|field| field.len_for_rate_limit())
                 .sum::<u64>()
-                .max(100);
+                .max(min_used_space);
 
             let mut usage = UsedSpaceOf::<T>::get(netuid, &who).unwrap_or_default();
             let cur_block_u64 = cur_block.saturated_into::<u64>();
