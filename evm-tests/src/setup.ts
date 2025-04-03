2c4d4e859643bf57872fcf432a7fb461654110f3
open-junius <zhou@opentensor.dev>
2025-04-03 11:37:55 +0800

    clean up code

--- evm-tests/src/setup.ts
+++ evm-tests/src/setup.ts
@@ -3,12 +3,6 @@ import { createClient, TypedApi, PolkadotClient, Binary } from 'polkadot-api';
 import { SUB_LOCAL_URL } from "./config"
 import { getWsProvider } from 'polkadot-api/ws-provider/web';
 
-// export async function getClient(url: ClientUrlType) {
-//     const provider = getWsProvider(url);
-//     const client = createClient(provider);
-//     return client
-// }
-
 let client: PolkadotClient | undefined = undefined
 
 export async function getClient() {
@@ -20,8 +14,6 @@ export async function getClient() {
 }
 
 before(async () => {
-    // const provider = getWsProvider(SUB_LOCAL_URL);
-    // client = createClient(provider);
 
 });
 
