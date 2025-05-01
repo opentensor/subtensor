42bf08bbadc1d05d68c782f0aaa6ced9009ff65f
John Reed <87283488+JohnReedV@users.noreply.github.com>
2025-05-01 11:21:13 -0700

    update commitments weights again

--- pallets/commitments/src/lib.rs
+++ pallets/commitments/src/lib.rs
@@ -198,9 +198,9 @@ pub mod pallet {
         /// Set the commitment for a given netuid
         #[pallet::call_index(0)]
         #[pallet::weight((
-            Weight::from_parts(28_000_000, 0)
-			.saturating_add(T::DbWeight::get().reads(2_u64))
-			.saturating_add(T::DbWeight::get().writes(2_u64)),
+            Weight::from_parts(38_000_000, 0)
+			.saturating_add(T::DbWeight::get().reads(5_u64))
+			.saturating_add(T::DbWeight::get().writes(4_u64)),
             DispatchClass::Operational,
             Pays::No
         ))]
@@ -311,9 +311,9 @@ pub mod pallet {
         /// Sudo-set the commitment rate limit
         #[pallet::call_index(1)]
         #[pallet::weight((
-            Weight::from_parts(10_000_000, 0)
-			.saturating_add(T::DbWeight::get().reads(2_u64))
-			.saturating_add(T::DbWeight::get().writes(2_u64)),
+            Weight::from_parts(3_596_000, 0)
+			.saturating_add(T::DbWeight::get().reads(0_u64))
+			.saturating_add(T::DbWeight::get().writes(1_u64)),
 			DispatchClass::Operational,
 			Pays::No
 		))]
@@ -326,9 +326,9 @@ pub mod pallet {
         /// Sudo-set MaxSpace
         #[pallet::call_index(2)]
         #[pallet::weight((
-            Weight::from_parts(10_000_000, 0)
-			.saturating_add(T::DbWeight::get().reads(2_u64))
-			.saturating_add(T::DbWeight::get().writes(2_u64)),
+            Weight::from_parts(3_556_000, 0)
+			.saturating_add(T::DbWeight::get().reads(0_u64))
+			.saturating_add(T::DbWeight::get().writes(1_u64)),
             DispatchClass::Operational,
             Pays::No
         ))]
