8270040007f7d5af6a8342935ffb85dd75c1baae
open-junius <zhou@opentensor.dev>
2025-05-17 21:25:19 +0800

    clean up code

--- evm-tests/test/runtime.call.precompile.test.ts
+++ evm-tests/test/runtime.call.precompile.test.ts
@@ -1,22 +1,16 @@
-
-
-
 import * as assert from "assert";
-
 import { getAliceSigner, getDevnetApi } from "../src/substrate"
 import {  generateRandomEthersWallet, getPublicClient } from "../src/utils";
 import { IDISPATCH_ADDRESS, ISTORAGE_QUERY_ADDRESS, ETH_LOCAL_URL } from "../src/config";
 import { devnet } from "@polkadot-api/descriptors"
-import { hexToBigInt, hexToNumber, PublicClient } from "viem";
+import { hexToNumber, PublicClient } from "viem";
 import { PolkadotSigner, TypedApi } from "polkadot-api";
 import { convertPublicKeyToSs58 } from "../src/address-utils"
 import { forceSetBalanceToEthAddress, setMaxChildkeyTake } from "../src/subtensor";
 
 describe("Test the dispatch precompile", () => {
     let publicClient: PublicClient;
-     // init eth part
     const wallet1 = generateRandomEthersWallet();
-
     let api: TypedApi<typeof devnet>
     let alice: PolkadotSigner;
 
@@ -25,8 +19,6 @@ describe("Test the dispatch precompile", () => {
         api = await getDevnetApi()
         alice = await getAliceSigner()
         await forceSetBalanceToEthAddress(api, wallet1.address)
-        
-        console.log("wallet1 address: ", wallet1.address)
     })
 
     it("Dispatch transfer call via precompile contract works correctly", async () => {
@@ -35,8 +27,6 @@ describe("Test the dispatch precompile", () => {
         const transferAmount = BigInt(1000000000);
         const transferCall = "0x050300d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d02286bee"
         const aliceBalance =  (await api.query.System.Account.getValue( convertPublicKeyToSs58(alice.publicKey))).data.free
-
-        console.log("alice balance before transfer: ", aliceBalance.toString())
         const txResponse = await wallet1.sendTransaction({
             to: IDISPATCH_ADDRESS,
             data: transferCall,
@@ -53,16 +43,12 @@ describe("Test the dispatch precompile", () => {
     it("Storage query call via precompile contract works correctly", async () => {
         let maxChildkeyTake = 257;
         await setMaxChildkeyTake(api, maxChildkeyTake)
-        // sudo_set_max_childkey_take
         // 0x658faa385070e074c85bf6b568cf0555f14f14d903e9994045ff1902b3e513dc
-    // key for min child key take
+        // key for min child key take
 
         const storageQuery = "0x658faa385070e074c85bf6b568cf0555dba018859cab7e989f77669457b394be"
 
         api.query.SubtensorModule.MaxChildkeyTake.getValue();
-
-        // api.apis.
-
         const rawCallResponse = await publicClient.call({
             to: ISTORAGE_QUERY_ADDRESS,
             data: storageQuery,
@@ -73,8 +59,5 @@ describe("Test the dispatch precompile", () => {
         }
         let value = hexToNumber(rawResultData);
         assert.equal(value, maxChildkeyTake, "value should be 257")
-
-
-        
     })
 });
--- pallets/subtensor/src/tests/batch_tx.rs
+++ pallets/subtensor/src/tests/batch_tx.rs
@@ -1,10 +1,7 @@
-use crate::MaxChildkeyTake;
-
 use super::mock::*;
-use frame_support::{StorageHasher, Twox128, assert_ok, traits::Currency};
+use frame_support::{assert_ok, traits::Currency};
 use frame_system::Config;
 use sp_core::U256;
-use sp_io::hashing::twox_128;
 // SKIP_WASM_BUILD=1 RUST_LOG=debug cargo test --package pallet-subtensor --lib -- tests::batch_tx::test_batch_txs --exact --show-output --nocapture
 #[test]
 fn test_batch_txs() {
@@ -33,44 +30,5 @@ fn test_batch_txs() {
         assert_eq!(Balances::total_balance(&alice), 6_000_000_000);
         assert_eq!(Balances::total_balance(&bob), 2_000_000_000);
         assert_eq!(Balances::total_balance(&charlie), 2_000_000_000);
-    });
-}
-
-// [101, 143, 170, 56, 80, 112, 224, 116, 200, 91, 246, 181, 104, 207, 5, 85, 219, 160, 24, 133, 156, 171, 126, 152, 159, 119, 102, 148, 87, 179, 148, 190]
-#[test]
-fn test_batch_txs2() {
-    let alice = U256::from(0);
-    let bob = U256::from(1);
-    let charlie = U256::from(2);
-    let initial_balances = vec![
-        (alice, 8_000_000_000),
-        (bob, 1_000_000_000),
-        (charlie, 1_000_000_000),
-    ];
-    test_ext_with_balances(initial_balances).execute_with(|| {
-        let init_value = SubtensorModule::get_max_childkey_take();
-        log::error!("Storage value: {:?}", init_value);
-
-        SubtensorModule::set_max_childkey_take(1000);
-
-        // let mut final_key = [0u8; 32];
-        // final_key[16..].copy_from_slice(twox_128(b"Subtensor"));
-        // final_key[..16].copy_from_slice(twox_128(b"MaxChildkeyTake"));
-        let final_key = [twox_128(b"SubtensorModule"), twox_128(b"MaxChildkeyTake")].concat();
-        log::error!("Storage value: {:?}", final_key);
-
-        let hash_key = MaxChildkeyTake::<Test>::hashed_key();
-        log::error!("Storage value: {:?}", hash_key);
-        // let key = [
-        //     101, 143, 170, 56, 80, 112, 224, 116, 200, 91, 246, 181, 104, 207, 5, 85, 219, 160, 24,
-        //     133, 156, 171, 126, 152, 159, 119, 102, 148, 87, 179, 148, 190,
-        // ];
-        let value = sp_io::storage::get(&final_key[..]);
-        log::error!("Storage value: {:?}", value);
-        assert_eq!(value, None);
-
-        sp_io::storage::set(&final_key[..], &[0u8; 32][..]);
-        let init_value = SubtensorModule::get_max_childkey_take();
-        log::error!("Storage value: {:?}", init_value);
     });
 }
