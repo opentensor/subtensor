0c3c6a05140ed6d5c5acb4e72121ac2a2c290c3c
Sam Johnson <sam@durosoft.com>
2025-05-20 21:35:18 -0400

    cargo fmt

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -18,16 +18,16 @@ use frame_support::{
     genesis_builder_helper::{build_state, get_preset},
     pallet_prelude::Get,
     traits::{
+        Contains, LinearStoragePrice, OnUnbalanced,
         fungible::{
             DecreaseIssuance, HoldConsideration, Imbalance as FungibleImbalance, IncreaseIssuance,
         },
-        Contains, LinearStoragePrice, OnUnbalanced,
     },
 };
 use frame_system::{EnsureNever, EnsureRoot, EnsureRootWithSuccess, RawOrigin};
 use pallet_commitments::CanCommit;
 use pallet_grandpa::{
-    fg_primitives, AuthorityId as GrandpaId, AuthorityList as GrandpaAuthorityList,
+    AuthorityId as GrandpaId, AuthorityList as GrandpaAuthorityList, fg_primitives,
 };
 use pallet_registry::CanRegisterIdentity;
 use pallet_subtensor::rpc_info::{
@@ -43,18 +43,18 @@ use smallvec::smallvec;
 use sp_api::impl_runtime_apis;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{
+    H160, H256, OpaqueMetadata, U256,
     crypto::{ByteArray, KeyTypeId},
-    OpaqueMetadata, H160, H256, U256,
 };
 use sp_runtime::generic::Era;
 use sp_runtime::{
-    create_runtime_str, generic, impl_opaque_keys,
+    AccountId32, ApplyExtrinsicResult, ConsensusEngineId, create_runtime_str, generic,
+    impl_opaque_keys,
     traits::{
         AccountIdLookup, BlakeTwo256, Block as BlockT, DispatchInfoOf, Dispatchable, NumberFor,
         One, PostDispatchInfoOf, UniqueSaturatedInto, Verify,
     },
     transaction_validity::{TransactionSource, TransactionValidity, TransactionValidityError},
-    AccountId32, ApplyExtrinsicResult, ConsensusEngineId,
 };
 use sp_std::cmp::Ordering;
 use sp_std::prelude::*;
@@ -66,19 +66,18 @@ use subtensor_runtime_common::{time::*, *};
 
 // A few exports that help ease life for downstream crates.
 pub use frame_support::{
-    construct_runtime, parameter_types,
+    StorageValue, construct_runtime, parameter_types,
     traits::{
-        ConstBool, ConstU128, ConstU32, ConstU64, ConstU8, FindAuthor, InstanceFilter,
+        ConstBool, ConstU8, ConstU32, ConstU64, ConstU128, FindAuthor, InstanceFilter,
         KeyOwnerProofSystem, OnFinalize, OnTimestampSet, PrivilegeCmp, Randomness, StorageInfo,
     },
     weights::{
+        IdentityFee, Weight, WeightToFeeCoefficient, WeightToFeeCoefficients,
+        WeightToFeePolynomial,
         constants::{
             BlockExecutionWeight, ExtrinsicBaseWeight, RocksDbWeight, WEIGHT_REF_TIME_PER_SECOND,
         },
-        IdentityFee, Weight, WeightToFeeCoefficient, WeightToFeeCoefficients,
-        WeightToFeePolynomial,
     },
-    StorageValue,
 };
 pub use frame_system::Call as SystemCall;
 pub use pallet_balances::Call as BalancesCall;
