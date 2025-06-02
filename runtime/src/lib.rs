755161dd20167eb0452b996fb590aaed9bc1bf56
Liam Aharon <liam.aharon@hotmail.com>
2025-06-03 05:01:52 +1200

    remove redundant stuff

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -12,6 +12,7 @@ mod bag_thresholds;
 pub mod check_nonce;
 mod migrations;
 
+use crate::time::*;
 use codec::{Compact, Decode, Encode};
 use frame_election_provider_support::bounds::ElectionBoundsBuilder;
 use frame_election_provider_support::{SequentialPhragmen, generate_solution_type, onchain};
@@ -270,23 +271,6 @@ pub const BABE_GENESIS_EPOCH_CONFIG: babe_primitives::BabeEpochConfiguration =
         c: PRIMARY_PROBABILITY,
         allowed_slots: babe_primitives::AllowedSlots::PrimaryAndSecondaryVRFSlots,
     };
-
-/// This determines the average expected block time that we are targeting.
-/// Blocks will be produced at a minimum duration defined by `SLOT_DURATION`.
-/// `SLOT_DURATION` is picked up by `pallet_timestamp` which is in turn picked
-/// up by `pallet_aura` to implement `fn slot_duration()`.
-///
-/// Change this to adjust the block time.
-pub const MILLISECS_PER_BLOCK: u64 = prod_or_fast!(12_000, 1000);
-
-// NOTE: Currently it is not possible to change the slot duration after the chain has started.
-//       Attempting to do so will brick block production.
-pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
-
-// Time is measured by number of blocks.
-pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
-pub const HOURS: BlockNumber = MINUTES * 60;
-pub const DAYS: BlockNumber = HOURS * 24;
 
 pub const MAXIMUM_BLOCK_WEIGHT: Weight =
     Weight::from_parts(4u64 * WEIGHT_REF_TIME_PER_SECOND, u64::MAX);
