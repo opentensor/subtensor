c47144efd5b7fc83cf764b8a1044ae7b1f7e7fe6
Aliaksandr Tsurko <ales@opentensor.dev>
2025-06-10 19:59:52 +0200

    Remove nonsensical loops

--- pallets/subtensor/src/macros/genesis.rs
+++ pallets/subtensor/src/macros/genesis.rs
@@ -41,64 +41,61 @@ mod genesis {
             // Set target registrations for validators as 1 per block.
             TargetRegistrationsPerInterval::<T>::insert(NetUid::ROOT, 1);
 
-            for net in 1..2 {
-                let netuid = NetUid::from(net);
-                let hotkey = DefaultAccount::<T>::get();
-                SubnetMechanism::<T>::insert(netuid, 1); // Make dynamic.
-                Owner::<T>::insert(hotkey.clone(), hotkey.clone());
-                SubnetAlphaIn::<T>::insert(netuid, 10_000_000_000);
-                SubnetTAO::<T>::insert(netuid, 10_000_000_000);
-                NetworksAdded::<T>::insert(netuid, true);
-                TotalNetworks::<T>::mutate(|n| *n = n.saturating_add(1));
-                SubnetworkN::<T>::insert(netuid, 0);
-                MaxAllowedUids::<T>::insert(netuid, 256u16);
-                MaxAllowedValidators::<T>::insert(netuid, 64u16);
-                MinAllowedWeights::<T>::insert(netuid, 0);
-                MaxWeightsLimit::<T>::insert(netuid, u16::MAX);
-                Tempo::<T>::insert(netuid, 100);
-                NetworkRegistrationAllowed::<T>::insert(netuid, true);
-                SubnetOwner::<T>::insert(netuid, hotkey.clone());
-                SubnetLocked::<T>::insert(netuid, 1);
-                LargestLocked::<T>::insert(netuid, 1);
-                Alpha::<T>::insert(
-                    // Lock the initial funds making this key the owner.
-                    (hotkey.clone(), hotkey.clone(), netuid),
-                    U64F64::saturating_from_num(1_000_000_000),
-                );
-                TotalHotkeyAlpha::<T>::insert(hotkey.clone(), netuid, 1_000_000_000);
-                TotalHotkeyShares::<T>::insert(
-                    hotkey.clone(),
-                    netuid,
-                    U64F64::saturating_from_num(1_000_000_000),
-                );
-                // TotalColdkeyAlpha::<T>::insert(hotkey.clone(), netuid, 1_000_000_000);
-                SubnetAlphaOut::<T>::insert(netuid, 1_000_000_000);
-                let mut staking_hotkeys = StakingHotkeys::<T>::get(hotkey.clone());
-                if !staking_hotkeys.contains(&hotkey) {
-                    staking_hotkeys.push(hotkey.clone());
-                    StakingHotkeys::<T>::insert(hotkey.clone(), staking_hotkeys.clone());
-                }
-
-                let block_number = Pallet::<T>::get_current_block_as_u64();
-                for next_uid in 0u16..1 {
-                    SubnetworkN::<T>::insert(netuid, next_uid.saturating_add(1));
-                    Rank::<T>::mutate(netuid, |v| v.push(0));
-                    Trust::<T>::mutate(netuid, |v| v.push(0));
-                    Active::<T>::mutate(netuid, |v| v.push(true));
-                    Emission::<T>::mutate(netuid, |v| v.push(0));
-                    Consensus::<T>::mutate(netuid, |v| v.push(0));
-                    Incentive::<T>::mutate(netuid, |v| v.push(0));
-                    Dividends::<T>::mutate(netuid, |v| v.push(0));
-                    LastUpdate::<T>::mutate(netuid, |v| v.push(block_number));
-                    PruningScores::<T>::mutate(netuid, |v| v.push(0));
-                    ValidatorTrust::<T>::mutate(netuid, |v| v.push(0));
-                    ValidatorPermit::<T>::mutate(netuid, |v| v.push(false));
-                    Keys::<T>::insert(netuid, next_uid, hotkey.clone()); // Make hotkey - uid association.
-                    Uids::<T>::insert(netuid, hotkey.clone(), next_uid); // Make uid - hotkey association.
-                    BlockAtRegistration::<T>::insert(netuid, next_uid, block_number); // Fill block at registration.
-                    IsNetworkMember::<T>::insert(hotkey.clone(), netuid, true); // Fill network is member.
-                }
+            let netuid = NetUid::from(1);
+            let hotkey = DefaultAccount::<T>::get();
+            SubnetMechanism::<T>::insert(netuid, 1); // Make dynamic.
+            Owner::<T>::insert(hotkey.clone(), hotkey.clone());
+            SubnetAlphaIn::<T>::insert(netuid, 10_000_000_000);
+            SubnetTAO::<T>::insert(netuid, 10_000_000_000);
+            NetworksAdded::<T>::insert(netuid, true);
+            TotalNetworks::<T>::mutate(|n| *n = n.saturating_add(1));
+            SubnetworkN::<T>::insert(netuid, 0);
+            MaxAllowedUids::<T>::insert(netuid, 256u16);
+            MaxAllowedValidators::<T>::insert(netuid, 64u16);
+            MinAllowedWeights::<T>::insert(netuid, 0);
+            MaxWeightsLimit::<T>::insert(netuid, u16::MAX);
+            Tempo::<T>::insert(netuid, 100);
+            NetworkRegistrationAllowed::<T>::insert(netuid, true);
+            SubnetOwner::<T>::insert(netuid, hotkey.clone());
+            SubnetLocked::<T>::insert(netuid, 1);
+            LargestLocked::<T>::insert(netuid, 1);
+            Alpha::<T>::insert(
+                // Lock the initial funds making this key the owner.
+                (hotkey.clone(), hotkey.clone(), netuid),
+                U64F64::saturating_from_num(1_000_000_000),
+            );
+            TotalHotkeyAlpha::<T>::insert(hotkey.clone(), netuid, 1_000_000_000);
+            TotalHotkeyShares::<T>::insert(
+                hotkey.clone(),
+                netuid,
+                U64F64::saturating_from_num(1_000_000_000),
+            );
+            // TotalColdkeyAlpha::<T>::insert(hotkey.clone(), netuid, 1_000_000_000);
+            SubnetAlphaOut::<T>::insert(netuid, 1_000_000_000);
+            let mut staking_hotkeys = StakingHotkeys::<T>::get(hotkey.clone());
+            if !staking_hotkeys.contains(&hotkey) {
+                staking_hotkeys.push(hotkey.clone());
+                StakingHotkeys::<T>::insert(hotkey.clone(), staking_hotkeys.clone());
             }
+
+            let block_number = Pallet::<T>::get_current_block_as_u64();
+
+            SubnetworkN::<T>::insert(netuid, 1);
+            Rank::<T>::mutate(netuid, |v| v.push(0));
+            Trust::<T>::mutate(netuid, |v| v.push(0));
+            Active::<T>::mutate(netuid, |v| v.push(true));
+            Emission::<T>::mutate(netuid, |v| v.push(0));
+            Consensus::<T>::mutate(netuid, |v| v.push(0));
+            Incentive::<T>::mutate(netuid, |v| v.push(0));
+            Dividends::<T>::mutate(netuid, |v| v.push(0));
+            LastUpdate::<T>::mutate(netuid, |v| v.push(block_number));
+            PruningScores::<T>::mutate(netuid, |v| v.push(0));
+            ValidatorTrust::<T>::mutate(netuid, |v| v.push(0));
+            ValidatorPermit::<T>::mutate(netuid, |v| v.push(false));
+            Keys::<T>::insert(netuid, 0, hotkey.clone()); // Make hotkey - uid association.
+            Uids::<T>::insert(netuid, hotkey.clone(), 0); // Make uid - hotkey association.
+            BlockAtRegistration::<T>::insert(netuid, 0, block_number); // Fill block at registration.
+            IsNetworkMember::<T>::insert(hotkey.clone(), netuid, true); // Fill network is member.
         }
     }
 }
