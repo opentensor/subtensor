f50b60bf5241badfa99820a00349a09cc0b2d367
Liam Aharon <liam.aharon@hotmail.com>
2025-06-10 13:47:16 +1200

    fix benchmark file

--- pallets/admin-utils/src/benchmarking.rs
+++ pallets/admin-utils/src/benchmarking.rs
@@ -9,7 +9,6 @@ use alloc::vec::Vec;
 use crate::Pallet as AdminUtils;
 use frame_benchmarking::v1::account;
 use frame_benchmarking::v2::*;
-use frame_support::BoundedVec;
 use frame_system::RawOrigin;
 
 use super::*;
@@ -17,25 +16,6 @@ use super::*;
 #[benchmarks]
 mod benchmarks {
     use super::*;
-
-    #[benchmark]
-    fn swap_authorities(a: Linear<0, 32>) {
-        let mut value: BoundedVec<
-            <T as pallet::Config>::AuthorityId,
-            <T as pallet::Config>::MaxAuthorities,
-        > = BoundedVec::new();
-
-        for idx in 1..=a {
-            let authority: <T as pallet::Config>::AuthorityId = account("Authority", idx, 0u32);
-            let result = value.try_push(authority.clone());
-            if result.is_err() {
-                // Handle the error, perhaps by breaking the loop or logging an error message
-            }
-        }
-
-        #[extrinsic_call]
-        _(RawOrigin::Root, value);
-    }
 
     #[benchmark]
     fn schedule_grandpa_change(a: Linear<0, 32>) {
