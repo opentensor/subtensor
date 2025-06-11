053c2675d7378aefbe24a6d34aeff41999616aa8
Loris Moulin <contact@lorismoulin.com>
2025-06-11 17:38:50 +0200

    commit Cargo.lock

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -2146,7 +2146,7 @@ impl_runtime_apis! {
 
         fn dispatch_benchmark(
             config: frame_benchmarking::BenchmarkConfig
-        ) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, Cow<'static, str>> {
+        ) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, parity_scale_codec::alloc::string::String> {
             use frame_benchmarking::{baseline, Benchmarking, BenchmarkBatch};
             use sp_storage::TrackedStorageKey;
 
