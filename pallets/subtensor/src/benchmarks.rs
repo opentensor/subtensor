cdd18f86c0669fb20557cbb0d0dee2f1e840a5b0
open-junius <zhou@opentensor.dev>
2025-06-22 18:23:44 +0800

    commit Cargo.lock

--- pallets/subtensor/src/benchmarks.rs
+++ pallets/subtensor/src/benchmarks.rs
@@ -9,7 +9,7 @@ use frame_benchmarking::v2::*;
 use frame_support::assert_ok;
 use frame_system::{RawOrigin, pallet_prelude::BlockNumberFor};
 pub use pallet::*;
-use sp_core::H256;
+use sp_core::{H160, H256};
 use sp_runtime::{
     BoundedVec,
     traits::{BlakeTwo256, Hash},
