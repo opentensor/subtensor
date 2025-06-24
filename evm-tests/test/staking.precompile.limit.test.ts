dcb7d1aceef02d8ea9e97d773b0c2574b0a06e2b
open-junius <zhou@opentensor.dev>
2025-06-24 20:50:22 +0800

    revert limit test

--- evm-tests/test/staking.precompile.limit.test.ts
+++ evm-tests/test/staking.precompile.limit.test.ts
@@ -43,73 +43,73 @@ describe("Test staking precompile add remove limit methods", () => {
     console.log("will test in subnet: ", netuid);
   });
 
-  // it("Staker add limit", async () => {
-  //   let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
-  //   let ss58Address = convertH160ToSS58(wallet1.address);
+  it("Staker add limit", async () => {
+    let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
+    let ss58Address = convertH160ToSS58(wallet1.address);
 
-  //   const alpha = await api.query.SubtensorModule.Alpha.getValue(
-  //     convertPublicKeyToSs58(hotkey.publicKey),
-  //     ss58Address,
-  //     netuid,
-  //   );
+    const alpha = await api.query.SubtensorModule.Alpha.getValue(
+      convertPublicKeyToSs58(hotkey.publicKey),
+      ss58Address,
+      netuid,
+    );
 
-  //   const contract = new ethers.Contract(
-  //     ISTAKING_V2_ADDRESS,
-  //     IStakingV2ABI,
-  //     wallet1,
-  //   );
+    const contract = new ethers.Contract(
+      ISTAKING_V2_ADDRESS,
+      IStakingV2ABI,
+      wallet1,
+    );
 
-  //   const tx = await contract.addStakeLimit(
-  //     hotkey.publicKey,
-  //     tao(2000),
-  //     tao(1000),
-  //     true,
-  //     netuid,
-  //   );
-  //   await tx.wait();
+    const tx = await contract.addStakeLimit(
+      hotkey.publicKey,
+      tao(2000),
+      tao(1000),
+      true,
+      netuid,
+    );
+    await tx.wait();
 
-  //   const alphaAfterAddStake = await api.query.SubtensorModule.Alpha.getValue(
-  //     convertPublicKeyToSs58(hotkey.publicKey),
-  //     ss58Address,
-  //     netuid,
-  //   );
+    const alphaAfterAddStake = await api.query.SubtensorModule.Alpha.getValue(
+      convertPublicKeyToSs58(hotkey.publicKey),
+      ss58Address,
+      netuid,
+    );
 
-  //   assert.ok(alphaAfterAddStake > alpha);
-  // });
+    assert.ok(alphaAfterAddStake > alpha);
+  });
 
-  // it("Staker remove limit", async () => {
-  //   let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
-  //   let ss58Address = convertH160ToSS58(wallet1.address);
+  it("Staker remove limit", async () => {
+    let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
+    let ss58Address = convertH160ToSS58(wallet1.address);
 
-  //   const alpha = await api.query.SubtensorModule.Alpha.getValue(
-  //     convertPublicKeyToSs58(hotkey.publicKey),
-  //     ss58Address,
-  //     netuid,
-  //   );
+    const alpha = await api.query.SubtensorModule.Alpha.getValue(
+      convertPublicKeyToSs58(hotkey.publicKey),
+      ss58Address,
+      netuid,
+    );
 
-  //   const contract = new ethers.Contract(
-  //     ISTAKING_V2_ADDRESS,
-  //     IStakingV2ABI,
-  //     wallet1,
-  //   );
+    const contract = new ethers.Contract(
+      ISTAKING_V2_ADDRESS,
+      IStakingV2ABI,
+      wallet1,
+    );
 
-  //   const tx = await contract.removeStakeLimit(
-  //     hotkey.publicKey,
-  //     tao(100),
-  //     tao(1),
-  //     true,
-  //     netuid,
-  //   );
-  //   await tx.wait();
+    const tx = await contract.removeStakeLimit(
+      hotkey.publicKey,
+      tao(100),
+      tao(1),
+      true,
+      netuid,
+    );
+    await tx.wait();
 
-  //   const alphaAfterRemoveStake = await api.query.SubtensorModule.Alpha.getValue(
-  //     convertPublicKeyToSs58(hotkey.publicKey),
-  //     ss58Address,
-  //     netuid,
-  //   );
+    const alphaAfterRemoveStake = await api.query.SubtensorModule.Alpha.getValue(
+      convertPublicKeyToSs58(hotkey.publicKey),
+      ss58Address,
+      netuid,
+    );
 
-  //   assert.ok(alphaAfterRemoveStake < alpha);
-  // });
+    assert.ok(alphaAfterRemoveStake < alpha);
+  });
 
   it("Staker remove stake with limit price", async () => {
     let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
@@ -130,7 +130,7 @@ describe("Test staking precompile add remove limit methods", () => {
     const tx = await contract.removeStakeFullLimit(
       hotkey.publicKey,
       netuid,
-      tao(1),
+      tao(10),
     );
     await tx.wait();
 
