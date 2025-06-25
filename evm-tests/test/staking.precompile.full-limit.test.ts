92d7ca25f912ef636388a6640ca72eb4f63c48e8
open-junius <zhou@opentensor.dev>
2025-06-25 18:33:06 +0800

    fix test case

--- evm-tests/test/staking.precompile.full-limit.test.ts
+++ evm-tests/test/staking.precompile.full-limit.test.ts
@@ -23,6 +23,7 @@ describe("Test staking precompile add remove limit methods", () => {
   const hotkey = getRandomSubstrateKeypair();
   const coldkey = getRandomSubstrateKeypair();
   const wallet1 = generateRandomEthersWallet();
+  const wallet2 = generateRandomEthersWallet();
 
   let api: TypedApi<typeof devnet>;
 
@@ -37,106 +38,147 @@ describe("Test staking precompile add remove limit methods", () => {
       convertPublicKeyToSs58(coldkey.publicKey),
     );
     await forceSetBalanceToEthAddress(api, wallet1.address);
+    await forceSetBalanceToEthAddress(api, wallet2.address);
+
     await addNewSubnetwork(api, hotkey, coldkey);
     let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
     await startCall(api, netuid, coldkey);
     console.log("will test in subnet: ", netuid);
   });
 
-  it("Staker add limit", async () => {
-    let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
-    let ss58Address = convertH160ToSS58(wallet1.address);
+  describe("Add limit then remove stake with limit price", () => {
+    it("Staker add limit for wallet 1", async () => {
+      let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
+      let ss58Address = convertH160ToSS58(wallet1.address);
 
-    const alpha = await api.query.SubtensorModule.Alpha.getValue(
-      convertPublicKeyToSs58(hotkey.publicKey),
-      ss58Address,
-      netuid,
-    );
+      const alpha = await api.query.SubtensorModule.Alpha.getValue(
+        convertPublicKeyToSs58(hotkey.publicKey),
+        ss58Address,
+        netuid,
+      );
 
-    const contract = new ethers.Contract(
-      ISTAKING_V2_ADDRESS,
-      IStakingV2ABI,
-      wallet1,
-    );
+      const contract = new ethers.Contract(
+        ISTAKING_V2_ADDRESS,
+        IStakingV2ABI,
+        wallet1,
+      );
 
-    const tx = await contract.addStakeLimit(
-      hotkey.publicKey,
-      tao(2000),
-      tao(1000),
-      true,
-      netuid,
-    );
-    await tx.wait();
+      const tx = await contract.addStakeLimit(
+        hotkey.publicKey,
+        tao(2000),
+        tao(1000),
+        true,
+        netuid,
+      );
+      await tx.wait();
 
-    const alphaAfterAddStake = await api.query.SubtensorModule.Alpha.getValue(
-      convertPublicKeyToSs58(hotkey.publicKey),
-      ss58Address,
-      netuid,
-    );
+      const alphaAfterAddStake = await api.query.SubtensorModule.Alpha.getValue(
+        convertPublicKeyToSs58(hotkey.publicKey),
+        ss58Address,
+        netuid,
+      );
 
-    assert.ok(alphaAfterAddStake > alpha);
+      assert.ok(alphaAfterAddStake > alpha);
+    });
+
+    it("Staker remove stake with limit price", async () => {
+      let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
+      let ss58Address = convertH160ToSS58(wallet1.address);
+
+      const alpha = await api.query.SubtensorModule.Alpha.getValue(
+        convertPublicKeyToSs58(hotkey.publicKey),
+        ss58Address,
+        netuid,
+      );
+
+      const contract = new ethers.Contract(
+        ISTAKING_V2_ADDRESS,
+        IStakingV2ABI,
+        wallet1,
+      );
+
+      const tx = await contract.removeStakeFullLimit(
+        hotkey.publicKey,
+        netuid,
+        90_000_000,
+      );
+      await tx.wait();
+
+      const alphaAfterRemoveStake = await api.query.SubtensorModule.Alpha.getValue(
+        convertPublicKeyToSs58(hotkey.publicKey),
+        ss58Address,
+        netuid,
+      );
+
+      assert.ok(alphaAfterRemoveStake < alpha);
+    });
   });
 
-  it("Staker remove stake with limit price", async () => {
-    let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
-    let ss58Address = convertH160ToSS58(wallet1.address);
+  describe("Add limit then remove stake full", () => {
 
-    const alpha = await api.query.SubtensorModule.Alpha.getValue(
-      convertPublicKeyToSs58(hotkey.publicKey),
-      ss58Address,
-      netuid,
-    );
+    it("Staker add limit for wallet 2", async () => {
+      let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
+      let ss58Address = convertH160ToSS58(wallet2.address);
 
-    const contract = new ethers.Contract(
-      ISTAKING_V2_ADDRESS,
-      IStakingV2ABI,
-      wallet1,
-    );
+      const alpha = await api.query.SubtensorModule.Alpha.getValue(
+        convertPublicKeyToSs58(hotkey.publicKey),
+        ss58Address,
+        netuid,
+      );
 
-    const tx = await contract.removeStakeFullLimit(
-      hotkey.publicKey,
-      netuid,
-      90_000_000,
-    );
-    await tx.wait();
+      const contract = new ethers.Contract(
+        ISTAKING_V2_ADDRESS,
+        IStakingV2ABI,
+        wallet2,
+      );
 
-    const alphaAfterRemoveStake = await api.query.SubtensorModule.Alpha.getValue(
-      convertPublicKeyToSs58(hotkey.publicKey),
-      ss58Address,
-      netuid,
-    );
+      const tx = await contract.addStakeLimit(
+        hotkey.publicKey,
+        tao(2000),
+        tao(1000),
+        true,
+        netuid,
+      );
+      await tx.wait();
 
-    assert.ok(alphaAfterRemoveStake < alpha);
-  });
+      const alphaAfterAddStake = await api.query.SubtensorModule.Alpha.getValue(
+        convertPublicKeyToSs58(hotkey.publicKey),
+        ss58Address,
+        netuid,
+      );
 
-  it("Staker remove stake with full", async () => {
-    let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
-    let ss58Address = convertH160ToSS58(wallet1.address);
+      assert.ok(alphaAfterAddStake > alpha);
+    });
 
-    const alpha = await api.query.SubtensorModule.Alpha.getValue(
-      convertPublicKeyToSs58(hotkey.publicKey),
-      ss58Address,
-      netuid,
-    );
+    it("Staker remove stake with full", async () => {
+      let netuid = (await api.query.SubtensorModule.TotalNetworks.getValue()) - 1;
+      let ss58Address = convertH160ToSS58(wallet2.address);
 
-    const contract = new ethers.Contract(
-      ISTAKING_V2_ADDRESS,
-      IStakingV2ABI,
-      wallet1,
-    );
+      const alpha = await api.query.SubtensorModule.Alpha.getValue(
+        convertPublicKeyToSs58(hotkey.publicKey),
+        ss58Address,
+        netuid,
+      );
 
-    const tx = await contract.removeStakeFull(
-      hotkey.publicKey,
-      netuid,
-    );
-    await tx.wait();
+      const contract = new ethers.Contract(
+        ISTAKING_V2_ADDRESS,
+        IStakingV2ABI,
+        wallet2,
+      );
 
-    const alphaAfterRemoveStake = await api.query.SubtensorModule.Alpha.getValue(
-      convertPublicKeyToSs58(hotkey.publicKey),
-      ss58Address,
-      netuid,
-    );
+      const tx = await contract.removeStakeFull(
+        hotkey.publicKey,
+        netuid,
+      );
+      await tx.wait();
 
-    assert.ok(alphaAfterRemoveStake < alpha);
+      const alphaAfterRemoveStake = await api.query.SubtensorModule.Alpha.getValue(
+        convertPublicKeyToSs58(hotkey.publicKey),
+        ss58Address,
+        netuid,
+      );
+
+      assert.ok(alphaAfterRemoveStake < alpha);
+    });
   });
 });
