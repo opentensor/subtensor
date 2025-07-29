cc25e44f2f6ce0d232925104284948ef49ab612c
open-junius <zhou@opentensor.dev>
2025-07-29 09:49:39 +0800

    commit Cargo.lock

--- pallets/proxy/src/benchmarking.rs
+++ pallets/proxy/src/benchmarking.rs
@@ -273,7 +273,7 @@ benchmarks! {
         }
 
         let proxies = EVMProxies::<T>::get(evm_address);
-        let last_proxy = proxies.get(T::MaxProxies::get() as usize - 1).unwrap();
+        let last_proxy = proxies.get(T::MaxProxies::get() as usize - 1)?;
         ensure!(Proxies::<T>::contains_key(last_proxy), "pure proxy not created");
     }: _(RawOrigin::Signed(caller.clone()), evm_address, last_proxy.clone())
     verify {
