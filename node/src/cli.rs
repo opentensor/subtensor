f05522743286e67d60842df0e3e63c29890316c2
Liam Aharon <liam.aharon@hotmail.com>
2025-07-31 13:33:23 +1200

    add todo to remove initial_consensus flag.

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -26,6 +26,8 @@ pub struct Cli {
     ///
     /// After starting, the consensus used by the node will automatically
     /// switch to whatever is required to continue validating / syncing.
+    ///
+    /// TODO: Remove this after the Babe transition has settled.
     #[arg(long, value_enum, ignore_case = true, default_value_t=SupportedConsensusMechanism::default())]
     pub initial_consensus: SupportedConsensusMechanism,
 
