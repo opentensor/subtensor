4fa3cbd2f3113f7cfd7cb7524f709077115f4768
open-junius <zhou@opentensor.dev>
2025-08-04 09:00:12 +0800

    add blake2 128 precompile

---
+++ precompiles/src/blake2_128.rs
@@ -0,0 +1,14 @@
+/// The blake2_128 precompile.
+use fp_evm::{ExitSucceed, LinearCostPrecompile, PrecompileFailure};
+
+pub struct BlakeTwo128;
+
+impl LinearCostPrecompile for BlakeTwo128 {
+    const BASE: u64 = 60;
+    const WORD: u64 = 12;
+
+    fn execute(input: &[u8], _cost: u64) -> Result<(ExitSucceed, Vec<u8>), PrecompileFailure> {
+        let ret = sp_io::hashing::blake2_128(input);
+        Ok((ExitSucceed::Returned, ret.to_vec()))
+    }
+}
