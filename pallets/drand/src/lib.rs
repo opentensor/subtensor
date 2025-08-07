c5bf77975d833010c3457ebb51f20095000fa09f
John Reed <87283488+JohnReedV@users.noreply.github.com>
2025-08-07 15:27:44 -0700

    set oldest round

--- pallets/drand/src/lib.rs
+++ pallets/drand/src/lib.rs
@@ -277,12 +277,11 @@ pub mod pallet {
             }
         }
         fn on_runtime_upgrade() -> frame_support::weights::Weight {
-            /* let weight = */
-            frame_support::weights::Weight::from_parts(0, 0) /*;*/
+            let mut weight = frame_support::weights::Weight::from_parts(0, 0);
 
-            //weight = weight.saturating_add(migrations::migrate_prune_old_pulses::<T>());
+            weight = weight.saturating_add(migrations::migrate_set_oldest_round::<T>());
 
-            //weight
+            weight
         }
     }
 
