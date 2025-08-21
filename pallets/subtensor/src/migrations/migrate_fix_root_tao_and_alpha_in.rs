51f78d37ce44f6bc49a39fef0074d455908c2ae3
Greg Zaitsev <gregz@opentensor.dev>
2025-08-21 12:14:38 -0400

    Set numbers for migrate_fix_root_tao_and_alpha_in

--- pallets/subtensor/src/migrations/migrate_fix_root_tao_and_alpha_in.rs
+++ pallets/subtensor/src/migrations/migrate_fix_root_tao_and_alpha_in.rs
@@ -19,22 +19,25 @@ pub fn migrate_fix_root_tao_and_alpha_in<T: Config>() -> Weight {
         String::from_utf8_lossy(&migration_name)
     );
 
-    // Update counters
-    // TODO: Will be filled with real numbers once the runtime 302 is on mainnet
+    // Update counters (unstaked more than stake)
+    let total_staked = 2_109_761_275_100_688_u64;
+    let total_unstaked = 2_179_659_173_851_658_u64;
+    let reserve_diff = total_unstaked.saturating_sub(total_staked);
+    let volume_diff = (total_unstaked as u128).saturating_add(total_staked as u128);
     SubnetTAO::<T>::mutate(NetUid::ROOT, |amount| {
-        *amount = amount.saturating_add(TaoCurrency::from(100_000_000_000));
+        *amount = amount.saturating_sub(TaoCurrency::from(reserve_diff));
     });
     SubnetAlphaIn::<T>::mutate(NetUid::ROOT, |amount| {
-        *amount = amount.saturating_add(AlphaCurrency::from(100_000_000_000));
+        *amount = amount.saturating_add(AlphaCurrency::from(reserve_diff));
     });
     SubnetAlphaOut::<T>::mutate(NetUid::ROOT, |amount| {
-        *amount = amount.saturating_add(AlphaCurrency::from(100_000_000_000));
+        *amount = amount.saturating_sub(AlphaCurrency::from(reserve_diff));
     });
     SubnetVolume::<T>::mutate(NetUid::ROOT, |amount| {
-        *amount = amount.saturating_add(100_000_000_000_u128);
+        *amount = amount.saturating_add(volume_diff);
     });
     TotalStake::<T>::mutate(|amount| {
-        *amount = amount.saturating_add(TaoCurrency::from(100_000_000_000));
+        *amount = amount.saturating_sub(TaoCurrency::from(reserve_diff));
     });
 
     weight = weight.saturating_add(T::DbWeight::get().writes(5));
