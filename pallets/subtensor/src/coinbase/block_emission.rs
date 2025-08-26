5783a2b8912793c7aa3d16998daa5ad4762d00e4
µ <maarten@coldint.io>
2025-08-26 21:18:34 +0200

    do not update (singular) BlockEmission for every subnet, but only on fetching Tao block emission

--- pallets/subtensor/src/coinbase/block_emission.rs
+++ pallets/subtensor/src/coinbase/block_emission.rs
@@ -98,7 +98,13 @@ impl<T: Config> Pallet<T> {
     ///
     pub fn get_block_emission() -> Result<TaoCurrency, &'static str> {
         // Convert the total issuance to a fixed-point number for calculation.
-        Self::get_block_emission_for_issuance(Self::get_total_issuance().into()).map(Into::into)
+        let block_emission =
+            Self::get_block_emission_for_issuance(Self::get_total_issuance().into());
+        let block_emission_u64: u64 = block_emission.unwrap_or(0);
+        if BlockEmission::<T>::get() != block_emission_u64 {
+            BlockEmission::<T>::put(block_emission_u64);
+        }
+        block_emission.map(Into::into)
     }
 
     /// Returns the block emission for an issuance value.
@@ -144,9 +150,6 @@ impl<T: Config> Pallet<T> {
             .saturating_mul(I96F32::saturating_from_num(DefaultBlockEmission::<T>::get()));
         // Convert to u64
         let block_emission_u64: u64 = block_emission.saturating_to_num::<u64>();
-        if BlockEmission::<T>::get() != block_emission_u64 {
-            BlockEmission::<T>::put(block_emission_u64);
-        }
         Ok(block_emission_u64)
     }
 }
