cc04156ec92dadee9e189ab8c1505e2358178681
open-junius <zhou@opentensor.dev>
2025-08-26 16:25:04 +0800

    commit Cargo.lock

--- pallets/proxy/src/weights.rs
+++ pallets/proxy/src/weights.rs
@@ -60,9 +60,7 @@ pub trait WeightInfo {
 	fn remove_proxy(p: u32, ) -> Weight;
 	fn remove_proxies(p: u32, ) -> Weight;
 	fn create_pure(p: u32, ) -> Weight;
-	fn create_evm_pure(p: u32, ) -> Weight;
 	fn kill_pure(p: u32, ) -> Weight;
-	fn kill_evm_pure(p: u32, ) -> Weight;
 }
 
 /// Weights for `pallet_proxy` using the Substrate node and recommended hardware.
@@ -225,41 +223,10 @@ impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
 			.saturating_add(T::DbWeight::get().reads(1_u64))
 			.saturating_add(T::DbWeight::get().writes(1_u64))
 	}
-
-	/// Storage: `Proxy::Proxies` (r:1 w:1)
-	/// Proof: `Proxy::Proxies` (`max_values`: None, `max_size`: Some(1241), added: 3716, mode: `MaxEncodedLen`)
-	/// The range of component `p` is `[1, 31]`.
-	fn create_evm_pure(p: u32, ) -> Weight {
-		// Proof Size summary in bytes:
-		//  Measured:  `173`
-		//  Estimated: `4706`
-		// Minimum execution time: 22_809_000 picoseconds.
-		Weight::from_parts(23_878_644, 4706)
-			// Standard Error: 1_600
-			.saturating_add(Weight::from_parts(10_149, 0).saturating_mul(p.into()))
-			.saturating_add(T::DbWeight::get().reads(1_u64))
-			.saturating_add(T::DbWeight::get().writes(1_u64))
-	}
-
 	/// Storage: `Proxy::Proxies` (r:1 w:1)
 	/// Proof: `Proxy::Proxies` (`max_values`: None, `max_size`: Some(1241), added: 3716, mode: `MaxEncodedLen`)
 	/// The range of component `p` is `[0, 30]`.
 	fn kill_pure(p: u32, ) -> Weight {
-		// Proof Size summary in bytes:
-		//  Measured:  `198 + p * (37 ±0)`
-		//  Estimated: `4706`
-		// Minimum execution time: 20_993_000 picoseconds.
-		Weight::from_parts(22_067_418, 4706)
-			// Standard Error: 1_673
-			.saturating_add(Weight::from_parts(52_703, 0).saturating_mul(p.into()))
-			.saturating_add(T::DbWeight::get().reads(1_u64))
-			.saturating_add(T::DbWeight::get().writes(1_u64))
-	}
-
-	/// Storage: `Proxy::Proxies` (r:1 w:1)
-	/// Proof: `Proxy::Proxies` (`max_values`: None, `max_size`: Some(1241), added: 3716, mode: `MaxEncodedLen`)
-	/// The range of component `p` is `[0, 30]`.
-	fn kill_evm_pure(p: u32, ) -> Weight {
 		// Proof Size summary in bytes:
 		//  Measured:  `198 + p * (37 ±0)`
 		//  Estimated: `4706`
@@ -431,41 +398,10 @@ impl WeightInfo for () {
 			.saturating_add(RocksDbWeight::get().reads(1_u64))
 			.saturating_add(RocksDbWeight::get().writes(1_u64))
 	}
-
-	/// Storage: `Proxy::Proxies` (r:1 w:1)
-	/// Proof: `Proxy::Proxies` (`max_values`: None, `max_size`: Some(1241), added: 3716, mode: `MaxEncodedLen`)
-	/// The range of component `p` is `[1, 31]`.
-	fn create_evm_pure(p: u32, ) -> Weight {
-		// Proof Size summary in bytes:
-		//  Measured:  `173`
-		//  Estimated: `4706`
-		// Minimum execution time: 22_809_000 picoseconds.
-		Weight::from_parts(23_878_644, 4706)
-			// Standard Error: 1_600
-			.saturating_add(Weight::from_parts(10_149, 0).saturating_mul(p.into()))
-			.saturating_add(RocksDbWeight::get().reads(1_u64))
-			.saturating_add(RocksDbWeight::get().writes(1_u64))
-	}
-
 	/// Storage: `Proxy::Proxies` (r:1 w:1)
 	/// Proof: `Proxy::Proxies` (`max_values`: None, `max_size`: Some(1241), added: 3716, mode: `MaxEncodedLen`)
 	/// The range of component `p` is `[0, 30]`.
 	fn kill_pure(p: u32, ) -> Weight {
-		// Proof Size summary in bytes:
-		//  Measured:  `198 + p * (37 ±0)`
-		//  Estimated: `4706`
-		// Minimum execution time: 20_993_000 picoseconds.
-		Weight::from_parts(22_067_418, 4706)
-			// Standard Error: 1_673
-			.saturating_add(Weight::from_parts(52_703, 0).saturating_mul(p.into()))
-			.saturating_add(RocksDbWeight::get().reads(1_u64))
-			.saturating_add(RocksDbWeight::get().writes(1_u64))
-	}
-
-	/// Storage: `Proxy::Proxies` (r:1 w:1)
-	/// Proof: `Proxy::Proxies` (`max_values`: None, `max_size`: Some(1241), added: 3716, mode: `MaxEncodedLen`)
-	/// The range of component `p` is `[0, 30]`.
-	fn kill_evm_pure(p: u32, ) -> Weight {
 		// Proof Size summary in bytes:
 		//  Measured:  `198 + p * (37 ±0)`
 		//  Estimated: `4706`
