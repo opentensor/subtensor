2c2233102a57344ccf31f756978b5ce4bf71c0b2
John Reed <87283488+JohnReedV@users.noreply.github.com>
2025-09-03 12:28:59 -0700

    test patch

--- pallets/subtensor/src/macros/dispatches.rs
+++ pallets/subtensor/src/macros/dispatches.rs
@@ -2063,7 +2063,7 @@ mod dispatches {
         ///     - The hotkey account to designate as the autostake destination.
         #[pallet::call_index(114)]
         #[pallet::weight((Weight::from_parts(5_170_000, 0)
-		.saturating_add(T::DbWeight::get().reads(0))
+		.saturating_add(T::DbWeight::get().reads(1))
 		.saturating_add(T::DbWeight::get().writes(1)), DispatchClass::Normal, Pays::No))]
         pub fn set_coldkey_auto_stake_hotkey(
             origin: T::RuntimeOrigin,
