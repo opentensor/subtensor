2e22929232157817669d04841a359c7b4a8d06c3
John Reed <87283488+JohnReedV@users.noreply.github.com>
2025-09-09 14:45:22 -0700

    only do one read

--- pallets/subtensor/src/coinbase/run_coinbase.rs
+++ pallets/subtensor/src/coinbase/run_coinbase.rs
@@ -512,13 +512,16 @@ impl<T: Config> Pallet<T> {
             }
 
             let owner: T::AccountId = Owner::<T>::get(&hotkey);
-            let destination = AutoStakeDestination::<T>::get(&owner).unwrap_or(hotkey.clone());
+            let maybe_dest = AutoStakeDestination::<T>::get(&owner);
 
-            if AutoStakeDestination::<T>::contains_key(&owner) {
+            // Always stake but only emit event if autostake is set.
+            let destination = maybe_dest.clone().unwrap_or(hotkey.clone());
+
+            if let Some(dest) = maybe_dest {
                 Self::deposit_event(Event::<T>::AutoStakeAdded {
                     netuid,
-                    destination: destination.clone(),
-                    hotkey,
+                    destination: dest,
+                    hotkey: hotkey.clone(),
                     owner: owner.clone(),
                     incentive,
                 });
