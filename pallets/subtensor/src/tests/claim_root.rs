0151f490ce243719a52ac33c35e8c901595a31b2
Shamil Gadelshin <shamilgadelshin@gmail.com>
2025-09-17 17:30:11 +0300

    Add basic test with drain_emissions

--- pallets/subtensor/src/tests/claim_root.rs
+++ pallets/subtensor/src/tests/claim_root.rs
@@ -1,10 +1,13 @@
-use crate::TotalHotkeyAlpha;
-use crate::tests::mock::{RuntimeOrigin, SubtensorModule, Test, add_dynamic_network, new_test_ext};
+use crate::tests::mock::{
+    RuntimeOrigin, SubtensorModule, Test, add_dynamic_network, new_test_ext,
+};
 use crate::{RootClaimType, RootClaimTypeEnum, SubnetAlphaIn, SubnetTAO, pallet};
+use crate::{RootClaimable, TotalHotkeyAlpha};
+use approx::assert_abs_diff_eq;
 use frame_support::assert_ok;
 use sp_core::U256;
-use substrate_fixed::types::U96F32;
-use subtensor_runtime_common::{AlphaCurrency, NetUid, TaoCurrency};
+use substrate_fixed::types::{I96F32, U96F32};
+use subtensor_runtime_common::{AlphaCurrency, Currency, NetUid, TaoCurrency};
 use subtensor_swap_interface::SwapHandler;
 
 #[test]
@@ -22,11 +25,14 @@ fn test_set_root_claim_type() {
 }
 
 #[test]
-fn test_claim_root() {
+fn test_claim_root_with_drain_emissions() {
     new_test_ext(1).execute_with(|| {
-        let coldkey = U256::from(1001);
+        let owner_coldkey = U256::from(1001);
         let hotkey = U256::from(1002);
-        let netuid = add_dynamic_network(&hotkey, &coldkey);
+        let coldkey = U256::from(1003);
+        let netuid = add_dynamic_network(&hotkey, &owner_coldkey);
+        let initial_balance = 10_000_000u64;
+        SubtensorModule::add_balance_to_coldkey_account(&coldkey, initial_balance.into());
 
         let tao_reserve = TaoCurrency::from(50_000_000_000);
         let alpha_in = AlphaCurrency::from(100_000_000_000);
@@ -35,6 +41,8 @@ fn test_claim_root() {
         let current_price =
             <Test as pallet::Config>::SwapInterface::current_alpha_price(netuid.into());
         assert_eq!(current_price, U96F32::from_num(0.5));
+
+        SubtensorModule::set_tao_weight(u64::MAX); // Set TAO weight to 1.0
 
         let stake = 1_000_000u64;
         TotalHotkeyAlpha::<Test>::insert(hotkey, netuid, AlphaCurrency::from(stake));
@@ -45,28 +53,85 @@ fn test_claim_root() {
             stake.into(),
         );
 
-        let claimable_amount = 1_000_000u64;
-        SubtensorModule::increase_root_claimable_for_hotkey_and_subnet(
-            &hotkey,
+        let pending_root_alpha = AlphaCurrency::from(1_000_000);
+        SubtensorModule::drain_pending_emission(
             netuid,
-            claimable_amount.into(),
+            AlphaCurrency::ZERO,
+            pending_root_alpha,
+            AlphaCurrency::ZERO,
+        );
+
+        let claimable = RootClaimable::<Test>::get(hotkey, netuid);
+
+        let validator_take_percent = 18u64;
+        let calculated_rate = (I96F32::from(stake) * I96F32::from(100u64 - validator_take_percent))
+            / I96F32::from(100u64)
+            / I96F32::from(u64::from(TotalHotkeyAlpha::<Test>::get(hotkey, netuid)));
+
+        assert_abs_diff_eq!(
+            (claimable * I96F32::from(1000u64)).saturating_to_num::<u64>(),
+            (calculated_rate * I96F32::from(1000u64)).saturating_to_num::<u64>(),
+            epsilon = 10u64,
         );
 
         assert_ok!(SubtensorModule::set_root_claim_type(
             RuntimeOrigin::signed(coldkey),
             RootClaimTypeEnum::Keep
         ),);
-
         assert_eq!(RootClaimType::<Test>::get(coldkey), RootClaimTypeEnum::Keep);
-
-        let new_stake =
-            SubtensorModule::get_stake_for_hotkey_and_coldkey_on_subnet(&hotkey, &coldkey, netuid);
-        assert_eq!(new_stake, 0u64.into());
 
         assert_ok!(SubtensorModule::claim_root(RuntimeOrigin::signed(coldkey),));
 
+        log::debug!("RootClaimable = {}", claimable);
         let new_stake =
             SubtensorModule::get_stake_for_hotkey_and_coldkey_on_subnet(&hotkey, &coldkey, netuid);
-        assert_eq!(new_stake, (stake * 2).into());
+
+        assert_abs_diff_eq!(
+            u64::from(new_stake),
+            (I96F32::from(stake) * claimable).saturating_to_num::<u64>(),
+            epsilon = 10u64,
+        );
+    });
+}
+
+/*
+Test 1 - Adding Stake Disproportionally
+
+1. Beginning of the epoch 1: Alice stakes 1 TAO to root, Bob stakes 1 TAO to root, and both say "I want airdrop"
+2. For simplicity let's say `root_alpha = 0.1` in every block. So at the end of the epoch 1 we have 36 root alpha to share between Alice and Bob. Each is entitled to 18 Alpha.
+3. Beginning of the epoch 2: Alice stakes 1 TAO more, Bob stakes 2 TAO more. State is now:
+
+```
+alpha_to_airdrop: 36
+rewards_per_tao: 18
+
+Alice's stake: 2
+Bob's stake: 3
+debt_alice: 18
+debt_bob: 36
+```
+
+4. As epoch 2 goes, we keep adding 0.1 root Alpha in every block, so it will be still +36 Alpha, but new Alpha is shared differently between Alice and Bob: Alice gets 2/5 of the new Alpha and Bob gets 3/5 of the new Alpha.
+
+5. End of epoch 2: Alice is entitled to 18 + 14.4 = 32.4 Alpha, Bob is entitled to 18 + 21.6 = 39.6 Alpha. State update is following:
+
+```
+alpha_to_airdrop: 72
+rewards_per_tao: 18 + 36/5 = 25.2
+```
+
+6. Alice and Bob want to claim their Alpha:
+
+```
+alpha_alice = 2 * 25.2 - 18 = 32.4
+alpha_bob = 3 * 25.2 - 36 = 39.6
+```
+
+*/
+
+#[test]
+fn test_adding_stake_disproportionally() {
+    new_test_ext(1).execute_with(|| {
+
     });
 }
