af73e51028b440f5e69a822cbc79699e8ffca029
open-junius <zhou@opentensor.dev>
2025-09-22 08:54:59 +0100

    add test file

---
+++ evm-tests/test/alphaPool.test.ts
@@ -0,0 +1,48 @@
+import * as assert from "assert";
+import * as chai from "chai";
+
+import { getDevnetApi } from "../src/substrate"
+import { generateRandomEthersWallet, getPublicClient } from "../src/utils";
+import { ETH_LOCAL_URL } from "../src/config";
+import { devnet } from "@polkadot-api/descriptors"
+import { PublicClient } from "viem";
+import { TypedApi } from "polkadot-api";
+import { ALPHA_POOL_CONTRACT_ABI, ALPHA_POOL_CONTRACT_BYTECODE } from "../src/contracts/alphaPool";
+import { toViemAddress } from "../src/address-utils";
+import { forceSetBalanceToEthAddress, disableWhiteListCheck } from "../src/subtensor";
+import { ethers } from "ethers"
+describe("bridge token contract deployment", () => {
+  // init eth part
+  const wallet = generateRandomEthersWallet();
+  let publicClient: PublicClient;
+
+  // init substrate part
+  let api: TypedApi<typeof devnet>
+
+  before(async () => {
+    // init variables got from await and async
+    publicClient = await getPublicClient(ETH_LOCAL_URL)
+    api = await getDevnetApi()
+
+    await forceSetBalanceToEthAddress(api, wallet.address)
+    await disableWhiteListCheck(api, true)
+  });
+
+  it("Can deploy bridge token smart contract", async () => {
+    const contractFactory = new ethers.ContractFactory(ALPHA_POOL_CONTRACT_ABI, ALPHA_POOL_CONTRACT_BYTECODE, wallet)
+    const contract = await contractFactory.deploy("name",
+      "symbol", wallet.address)
+    await contract.waitForDeployment()
+    assert.notEqual(contract.target, undefined)
+
+    const contractAddress = contract.target.toString()
+
+    const code = await publicClient.getCode({ address: toViemAddress(contractAddress) })
+    if (code === undefined) {
+      throw new Error("code not available")
+    }
+    assert.ok(code.length > 100)
+    assert.ok(code.includes("0x60806040523480156"))
+  });
+
+});
