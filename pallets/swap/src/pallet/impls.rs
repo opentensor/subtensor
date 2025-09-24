09a028765f0e4a942a113be7f0056f53be0d914a
John Reed <87283488+JohnReedV@users.noreply.github.com>
2025-09-24 09:35:26 -0700

    Update impls.rs

--- pallets/swap/src/pallet/impls.rs
+++ pallets/swap/src/pallet/impls.rs
@@ -1286,7 +1286,7 @@ impl<T: Config> Pallet<T> {
                                 T::BalanceOps::increase_stake(
                                     &owner,
                                     &validator_hotkey,
-                                    NetUid::ROOT,
+                                    netuid,
                                     alpha_total_from_pool,
                                 )?;
 
