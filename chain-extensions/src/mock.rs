142842d398a2c5cc9a4f88c24ea2ad386e728743
Francisco Silva <franjs.francisco@gmail.com>
2025-10-20 17:52:28 +0200

    fix: remove InitialMaxWeightsLimit from chain-extensions mock

--- chain-extensions/src/mock.rs
+++ chain-extensions/src/mock.rs
@@ -265,7 +265,6 @@ parameter_types! {
 parameter_types! {
     pub const InitialMinAllowedWeights: u16 = 0;
     pub const InitialEmissionValue: u16 = 0;
-    pub const InitialMaxWeightsLimit: u16 = u16::MAX;
     pub BlockWeights: limits::BlockWeights = limits::BlockWeights::with_sensible_defaults(
         Weight::from_parts(2_000_000_000_000, u64::MAX),
         Perbill::from_percent(75),
@@ -349,7 +348,6 @@ impl pallet_subtensor::Config for Test {
     type Scheduler = Scheduler;
     type InitialMinAllowedWeights = InitialMinAllowedWeights;
     type InitialEmissionValue = InitialEmissionValue;
-    type InitialMaxWeightsLimit = InitialMaxWeightsLimit;
     type InitialTempo = InitialTempo;
     type InitialDifficulty = InitialDifficulty;
     type InitialAdjustmentInterval = InitialAdjustmentInterval;
