63e993139d7e96ada2b44910d4a9a78a072af718
John Reed <87283488+JohnReedV@users.noreply.github.com>
2025-10-22 03:42:53 -0700

    zeroize

--- node/src/shuffle_seed.rs
+++ node/src/shuffle_seed.rs
@@ -1,13 +1,16 @@
 use rand::{RngCore, rngs::OsRng};
 use sp_core::hashing::blake2_256;
+use zeroize::Zeroizing;
 
 pub fn ephemeral_block_salt<H: AsRef<[u8]>>(parent_hash: H) -> [u8; 32] {
-    let mut seed = [0u8; 32];
-    OsRng.fill_bytes(&mut seed);
+    // Fresh per-block seed; automatically zeroized when it goes out of scope.
+    let mut seed = Zeroizing::new([0u8; 32]);
+    OsRng.fill_bytes(seed.as_mut());
+
+    let mut buf = Zeroizing::new([0u8; 64]);
+    buf[..32].copy_from_slice(&*seed);
 
     let h = parent_hash.as_ref();
-    let mut buf = [0u8; 64];
-    buf[..32].copy_from_slice(&seed);
     let n = core::cmp::min(h.len(), 32);
     buf[32..32 + n].copy_from_slice(&h[..n]);
 
