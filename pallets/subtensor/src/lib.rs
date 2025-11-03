0d66053d30a0d9d9845d596a62108a343a8b7d7e
Greg Zaitsev <gregz@opentensor.dev>
2025-11-03 13:22:57 -0500

    Init flows instead of transition, set temperature = 1.0

--- pallets/subtensor/src/lib.rs
+++ pallets/subtensor/src/lib.rs
@@ -88,7 +88,6 @@ pub mod pallet {
     use frame_system::pallet_prelude::*;
     use pallet_drand::types::RoundNumber;
     use runtime_common::prod_or_fast;
-    use safe_math::FixedExt;
     use sp_core::{ConstU32, H160, H256};
     use sp_runtime::traits::{Dispatchable, TrailingZeroInput};
     use sp_std::collections::btree_map::BTreeMap;
@@ -1292,7 +1291,7 @@ pub mod pallet {
     #[pallet::type_value]
     /// Default value for flow normalization exponent.
     pub fn DefaultFlowNormExponent<T: Config>() -> U64F64 {
-        U64F64::saturating_from_num(15).safe_div(U64F64::saturating_from_num(10))
+        U64F64::saturating_from_num(1)
     }
     #[pallet::storage]
     /// --- ITEM --> Flow Normalization Exponent (p)
@@ -1302,7 +1301,7 @@ pub mod pallet {
     /// Default value for flow EMA smoothing.
     pub fn DefaultFlowEmaSmoothingFactor<T: Config>() -> u64 {
         // Example values:
-        //   half-life            factor value        i64 normalized
+        //   half-life            factor value        i64 normalized (x 2^63)
         //   216000 (1 month) --> 0.000003209009576 ( 29_597_889_189_277)
         //    50400 (1 week)  --> 0.000013752825678 (126_847_427_788_335)
         29_597_889_189_277
@@ -1316,9 +1315,6 @@ pub mod pallet {
     /// --- ITEM --> Flow EMA smoothing factor (flow alpha), u64 normalized
     pub type FlowEmaSmoothingFactor<T: Config> =
         StorageValue<_, u64, ValueQuery, DefaultFlowEmaSmoothingFactor<T>>;
-    #[pallet::storage]
-    /// --- ITEM --> Block when TAO flow calculation starts(ed)
-    pub type FlowFirstBlock<T: Config> = StorageValue<_, u64, OptionQuery>;
 
     /// ============================
     /// ==== Global Parameters =====
