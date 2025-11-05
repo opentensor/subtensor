61621611a024e0c5e2d5eca158f27b628929c312
Greg Zaitsev <gregz@opentensor.dev>
2025-11-04 22:32:34 -0500

    Do not call migration that removes tao flows

--- pallets/subtensor/src/macros/hooks.rs
+++ pallets/subtensor/src/macros/hooks.rs
@@ -159,9 +159,7 @@ mod hooks {
                 // Migrate Kappa to default (0.5)
                 .saturating_add(migrations::migrate_kappa_map_to_default::migrate_kappa_map_to_default::<T>())
                 // Remove obsolete map entries
-                .saturating_add(migrations::migrate_remove_tao_dividends::migrate_remove_tao_dividends::<T>())
-                // Re-init tao flows
-                .saturating_add(migrations::migrate_init_tao_flow::migrate_init_tao_flow::<T>());
+                .saturating_add(migrations::migrate_remove_tao_dividends::migrate_remove_tao_dividends::<T>());
             weight
         }
 
