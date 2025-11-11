789097464fb202947aecf9844bc5c0609d552428
konrad0960 <konradsebastianmolinski@gmail.com>
2025-11-11 21:30:56 +0100

    commit Cargo.lock

--- pallets/subtensor/src/tests/consensus_mode.rs
+++ pallets/subtensor/src/tests/consensus_mode.rs
@@ -1,32 +1,68 @@
-#![allow(
-    clippy::arithmetic_side_effects,
-    clippy::expect_used,
-    clippy::indexing_slicing,
-    clippy::unwrap_used
-)]
-
 use frame_support::{assert_err, assert_ok};
 use sp_core::U256;
 use substrate_fixed::types::I32F32;
 use subtensor_runtime_common::NetUid;
 
+use super::consensus::{fixed, fixed_proportion_to_u16};
 use super::mock::*;
 use crate::*;
+
+// ============================================================================
+// Test Helper Functions
+// ============================================================================
+
+/// Sets up a network with full owner permissions (root registration + network creation)
+/// Returns (netuid, hotkey, coldkey, signer)
+fn setup_network_with_owner() -> (NetUid, U256, U256, RuntimeOrigin) {
+    let hotkey = U256::from(1);
+    let coldkey = U256::from(457);
+    let netuid = add_dynamic_network(&hotkey, &coldkey);
+    let signer = RuntimeOrigin::signed(coldkey);
+
+    migrations::migrate_create_root_network::migrate_create_root_network::<Test>();
+    SubtensorModule::add_balance_to_coldkey_account(&coldkey, 1_000_000_000_000_000);
+    assert_ok!(SubtensorModule::root_register(signer.clone(), hotkey));
+    assert_ok!(SubtensorModule::register_network(signer.clone(), hotkey));
+
+    (netuid, hotkey, coldkey, signer)
+}
+
+/// Sets up a basic network environment for consensus testing
+/// Creates network and enables liquid alpha
+fn setup_consensus_test_environment(netuid: NetUid) {
+    add_network(netuid, 0, 0);
+    SubtensorModule::set_liquid_alpha_enabled(netuid, true);
+}
+
+/// Creates test consensus data and stores it in the system
+/// Returns (current_consensus, previous_values)
+fn create_test_consensus_data(netuid: NetUid) -> (Vec<I32F32>, Vec<f32>) {
+    let current_consensus: Vec<I32F32> = vec![
+        I32F32::from_num(0.2),
+        I32F32::from_num(0.3),
+        I32F32::from_num(0.4),
+        I32F32::from_num(0.1),
+    ];
+
+    let previous_values: Vec<f32> = vec![0.1, 0.2, 0.3, 0.4];
+    let previous_consensus_u16: Vec<u16> = previous_values
+        .iter()
+        .map(|&v| fixed_proportion_to_u16(fixed(v)))
+        .collect();
+    Consensus::<Test>::insert(netuid, previous_consensus_u16);
+
+    (current_consensus, previous_values)
+}
+
+// ============================================================================
+// Tests
+// ============================================================================
 
 /// Test setting consensus mode when liquid alpha is disabled
 #[test]
 fn test_set_consensus_mode_liquid_alpha_disabled() {
     new_test_ext(1).execute_with(|| {
-        let hotkey = U256::from(1);
-        let coldkey = U256::from(1 + 456);
-        let netuid = add_dynamic_network(&hotkey, &coldkey);
-        let signer = RuntimeOrigin::signed(coldkey);
-
-        // Setup network
-        migrations::migrate_create_root_network::migrate_create_root_network::<Test>();
-        SubtensorModule::add_balance_to_coldkey_account(&coldkey, 1_000_000_000_000_000);
-        assert_ok!(SubtensorModule::root_register(signer.clone(), hotkey));
-        assert_ok!(SubtensorModule::register_network(signer.clone(), hotkey));
+        let (netuid, _hotkey, _coldkey, signer) = setup_network_with_owner();
 
         // Liquid Alpha is disabled by default
         assert!(!SubtensorModule::get_liquid_alpha_enabled(netuid));
@@ -57,22 +93,12 @@ fn test_set_consensus_mode_liquid_alpha_disabled() {
 #[test]
 fn test_set_consensus_mode_permissions() {
     new_test_ext(1).execute_with(|| {
-        let hotkey = U256::from(1);
-        let coldkey = U256::from(1 + 456);
+        let (netuid, _hotkey, _coldkey, owner_signer) = setup_network_with_owner();
         let non_owner = U256::from(999);
-        let netuid = add_dynamic_network(&hotkey, &coldkey);
-        let owner_signer = RuntimeOrigin::signed(coldkey);
         let non_owner_signer = RuntimeOrigin::signed(non_owner);
 
-        // Setup network
+        // Enable liquid alpha for this test
         SubtensorModule::set_liquid_alpha_enabled(netuid, true);
-        migrations::migrate_create_root_network::migrate_create_root_network::<Test>();
-        SubtensorModule::add_balance_to_coldkey_account(&coldkey, 1_000_000_000_000_000);
-        assert_ok!(SubtensorModule::root_register(owner_signer.clone(), hotkey));
-        assert_ok!(SubtensorModule::register_network(
-            owner_signer.clone(),
-            hotkey
-        ));
 
         // Non-owner should fail
         assert_err!(
@@ -98,7 +124,7 @@ fn test_set_consensus_mode_permissions() {
 fn test_default_consensus_mode() {
     new_test_ext(1).execute_with(|| {
         let hotkey = U256::from(1);
-        let coldkey = U256::from(1 + 456);
+        let coldkey = U256::from(457);
         let netuid = add_dynamic_network(&hotkey, &coldkey);
 
         // Default should be Auto
@@ -111,17 +137,10 @@ fn test_default_consensus_mode() {
 #[test]
 fn test_set_and_get_consensus_modes() {
     new_test_ext(1).execute_with(|| {
-        let hotkey = U256::from(1);
-        let coldkey = U256::from(1 + 456);
-        let netuid = add_dynamic_network(&hotkey, &coldkey);
-        let signer = RuntimeOrigin::signed(coldkey);
+        let (netuid, _hotkey, _coldkey, signer) = setup_network_with_owner();
 
-        // Setup network
+        // Enable liquid alpha for this test
         SubtensorModule::set_liquid_alpha_enabled(netuid, true);
-        migrations::migrate_create_root_network::migrate_create_root_network::<Test>();
-        SubtensorModule::add_balance_to_coldkey_account(&coldkey, 1_000_000_000_000_000);
-        assert_ok!(SubtensorModule::root_register(signer.clone(), hotkey));
-        assert_ok!(SubtensorModule::register_network(signer.clone(), hotkey));
 
         // Test Current mode
         assert_ok!(SubtensorModule::do_set_liquid_alpha_consensus_mode(
@@ -145,17 +164,6 @@ fn test_set_and_get_consensus_modes() {
             ConsensusMode::Previous
         );
 
-        // Test Max mode
-        assert_ok!(SubtensorModule::do_set_liquid_alpha_consensus_mode(
-            signer.clone(),
-            netuid,
-            ConsensusMode::Max
-        ));
-        assert_eq!(
-            SubtensorModule::get_liquid_alpha_consensus_mode(netuid),
-            ConsensusMode::Max
-        );
-
         // Test Auto mode
         assert_ok!(SubtensorModule::do_set_liquid_alpha_consensus_mode(
             signer.clone(),
@@ -176,35 +184,18 @@ fn test_compute_consensus_current_mode() {
         let netuid: NetUid = 1.into();
         let n: usize = 4;
 
-        // Create network
-        add_network(netuid, 0, 0);
-        SubtensorModule::set_liquid_alpha_enabled(netuid, true);
+        // Setup network and test data
+        setup_consensus_test_environment(netuid);
         SubtensorModule::set_liquid_alpha_consensus_mode(netuid, ConsensusMode::Current);
-
-        // Setup some current consensus values
-        let current_consensus: Vec<I32F32> = vec![
-            I32F32::from_num(0.2),
-            I32F32::from_num(0.3),
-            I32F32::from_num(0.4),
-            I32F32::from_num(0.1),
-        ];
-
-        // Store some different previous consensus values in storage
-        let previous_consensus_u16: Vec<u16> = vec![
-            (0.1 * u16::MAX as f32) as u16,
-            (0.2 * u16::MAX as f32) as u16,
-            (0.3 * u16::MAX as f32) as u16,
-            (0.4 * u16::MAX as f32) as u16,
-        ];
-        Consensus::<Test>::insert(netuid, previous_consensus_u16);
+        let (current_consensus, _previous_values) = create_test_consensus_data(netuid);
 
         // Compute consensus for liquid alpha
         let result = SubtensorModule::compute_consensus_for_liquid_alpha(netuid, &current_consensus);
 
         // Should return current consensus (not previous)
         assert_eq!(result.len(), n);
-        for i in 0..n {
-            assert_eq!(result[i], current_consensus[i]);
+        for (res, curr) in result.iter().zip(current_consensus.iter()) {
+            assert_eq!(res, curr);
         }
     });
 }
@@ -216,152 +207,25 @@ fn test_compute_consensus_previous_mode() {
         let netuid: NetUid = 1.into();
         let n: usize = 4;
 
-        // Create network
-        add_network(netuid, 0, 0);
-        SubtensorModule::set_liquid_alpha_enabled(netuid, true);
+        // Setup network and test data
+        setup_consensus_test_environment(netuid);
         SubtensorModule::set_liquid_alpha_consensus_mode(netuid, ConsensusMode::Previous);
-
-        // Setup some current consensus values
-        let current_consensus: Vec<I32F32> = vec![
-            I32F32::from_num(0.2),
-            I32F32::from_num(0.3),
-            I32F32::from_num(0.4),
-            I32F32::from_num(0.1),
-        ];
-
-        // Store some different previous consensus values in storage
-        let previous_values: Vec<f32> = vec![0.1, 0.2, 0.3, 0.4];
-        let previous_consensus_u16: Vec<u16> = previous_values
-            .iter()
-            .map(|&v| (v * u16::MAX as f32) as u16)
-            .collect();
-        Consensus::<Test>::insert(netuid, previous_consensus_u16.clone());
+        let (current_consensus, previous_values) = create_test_consensus_data(netuid);
 
         // Compute consensus for liquid alpha
         let result = SubtensorModule::compute_consensus_for_liquid_alpha(netuid, &current_consensus);
 
         // Should return previous consensus from storage (not current)
         assert_eq!(result.len(), n);
-        for i in 0..n {
-            let expected = I32F32::from_num(previous_values[i]);
+        for (res, &prev) in result.iter().zip(previous_values.iter()) {
+            let expected = I32F32::from_num(prev);
             // Allow small floating point difference
-            let diff = if result[i] > expected {
-                result[i] - expected
+            let diff = if *res > expected {
+                *res - expected
             } else {
-                expected - result[i]
+                expected - *res
             };
             assert!(diff < I32F32::from_num(0.001), "Values should be approximately equal");
-        }
-    });
-}
-
-/// Test compute_consensus_for_liquid_alpha with Max mode
-#[test]
-fn test_compute_consensus_max_mode() {
-    new_test_ext(1).execute_with(|| {
-        let netuid: NetUid = 1.into();
-        let n: usize = 4;
-
-        // Create network
-        add_network(netuid, 0, 0);
-        SubtensorModule::set_liquid_alpha_enabled(netuid, true);
-        SubtensorModule::set_liquid_alpha_consensus_mode(netuid, ConsensusMode::Max);
-
-        // Setup current consensus values (some larger, some smaller than previous)
-        let current_consensus: Vec<I32F32> = vec![
-            I32F32::from_num(0.2), // larger than 0.1
-            I32F32::from_num(0.2), // same as 0.2
-            I32F32::from_num(0.25), // smaller than 0.3
-            I32F32::from_num(0.5), // larger than 0.4
-        ];
-
-        // Store previous consensus values in storage
-        let previous_values: Vec<f32> = vec![0.1, 0.2, 0.3, 0.4];
-        let previous_consensus_u16: Vec<u16> = previous_values
-            .iter()
-            .map(|&v| (v * u16::MAX as f32) as u16)
-            .collect();
-        Consensus::<Test>::insert(netuid, previous_consensus_u16);
-
-        // Compute consensus for liquid alpha
-        let result = SubtensorModule::compute_consensus_for_liquid_alpha(netuid, &current_consensus);
-
-        // Should return element-wise max of current and previous
-        assert_eq!(result.len(), n);
-
-        // Expected max values
-        let expected: Vec<f32> = vec![0.2, 0.2, 0.3, 0.5];
-        for i in 0..n {
-            let expected_val = I32F32::from_num(expected[i]);
-            let diff = if result[i] > expected_val {
-                result[i] - expected_val
-            } else {
-                expected_val - result[i]
-            };
-            assert!(diff < I32F32::from_num(0.001), "Values should be approximately equal at index {}", i);
-        }
-    });
-}
-
-/// Test compute_consensus_for_liquid_alpha with Max mode when network grows
-/// (current consensus is longer than previous consensus)
-#[test]
-fn test_compute_consensus_max_mode_network_growth() {
-    new_test_ext(1).execute_with(|| {
-        let netuid: NetUid = 1.into();
-
-        // Create network
-        add_network(netuid, 0, 0);
-        SubtensorModule::set_liquid_alpha_enabled(netuid, true);
-        SubtensorModule::set_liquid_alpha_consensus_mode(netuid, ConsensusMode::Max);
-
-        // Setup current consensus with 6 values (network has grown)
-        let current_consensus: Vec<I32F32> = vec![
-            I32F32::from_num(0.2), // larger than 0.1
-            I32F32::from_num(0.15), // smaller than 0.2
-            I32F32::from_num(0.25), // smaller than 0.3
-            I32F32::from_num(0.5), // larger than 0.4
-            I32F32::from_num(0.1), // new position (no previous) - should use current
-            I32F32::from_num(0.3), // new position (no previous) - should use current
-        ];
-
-        // Store previous consensus values in storage (only 4 values - network was smaller)
-        let previous_values: Vec<f32> = vec![0.1, 0.2, 0.3, 0.4];
-        let previous_consensus_u16: Vec<u16> = previous_values
-            .iter()
-            .map(|&v| (v * u16::MAX as f32) as u16)
-            .collect();
-        Consensus::<Test>::insert(netuid, previous_consensus_u16);
-
-        // Compute consensus for liquid alpha
-        let result = SubtensorModule::compute_consensus_for_liquid_alpha(netuid, &current_consensus);
-
-        // Should return element-wise max of current and previous
-        // For positions where previous doesn't exist, treat previous as 0 (so use current)
-        assert_eq!(result.len(), 6);
-
-        // Expected max values:
-        // [0] max(0.2, 0.1) = 0.2
-        // [1] max(0.15, 0.2) = 0.2
-        // [2] max(0.25, 0.3) = 0.3
-        // [3] max(0.5, 0.4) = 0.5
-        // [4] max(0.1, 0) = 0.1 (no previous, treat as 0)
-        // [5] max(0.3, 0) = 0.3 (no previous, treat as 0)
-        let expected: Vec<f32> = vec![0.2, 0.2, 0.3, 0.5, 0.1, 0.3];
-        for i in 0..6 {
-            let expected_val = I32F32::from_num(expected[i]);
-            let diff = if result[i] > expected_val {
-                result[i] - expected_val
-            } else {
-                expected_val - result[i]
-            };
-            assert!(
-                diff < I32F32::from_num(0.001),
-                "Values should be approximately equal at index {}. Expected: {}, Got: {}",
-                i,
-                expected[i],
-                result[i]
-            );
         }
     });
 }
@@ -373,34 +237,18 @@ fn test_compute_consensus_auto_mode() {
         let netuid: NetUid = 1.into();
         let n: usize = 4;
 
-        // Create network
-        add_network(netuid, 0, 0);
-        SubtensorModule::set_liquid_alpha_enabled(netuid, true);
+        // Setup network and test data
+        setup_consensus_test_environment(netuid);
         SubtensorModule::set_liquid_alpha_consensus_mode(netuid, ConsensusMode::Auto);
-
-        // Setup current consensus values
-        let current_consensus: Vec<I32F32> = vec![
-            I32F32::from_num(0.2),
-            I32F32::from_num(0.3),
-            I32F32::from_num(0.4),
-            I32F32::from_num(0.1),
-        ];
-
-        // Store previous consensus values in storage
-        let previous_values: Vec<f32> = vec![0.1, 0.2, 0.3, 0.4];
-        let previous_consensus_u16: Vec<u16> = previous_values
-            .iter()
-            .map(|&v| (v * u16::MAX as f32) as u16)
-            .collect();
-        Consensus::<Test>::insert(netuid, previous_consensus_u16);
+        let (current_consensus, previous_values) = create_test_consensus_data(netuid);
 
         // Test 1: bond_penalty != 1, should use Current
         SubtensorModule::set_bonds_penalty(netuid, u16::MAX / 2); // 0.5
         let result = SubtensorModule::compute_consensus_for_liquid_alpha(netuid, &current_consensus);
 
         assert_eq!(result.len(), n);
-        for i in 0..n {
-            assert_eq!(result[i], current_consensus[i], "Should use current consensus when bond_penalty != 1");
+        for (res, curr) in result.iter().zip(current_consensus.iter()) {
+            assert_eq!(res, curr, "Should use current consensus when bond_penalty != 1");
         }
 
         // Test 2: bond_penalty == 1, should use Previous
@@ -408,12 +256,12 @@ fn test_compute_consensus_auto_mode() {
         let result = SubtensorModule::compute_consensus_for_liquid_alpha(netuid, &current_consensus);
 
         assert_eq!(result.len(), n);
-        for i in 0..n {
-            let expected = I32F32::from_num(previous_values[i]);
-            let diff = if result[i] > expected {
-                result[i] - expected
+        for (res, &prev) in result.iter().zip(previous_values.iter()) {
+            let expected = I32F32::from_num(prev);
+            let diff = if *res > expected {
+                *res - expected
             } else {
-                expected - result[i]
+                expected - *res
             };
             assert!(diff < I32F32::from_num(0.001), "Should use previous consensus when bond_penalty == 1");
         }
