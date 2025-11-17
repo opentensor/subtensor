b30ffc77885464b20c3d4d5b1c39f532b6a4237d
Loris Moulin <contact@lorismoulin.com>
2025-11-17 11:54:15 -0300

    fix typo

--- pallets/subtensor/src/migrations/migrate_remove_unknown_neuron_axon_cert_prom.rs
+++ pallets/subtensor/src/migrations/migrate_remove_unknown_neuron_axon_cert_prom.rs
@@ -65,7 +65,7 @@ pub fn migrate_remove_unknown_neuron_axon_cert_prom<T: Config>() -> Weight {
         weight.saturating_accrue(T::DbWeight::get().writes(cleaned_prometheus as u64));
 
         log::info!(
-            "Cleaned {} axons, {} neuron Certificates, {} prometheus for network {}",
+            "Cleaned {} axons, {} neuron certificates, {} prometheus for network {}",
             cleaned_axons,
             cleaned_certificates,
             cleaned_prometheus,
