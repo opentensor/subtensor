de6703b0c2a6bf9e97acc916edced6047164a0a1
open-junius <zhou@opentensor.dev>
2025-12-02 17:43:27 +0800

    more time to run localnet.sh

--- evm-tests/run-ci.sh
+++ evm-tests/run-ci.sh
@@ -5,7 +5,7 @@ echo "start run-ci.sh"
 scripts/localnet.sh &>/dev/null &
 
 i=1
-while [ $i -le 1000 ]; do
+while [ $i -le 2000 ]; do
   if nc -z localhost 9944; then
     echo "node subtensor is running after $i seconds"
     break
@@ -15,7 +15,7 @@ while [ $i -le 1000 ]; do
 done
 
 # port not available exit with error
-if [ "$i" -eq 1000 ]; then
+if [ "$i" -eq 2000 ]; then
     exit 1
 fi
 
