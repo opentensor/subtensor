3b4ed9b434c9c15f0508a4294340eeae15605bd6
John Reed <87283488+JohnReedV@users.noreply.github.com>
2025-12-03 15:43:33 -0800

    fix bug

--- node/src/mev_shield/proposer.rs
+++ node/src/mev_shield/proposer.rs
@@ -702,10 +702,6 @@ pub fn spawn_revealer<B, C, Pool>(
                                     error_message,
                                     hex::encode(aead_body_hash),
                                 );
-                                failed_calls.push((
-                                    id,
-                                    create_failed_call(id, error_message),
-                                ));
                                 continue;
                             }
                         };
