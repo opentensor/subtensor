ad20881dae46063457e10974b867840d1a1de685
unconst <jake@bittensor.com>
2025-12-16 13:47:50 -0600

    remove details

--- pallets/subtensor/src/coinbase/run_coinbase.rs
+++ pallets/subtensor/src/coinbase/run_coinbase.rs
@@ -79,7 +79,7 @@ impl<T: Config> Pallet<T> {
                 if let Ok(buy_swap_result_ok) = buy_swap_result {
                     let bought_alpha: AlphaCurrency = buy_swap_result_ok.amount_paid_out.into();
                     PendingRootAlphaDivs::<T>::mutate(*netuid_i, |total| {
-                        *total = total.saturating_add(tou64!(bought_alpha).into());
+                        *total = total.saturating_add(bought_alpha);
                     });
                 }
             }
@@ -144,8 +144,8 @@ impl<T: Config> Pallet<T> {
             let alpha_out_i: U96F32 = alpha_emission_i;
             let mut alpha_in_i: U96F32 = tao_emission_i.safe_div_or(price_i, U96F32::from_num(0.0));
 
-            if alpha_in_i > Self::root_proportion( netuid ) {
-                alpha_in_i = Self::root_proportion( netuid );
+            if alpha_in_i > Self::root_proportion( netuid_i ) {
+                alpha_in_i = Self::root_proportion( netuid_i );
                 tao_in_i = alpha_in_i.saturating_mul(price_i);
             }
 
