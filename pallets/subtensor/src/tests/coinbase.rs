afa0d4556e7f415416d0fe21beb8118f88ab4ce7
Greg Zaitsev <gregz@opentensor.dev>
2025-12-24 13:23:37 -0500

    Fix test_coinbase_subnet_terms_with_alpha_in_gt_alpha_emission

--- pallets/subtensor/src/tests/coinbase.rs
+++ pallets/subtensor/src/tests/coinbase.rs
@@ -3349,80 +3349,81 @@ fn test_coinbase_subnets_with_no_reg_get_no_emission() {
 // Tests for the excess TAO condition
 #[test]
 fn test_coinbase_subnet_terms_with_alpha_in_gt_alpha_emission() {
-    todo!();
+    new_test_ext(1).execute_with(|| {
+        let zero = U96F32::saturating_from_num(0);
+        let netuid0 = add_dynamic_network(&U256::from(1), &U256::from(2));
+        mock::setup_reserves(
+            netuid0,
+            TaoCurrency::from(1_000_000_000_000_000),
+            AlphaCurrency::from(1_000_000_000_000_000),
+        );
+        // Initialize swap v3
+        Swap::maybe_initialize_palswap(netuid0);
 
-    // new_test_ext(1).execute_with(|| {
-    //     let zero = U96F32::saturating_from_num(0);
-    //     let netuid0 = add_dynamic_network(&U256::from(1), &U256::from(2));
-    //     mock::setup_reserves(
-    //         netuid0,
-    //         TaoCurrency::from(1_000_000_000_000_000),
-    //         AlphaCurrency::from(1_000_000_000_000_000),
-    //     );
-    //     // Initialize swap v3
-    //     Swap::maybe_initialize_palswap(netuid0);
+        // Set netuid0 to have price tao_emission / price > alpha_emission
+        let alpha_emission = U96F32::saturating_from_num(
+            SubtensorModule::get_block_emission_for_issuance(
+                SubtensorModule::get_alpha_issuance(netuid0).into(),
+            )
+            .unwrap_or(0),
+        );
+        let price_to_set: U64F64 = U64F64::saturating_from_num(0.01);
+        let price_to_set_fixed: U96F32 = U96F32::saturating_from_num(price_to_set);
 
-    //     // Set netuid0 to have price tao_emission / price > alpha_emission
-    //     let alpha_emission = U96F32::saturating_from_num(
-    //         SubtensorModule::get_block_emission_for_issuance(
-    //             SubtensorModule::get_alpha_issuance(netuid0).into(),
-    //         )
-    //         .unwrap_or(0),
-    //     );
-    //     let price_to_set: U64F64 = U64F64::saturating_from_num(0.01);
-    //     let price_to_set_fixed: U96F32 = U96F32::saturating_from_num(price_to_set);
-    //     let sqrt_price_to_set: U64F64 = sqrt(price_to_set).unwrap();
+        let tao_emission: U96F32 = U96F32::saturating_from_num(alpha_emission)
+            .saturating_mul(price_to_set_fixed)
+            .saturating_add(U96F32::saturating_from_num(0.01));
 
-    //     let tao_emission: U96F32 = U96F32::saturating_from_num(alpha_emission)
-    //         .saturating_mul(price_to_set_fixed)
-    //         .saturating_add(U96F32::saturating_from_num(0.01));
+        // Set the price
+        let tao = TaoCurrency::from(1_000_000_000_u64);
+        let alpha = AlphaCurrency::from((U64F64::saturating_from_num(u64::from(tao)) / price_to_set).to_num::<u64>());
+        SubnetTAO::<Test>::insert(netuid0, tao);
+        SubnetAlphaIn::<Test>::insert(netuid0, alpha);
 
-    //     // Set the price
-    //     pallet_subtensor_swap::AlphaSqrtPrice::<Test>::insert(netuid0, sqrt_price_to_set);
-    //     // Check the price is set
-    //     assert_abs_diff_eq!(
-    //         pallet_subtensor_swap::Pallet::<Test>::current_alpha_price(netuid0).to_num::<f64>(),
-    //         price_to_set.to_num::<f64>(),
-    //         epsilon = 0.001
-    //     );
+        // Check the price is set
+        assert_abs_diff_eq!(
+            pallet_subtensor_swap::Pallet::<Test>::current_alpha_price(netuid0).to_num::<f64>(),
+            price_to_set.to_num::<f64>(),
+            epsilon = 0.001
+        );
 
-    //     let subnet_emissions = BTreeMap::from([(netuid0, tao_emission)]);
+        let subnet_emissions = BTreeMap::from([(netuid0, tao_emission)]);
 
-    //     let (tao_in, alpha_in, alpha_out, excess_tao) =
-    //         SubtensorModule::get_subnet_terms(&subnet_emissions);
+        let (tao_in, alpha_in, alpha_out, excess_tao) =
+            SubtensorModule::get_subnet_terms(&subnet_emissions);
 
-    //     // Check our condition is met
-    //     assert!(tao_emission / price_to_set_fixed > alpha_emission);
+        // Check our condition is met
+        assert!(tao_emission / price_to_set_fixed > alpha_emission);
 
-    //     // alpha_out should be the alpha_emission, always
-    //     assert_abs_diff_eq!(
-    //         alpha_out[&netuid0].to_num::<f64>(),
-    //         alpha_emission.to_num::<f64>(),
-    //         epsilon = 0.01
-    //     );
+        // alpha_out should be the alpha_emission, always
+        assert_abs_diff_eq!(
+            alpha_out[&netuid0].to_num::<f64>(),
+            alpha_emission.to_num::<f64>(),
+            epsilon = 0.01
+        );
 
-    //     // alpha_in should equal the alpha_emission
-    //     assert_abs_diff_eq!(
-    //         alpha_in[&netuid0].to_num::<f64>(),
-    //         alpha_emission.to_num::<f64>(),
-    //         epsilon = 0.01
-    //     );
-    //     // tao_in should be the alpha_in at the ratio of the price
-    //     assert_abs_diff_eq!(
-    //         tao_in[&netuid0].to_num::<f64>(),
-    //         alpha_in[&netuid0]
-    //             .saturating_mul(price_to_set_fixed)
-    //             .to_num::<f64>(),
-    //         epsilon = 0.01
-    //     );
+        // alpha_in should equal the alpha_emission
+        assert_abs_diff_eq!(
+            alpha_in[&netuid0].to_num::<f64>(),
+            alpha_emission.to_num::<f64>(),
+            epsilon = 0.01
+        );
+        // tao_in should be the alpha_in at the ratio of the price
+        assert_abs_diff_eq!(
+            tao_in[&netuid0].to_num::<f64>(),
+            alpha_in[&netuid0]
+                .saturating_mul(price_to_set_fixed)
+                .to_num::<f64>(),
+            epsilon = 0.01
+        );
 
-    //     // excess_tao should be the difference between the tao_emission and the tao_in
-    //     assert_abs_diff_eq!(
-    //         excess_tao[&netuid0].to_num::<f64>(),
-    //         tao_emission.to_num::<f64>() - tao_in[&netuid0].to_num::<f64>(),
-    //         epsilon = 0.01
-    //     );
-    // });
+        // excess_tao should be the difference between the tao_emission and the tao_in
+        assert_abs_diff_eq!(
+            excess_tao[&netuid0].to_num::<f64>(),
+            tao_emission.to_num::<f64>() - tao_in[&netuid0].to_num::<f64>(),
+            epsilon = 0.01
+        );
+    });
 }
 
 #[test]
