9c11f2e2000118a615511505137999ad8fe23186
Loris Moulin <contact@lorismoulin.com>
2026-01-22 12:00:00 -0300

    cargo clippy

--- pallets/subtensor/src/tests/swap_coldkey.rs
+++ pallets/subtensor/src/tests/swap_coldkey.rs
@@ -1467,11 +1467,11 @@ fn test_reset_coldkey_swap_works() {
 
         assert_ok!(SubtensorModule::reset_coldkey_swap(
             RuntimeOrigin::root(),
-            who.clone(),
+            who,
         ));
 
-        assert!(!ColdkeySwapAnnouncements::<Test>::contains_key(&who));
-        assert!(!ColdkeySwapDisputes::<Test>::contains_key(&who));
+        assert!(!ColdkeySwapAnnouncements::<Test>::contains_key(who));
+        assert!(!ColdkeySwapDisputes::<Test>::contains_key(who));
         assert!(matches!(
             last_event(),
             RuntimeEvent::SubtensorModule(Event::ColdkeySwapReset { who })
