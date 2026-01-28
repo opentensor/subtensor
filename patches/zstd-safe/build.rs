e60183aea432e0a32a2c8218fa4f7373206d3c29
Greg Zaitsev <gregz@opentensor.dev>
2026-01-28 11:44:51 -0500

    Move zstd patches out to a separate repo

--- patches/zstd-safe/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // Force the `std` feature in some cases
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
-
-    if target_os == "wasi" || target_os == "emscripten" || target_os == "hermit" {
-        println!("cargo:rustc-cfg=feature=\"std\"");
-    }
-}
