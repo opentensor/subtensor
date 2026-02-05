05230694304dffc528f00b870b9bdacb878190c7
John Reed <87283488+JohnReedV@users.noreply.github.com>
2026-02-05 07:42:39 -0800

    Update pallets/utility/src/lib.rs
    Co-authored-by: Loris Moulin <45130584+l0r1s@users.noreply.github.com>

--- pallets/utility/src/lib.rs
+++ pallets/utility/src/lib.rs
@@ -307,7 +307,7 @@ pub mod pallet {
         /// - O(C) where C is the number of calls to be batched.
         #[pallet::call_index(2)]
         #[pallet::weight({
-			let (dispatch_weight, _, pays) = Pallet::<T>::weight_and_dispatch_class(calls);
+			let (dispatch_weight, pays) = Pallet::<T>::weight_and_dispatch_class(calls);
 			let dispatch_weight = dispatch_weight.saturating_add(T::WeightInfo::batch_all(calls.len() as u32));
 			(dispatch_weight, DispatchClass::Normal, pays)
 		})]
