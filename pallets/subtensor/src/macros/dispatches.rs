203628c3d00ec5d846902e56ead386454fd026e0
open-junius <zhou@opentensor.dev>
2026-02-25 21:56:31 +0800

    correct weights for dissolve network

--- pallets/subtensor/src/macros/dispatches.rs
+++ pallets/subtensor/src/macros/dispatches.rs
@@ -1243,8 +1243,8 @@ mod dispatches {
         /// The caller must be the owner of the network
         #[pallet::call_index(61)]
         #[pallet::weight(Weight::from_parts(119_000_000, 0)
-		.saturating_add(T::DbWeight::get().reads(6))
-		.saturating_add(T::DbWeight::get().writes(31)))]
+		.saturating_add(T::DbWeight::get().reads(3))
+		.saturating_add(T::DbWeight::get().writes(3)))]
         pub fn dissolve_network(
             origin: OriginFor<T>,
             _coldkey: T::AccountId,
