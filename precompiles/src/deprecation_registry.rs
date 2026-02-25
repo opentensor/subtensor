57f8567678c21ad314c1827038cf74c2a84f91bd
Pawel Kulpa Reef <pawel.kulpa@reef.pl>
2026-02-25 14:16:39 +0100

    commit Cargo.lock

--- precompiles/src/deprecation_registry.rs
+++ precompiles/src/deprecation_registry.rs
@@ -1,3 +1,4 @@
+use alloc::string::String;
 use core::marker::PhantomData;
 
 use pallet_admin_utils::DeprecationRegistryStorage;
--- precompiles/src/neuron.rs
+++ precompiles/src/neuron.rs
@@ -1,3 +1,4 @@
+use alloc::string::String;
 use core::marker::PhantomData;
 use subtensor_runtime_common::{Currency, NetUid};
 
