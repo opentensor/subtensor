c33d6035d73b36f5b8054433c86f0e7ce42bd2c5
Greg Zaitsev <gregz@opentensor.dev>
2026-03-04 12:33:06 -0500

    Spec bump

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -267,7 +267,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     //   `spec_version`, and `authoring_version` are the same between Wasm and native.
     // This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
     //   the compatible custom types.
-    spec_version: 386,
+    spec_version: 387,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
