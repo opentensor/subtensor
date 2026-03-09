9dc9db6be903619fbcbb9888dd23212db3c68ca8
Evgeny Svirsky <e.svirsky@tut.by>
2026-03-09 14:23:18 +0100

    - Added remove stake test

--- ts-tests/suites/dev/subtensor/staking/test-add-staking.ts
+++ ts-tests/suites/dev/subtensor/staking/test-add-staking.ts
@@ -1,7 +1,7 @@
 import { beforeAll, describeSuite, expect } from "@moonwall/cli";
 import type { ApiPromise } from "@polkadot/api";
-import { generateKeyringPair } from "@moonwall/util";
-import {BN} from "@polkadot/util";
+import { generateKeyringPair, type KeyringPair } from "@moonwall/util";
+import { BN } from "@polkadot/util";
 
 describeSuite({
     id: "DEV_SUB_STAKING_ADD_STAKING_01",
@@ -11,17 +11,26 @@ describeSuite({
         let polkadotJs: ApiPromise;
         let netuid1: number;
 
+        let alice: KeyringPair;
+        let bob: KeyringPair;
+        let appAccount: KeyringPair;
+        const appFees = new BN(100_000);
+
         beforeAll(() => {
             polkadotJs = context.polkadotJs();
+
+            alice = context.keyring.alice;
+            bob = context.keyring.bob;
+            appAccount = generateKeyringPair("sr25519"); // some random app account
+            console.log("appAccount", appAccount.address);
         });
 
         it({
             id: "T01",
-            title: "Add staking payable",
+            title: "Add stake payable",
             test: async () => {
                 const alice = context.keyring.alice;
                 const bob = context.keyring.bob;
-                const appAccount = generateKeyringPair("sr25519"); // some random app account
                 const appFees = new BN(100_000);
 
                 // Register network
@@ -37,12 +46,16 @@ describeSuite({
 
                 // Enabling subtokens
                 const tx1 = polkadotJs.tx.adminUtils.sudoSetSubtokenEnabled(netuid1, true);
-                await context.createBlock([
-                    await polkadotJs.tx.sudo.sudo(tx1).signAsync(alice),
-                ]);
+                await context.createBlock([await polkadotJs.tx.sudo.sudo(tx1).signAsync(alice)]);
 
                 // Adding stake
-                tx = polkadotJs.tx.subtensorModule.addStakePayable(bob.address, netuid1, 1000_000_000, appAccount.address, appFees);
+                tx = polkadotJs.tx.subtensorModule.addStakePayable(
+                    bob.address,
+                    netuid1,
+                    1000_000_000,
+                    appAccount.address,
+                    appFees
+                );
                 await context.createBlock([await tx.signAsync(alice)]);
 
                 events = await polkadotJs.query.system.events();
@@ -57,9 +70,46 @@ describeSuite({
                 expect(stakeAddedEvent.length).to.be.equal(1);
                 expect(feeTransferredEvent.length).to.be.equal(1);
 
-                const appAccountBalance = (await polkadotJs.query.system.account(appAccount.address)).data.free.toString();
+                const appAccountBalance = (
+                    await polkadotJs.query.system.account(appAccount.address)
+                ).data.free.toString();
                 const appAccountBalanceBN = new BN(appAccountBalance);
                 expect(appAccountBalanceBN.eq(appFees)).to.be.true;
+            },
+        });
+
+        it({
+            id: "T02",
+            title: "Remove stake payable",
+            test: async () => {
+                // Removing stake
+                const tx = polkadotJs.tx.subtensorModule.removeStakePayable(
+                    bob.address,
+                    netuid1,
+                    500_000_000,
+                    appAccount.address,
+                    appFees
+                );
+                await context.createBlock([await tx.signAsync(alice)]);
+
+                const events = await polkadotJs.query.system.events();
+                const stakeAddedEvent = events.filter((a) => {
+                    return a.event.method === "StakeRemoved";
+                });
+
+                const feeTransferredEvent = events.filter((a) => {
+                    return a.event.method === "FeesTransferred";
+                });
+
+                expect(stakeAddedEvent.length).to.be.equal(1);
+                expect(feeTransferredEvent.length).to.be.equal(1);
+
+                const appAccountBalance = (
+                    await polkadotJs.query.system.account(appAccount.address)
+                ).data.free.toString();
+                const appAccountBalanceBN = new BN(appAccountBalance);
+                console.log(appAccountBalanceBN.toNumber());
+                expect(appAccountBalanceBN.eq(appFees.add(appFees))).to.be.true; // We expect fees has been paid twice
             },
         });
     },
