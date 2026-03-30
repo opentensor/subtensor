fd10c5b44b11458310b732db7723e1267057a37c
Loris Moulin <contact@lorismoulin.com>
2026-03-30 10:48:53 -0300

    try ci with missing pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1760,7 +1760,6 @@ mod benches {
         [pallet_subtensor_swap, Swap]
         [pallet_shield, MevShield]
         [pallet_subtensor_proxy, Proxy]
-        [pallet_subtensor_utility, Utility]
     );
 }
 
