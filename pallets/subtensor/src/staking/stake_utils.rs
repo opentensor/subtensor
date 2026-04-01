807b5f4871689b01ca43d9291116b76967e9ca35
John Reed <87283488+JohnReedV@users.noreply.github.com>
2026-04-01 12:17:15 -0700

    remove potential panic from get median alpha price

--- pallets/subtensor/src/staking/stake_utils.rs
+++ pallets/subtensor/src/staking/stake_utils.rs
@@ -1,7 +1,7 @@
 use super::*;
 use safe_math::*;
 use share_pool::{SafeFloat, SharePool, SharePoolDataOperations};
-use sp_std::ops::Neg;
+use sp_std::{collections::btree_map::BTreeMap, ops::Neg};
 use substrate_fixed::types::{I64F64, I96F32, U96F32};
 use subtensor_runtime_common::{AlphaBalance, AuthorshipInfo, NetUid, TaoBalance, Token};
 use subtensor_swap_interface::{Order, SwapHandler, SwapResult};
@@ -74,56 +74,68 @@ impl<T: Config> Pallet<T> {
         let zero_price = U96F32::saturating_from_num(0_u64);
         let two = U96F32::saturating_from_num(2_u64);
 
-        let mut prices: Vec<U96F32> = NetworksAdded::<T>::iter()
-            .filter_map(|(netuid, added)| {
-                if added && netuid != NetUid::ROOT {
-                    let price = T::SwapInterface::current_alpha_price(netuid);
-                    if price > zero_price {
-                        Some(price)
-                    } else {
-                        None
-                    }
-                } else {
-                    None
-                }
-            })
-            .collect();
+        let mut price_counts: BTreeMap<U96F32, usize> = BTreeMap::new();
+        let mut total_prices: usize = 0;
 
-        if prices.is_empty() {
+        for (netuid, added) in NetworksAdded::<T>::iter() {
+            if !added || netuid == NetUid::ROOT {
+                continue;
+            }
+
+            let price = T::SwapInterface::current_alpha_price(netuid);
+            if price <= zero_price {
+                continue;
+            }
+
+            total_prices = total_prices.saturating_add(1);
+
+            if let Some(count) = price_counts.get_mut(&price) {
+                *count = count.saturating_add(1);
+            } else {
+                price_counts.insert(price, 1usize);
+            }
+        }
+
+        if total_prices == 0 {
             return default_price;
         }
 
-        prices.sort_unstable();
-
-        let len = prices.len();
-
-        let Some(mid_index) = len.checked_div(2) else {
+        let Some(last_index) = total_prices.checked_sub(1) else {
+            return default_price;
+        };
+        let Some(lower_target) = last_index.checked_div(2) else {
+            return default_price;
+        };
+        let Some(upper_target) = total_prices.checked_div(2) else {
             return default_price;
         };
 
-        let Some(remainder) = len.checked_rem(2) else {
-            return default_price;
-        };
+        let mut cumulative: usize = 0;
+        let mut lower_price: Option<U96F32> = None;
+        let mut upper_price: Option<U96F32> = None;
 
-        if remainder == 0 {
-            let Some(left_index) = mid_index.checked_sub(1) else {
-                return default_price;
-            };
+        for (price, count) in price_counts.into_iter() {
+            let next_cumulative = cumulative.saturating_add(count);
 
-            match (
-                prices.get(left_index).copied(),
-                prices.get(mid_index).copied(),
-            ) {
-                (Some(left_price), Some(right_price)) => {
-                    left_price.saturating_add(right_price).safe_div(two)
-                }
-                _ => default_price,
+            if lower_price.is_none() && lower_target < next_cumulative {
+                lower_price = Some(price);
             }
-        } else {
-            match prices.get(mid_index).copied() {
-                Some(price) => price,
-                None => default_price,
+
+            if upper_price.is_none() && upper_target < next_cumulative {
+                upper_price = Some(price);
             }
+
+            if lower_price.is_some() && upper_price.is_some() {
+                break;
+            }
+
+            cumulative = next_cumulative;
+        }
+
+        match (lower_price, upper_price) {
+            (Some(_), Some(upper)) if lower_target == upper_target => upper,
+            (Some(lower), Some(upper)) => lower.saturating_add(upper).safe_div(two),
+            _ => default_price,
         }
     }
 
