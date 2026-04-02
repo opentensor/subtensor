f3f2c26cd687f15915886a75cbf32697d41a6ed9
Evgeny Svirsky <e.svirsky@tut.by>
2026-04-02 13:31:01 +0200

    - Added all the affected keys

--- pallets/subtensor/src/migrations/migrate_fix_root_claimed_overclaim.rs
+++ pallets/subtensor/src/migrations/migrate_fix_root_claimed_overclaim.rs
@@ -12,6 +12,12 @@ pub fn decode_account_id32<T: Config>(ss58_string: &str) -> Option<T::AccountId>
     T::AccountId::decode(&mut account_id32_slice).ok()
 }
 
+struct HotkeySwapFix {
+    old_hotkey_ss58: &'static str,
+    new_hotkey_ss58: &'static str,
+    netuid: u16,
+}
+
 /// Fixes the consequences of a bug in `perform_hotkey_swap_on_one_subnet` where
 /// `transfer_root_claimable_for_new_hotkey` unconditionally transferred the **entire**
 /// `RootClaimable` BTreeMap (all subnets) from the old hotkey to the new hotkey, even
@@ -43,33 +49,110 @@ pub fn migrate_fix_root_claimed_overclaim<T: Config>() -> Weight {
     );
 
     // Only run on mainnet.
-    // Mainnet genesis: 0x2f0555cc76fc2840a25a6ea3b9637146806f1f44b090c175ffde2a7e5ab36c03
+    // Mainnet genesis: 0xa1fba751d7e4059a3d298aac93054692c8ef12a04e82b66df4fe021d656a7336
     let genesis_hash = frame_system::Pallet::<T>::block_hash(BlockNumberFor::<T>::zero());
     let genesis_bytes = genesis_hash.as_ref();
+    let mut claimed_restored: u64 = 0;
     let mainnet_genesis =
-        hex_literal::hex!("2f0555cc76fc2840a25a6ea3b9637146806f1f44b090c175ffde2a7e5ab36c03");
+        hex_literal::hex!("a1fba751d7e4059a3d298aac93054692c8ef12a04e82b66df4fe021d656a7336");
     if genesis_bytes == mainnet_genesis {
-        let old_hotkey_ss58 = "5GmvyePN9aYErXBBhBnxZKGoGk4LKZApE4NkaSzW62CYCYNA";
-        let new_hotkey_ss58 = "5H6BqkzjYvViiqp7rQLXjpnaEmW7U9CoKxXhQ4efMqtX1mQw";
-        let netuid = NetUid::from(27);
+        let fixes: &[HotkeySwapFix] = &[HotkeySwapFix {
+            old_hotkey_ss58: "5GmvyePN9aYErXBBhBnxZKGoGk4LKZApE4NkaSzW62CYCYNA",
+            new_hotkey_ss58: "5H6BqkzjYvViiqp7rQLXjpnaEmW7U9CoKxXhQ4efMqtX1mQw",
+            netuid: 27,
+        }, HotkeySwapFix {
+            old_hotkey_ss58: "5CmKE9k1z1DDQBh81nfwRtbLq22mgS8wMPS9h36LVe4oGJTK",
+            new_hotkey_ss58: "5EnpBz2DoMTzMztFSVPSpi8jP2yfGadU6kgZgsjqnfvonMgu",
+            netuid: 9,
+        }, HotkeySwapFix {
+            old_hotkey_ss58: "5C4s95N2JJbWwPPAr8JYwQBZQwxbZTYGjYbm6XtH2LgYV8Zx",
+            new_hotkey_ss58: "5ChzWkapDYgVxT88ZmBQS8QM63V9VWSA3eFpSipsX2xbTNZN",
+            netuid: 13,
+        }, HotkeySwapFix {
+            old_hotkey_ss58: "5GHrTeuFnJYjNJx773URbYb9Pk3bRRDiJHJFBNECZpjGqZPY",
+            new_hotkey_ss58: "5DAmVrUgpTX9xmRyZ7R3UUFNSzh7ZNY6qYxv9N4VeCq6mHHL",
+            netuid: 65,
+        }, HotkeySwapFix {
+            old_hotkey_ss58: "5EtM9iXMAYRsmt6aoQAoWNDX6yaBnjhmnEQhWKv8HpwkVtML",
+            new_hotkey_ss58: "5ECzcM7sixWNEeD6RbpeEHW1YcYMFejwHuvDBgQxVSjGyrMS",
+            netuid: 11,
+        }, HotkeySwapFix {
+            old_hotkey_ss58: "5HK5tp6t2S59DywmHRWPBVJeJ86T61KjurYqeooqj8sREpeN",
+            new_hotkey_ss58: "5DF3nhgzpr4EZas8dXZYa4mYZBxRCU7AuiCV7Qs2JWAGA6sY",
+            netuid: 41,
+        }, HotkeySwapFix {
+            old_hotkey_ss58: "5HK5tp6t2S59DywmHRWPBVJeJ86T61KjurYqeooqj8sREpeN",
+            new_hotkey_ss58: "5E4pFBKCyk2RxQqifEBu37jb5vgoj9ZrVS7iQdQy4PNr33Ge",
+            netuid: 44,
+        }, HotkeySwapFix {
+            old_hotkey_ss58: "5HK5tp6t2S59DywmHRWPBVJeJ86T61KjurYqeooqj8sREpeN",
+            new_hotkey_ss58: "5DhQbRT3ZfHcVumNtAm5BbzeGHrFRHHi7nofgu76VWipnGSb",
+            netuid: 50,
+        }, HotkeySwapFix {
+            old_hotkey_ss58: "5HK5tp6t2S59DywmHRWPBVJeJ86T61KjurYqeooqj8sREpeN",
+            new_hotkey_ss58: "5Gj37iVQG5hMSxU3AE89x5p3aEEfPZk6Rtmtbwepght4tbri",
+            netuid: 51,
+        }, HotkeySwapFix {
+            old_hotkey_ss58: "5HK5tp6t2S59DywmHRWPBVJeJ86T61KjurYqeooqj8sREpeN",
+            new_hotkey_ss58: "5DyM1rxnDu8QSjbbh5bPV2GMK6UTPRXdUM6mNViBBut9Ma6w",
+            netuid: 54,
+        }, HotkeySwapFix {
+            old_hotkey_ss58: "5HK5tp6t2S59DywmHRWPBVJeJ86T61KjurYqeooqj8sREpeN",
+            new_hotkey_ss58: "5Ci5t4vPK3eCGhFWneB58fodg3x9oS2m8seKoDApFKUqyw4e",
+            netuid: 64,
+        }, HotkeySwapFix {
+            old_hotkey_ss58: "5HK5tp6t2S59DywmHRWPBVJeJ86T61KjurYqeooqj8sREpeN",
+            new_hotkey_ss58: "5Et5VQUMX7VqGyvZycjv5FBBC5FQbLGUJiRMWMnEVnMLXKm9",
+            netuid: 93,
+        }];
 
-        if let (Some(old_hotkey), Some(new_hotkey)) = (
-            decode_account_id32::<T>(old_hotkey_ss58),
-            decode_account_id32::<T>(new_hotkey_ss58),
-        ) {
+        let root_netuid = NetUid::from(0);
+
+        for fix in fixes {
+            let netuid = NetUid::from(fix.netuid);
+
+            let (old_hotkey, new_hotkey) = match (
+                decode_account_id32::<T>(fix.old_hotkey_ss58),
+                decode_account_id32::<T>(fix.new_hotkey_ss58),
+            ) {
+                (Some(old), Some(new)) => (old, new),
+                _ => {
+                    log::error!(
+                        "Failed to decode hotkeys for netuid {}, skipping",
+                        fix.netuid
+                    );
+                    continue;
+                }
+            };
+
             // Reverting the Root Claimable because it only should happen for root subnet
             Pallet::<T>::transfer_root_claimable_for_new_hotkey(&new_hotkey, &old_hotkey);
             weight.saturating_accrue(T::DbWeight::get().reads_writes(2, 2));
 
-            let alpha_values: Vec<((T::AccountId, NetUid), U64F64)> =
-                Alpha::<T>::iter_prefix((&new_hotkey,)).collect();
-            weight.saturating_accrue(T::DbWeight::get().reads(alpha_values.len() as u64));
+            // Collect all coldkeys that have non-zero alpha on root subnet
+            // (meaning they had root stake at swap time)
+            let alpha_on_swapped_subnet: Vec<T::AccountId> =
+                Alpha::<T>::iter_prefix((&new_hotkey,))
+                    .filter(|((coldkey, netuid_alpha), _)| {
+                        // Must be on the subnet that was swapped
+                        if *netuid_alpha != netuid {
+                            return false;
+                        }
+                        // Must have non-zero alpha on root subnet for old hotkey
+                        // (guards against reverting claims for keys with no root stake)
+                        let root_alpha = Alpha::<T>::get((&old_hotkey, coldkey, root_netuid));
+                        root_alpha != U64F64::from_num(0u64)
+                    })
+                    .map(|((coldkey, _), _)| coldkey)
+                    .collect();
 
-            // Reverting back root claimed
-            for ((coldkey, _), _) in alpha_values
-                .into_iter()
-                .filter(|((_, netuid_alpha), alpha)| *netuid_alpha == netuid && *alpha != 0)
-            {
+            weight.saturating_accrue(
+                T::DbWeight::get().reads(alpha_on_swapped_subnet.len() as u64 * 2),
+            );
+
+            // Revert RootClaimed for each qualifying coldkey
+            for coldkey in alpha_on_swapped_subnet {
+                claimed_restored = claimed_restored.saturating_add(1);
                 Pallet::<T>::transfer_root_claimed_for_new_keys(
                     netuid,
                     &new_hotkey,
@@ -79,8 +162,6 @@ pub fn migrate_fix_root_claimed_overclaim<T: Config>() -> Weight {
                 );
                 weight.saturating_accrue(T::DbWeight::get().reads_writes(2, 2));
             }
-        } else {
-            log::error!("Failed to decode hotkeys, skipping");
         }
     }
 
@@ -88,7 +169,7 @@ pub fn migrate_fix_root_claimed_overclaim<T: Config>() -> Weight {
     HasMigrationRun::<T>::insert(&migration_name, true);
     weight.saturating_accrue(T::DbWeight::get().writes(1));
 
-    log::info!("Migration 'migrate_fix_root_claimed_overclaim' completed.");
+    log::info!("Migration 'migrate_fix_root_claimed_overclaim' completed. Claimed restored: {claimed_restored}");
 
     weight
 }
