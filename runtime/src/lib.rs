6bc8883ee6166593ea0564729953845f15cff335
Loris Moulin <contact@lorismoulin.com>
2026-04-03 13:24:27 -0300

    added back utility pallet to benchmarks

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1761,6 +1761,7 @@ mod benches {
         [pallet_subtensor_swap, Swap]
         [pallet_shield, MevShield]
         [pallet_subtensor_proxy, Proxy]
+        [pallet_subtensor_utility, Utility]
     );
 }
 
