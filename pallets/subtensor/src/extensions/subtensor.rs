51e4a93d04f6eab8eb36b10870e739dd64fc192d
open-junius <zhou@opentensor.dev>
2026-04-08 12:20:52 +0800

    more unit tests

--- pallets/subtensor/src/extensions/subtensor.rs
+++ pallets/subtensor/src/extensions/subtensor.rs
@@ -206,8 +206,7 @@ where
             }
             Some(Call::reveal_mechanism_weights {
                 netuid,
-                #[warn(unused_variables)]
-                    mecid: _,
+                mecid: _,
                 uids,
                 values,
                 salt,
@@ -362,7 +361,7 @@ where
 
                 let block: u64 = Pallet::<T>::get_current_block_as_u64();
 
-                if !Pallet::<T>::exceeds_tx_rate_limit(Pallet::<T>::get_last_tx_block(who), block) {
+                if Pallet::<T>::exceeds_tx_rate_limit(Pallet::<T>::get_last_tx_block(who), block) {
                     return Err(CustomTransactionError::RateLimitExceeded.into());
                 }
 
