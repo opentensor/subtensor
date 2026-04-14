bce46993315ee801cbab9bfe6aa5a4d9e6d049e5
Greg Zaitsev <gregz@opentensor.dev>
2026-04-14 19:53:03 -0400

    Fix try-runtime

--- pallets/subtensor/src/utils/try_state.rs
+++ pallets/subtensor/src/utils/try_state.rs
@@ -10,22 +10,50 @@ impl<T: Config> Pallet<T> {
         // Get the total currency issuance
         let currency_issuance = <T as Config>::Currency::total_issuance();
 
+        // Calculate the expected total issuance
+        let expected_total_issuance =
+            currency_issuance.saturating_add(TotalStake::<T>::get().into());
+        let expected_fixed_total_issuance = currency_issuance;
+
         // Verify the diff between calculated TI and actual TI is less than delta
         //
         // These values can be off slightly due to float rounding errors.
         // They are corrected every runtime upgrade.
-        let total_issuance = Self::get_total_issuance();
+        let delta = TaoBalance::from(1000);
+        let total_issuance = TotalIssuance::<T>::get();
 
-        if currency_issuance != total_issuance {
+        let diff = if total_issuance > expected_total_issuance {
+            total_issuance.checked_sub(&expected_total_issuance)
+        } else {
+            expected_total_issuance.checked_sub(&total_issuance)
+        }
+        .expect("LHS > RHS");
+
+        let diff_fixed = if total_issuance > expected_fixed_total_issuance {
+            total_issuance.checked_sub(&expected_fixed_total_issuance)
+        } else {
+            expected_fixed_total_issuance.checked_sub(&total_issuance)
+        }
+        .expect("LHS > RHS");
+
+        if diff > delta {
             log::error!(
-                "currency_issuance: {} != total_issuance: {}",
-                currency_issuance,
+                "expected_total_issuance: {} != total_issuance: {}",
+                expected_total_issuance,
+                total_issuance
+            );
+        }
+
+        if diff_fixed > delta {
+            log::error!(
+                "expected_fixed_total_issuance: {} != total_issuance: {}",
+                expected_fixed_total_issuance,
                 total_issuance
             );
         }
 
         ensure!(
-            currency_issuance == total_issuance,
+            (diff <= delta) || (diff_fixed <= delta),
             "TotalIssuance diff greater than allowable delta",
         );
 
