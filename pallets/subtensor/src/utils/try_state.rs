4171384ae5837c2025c2d0c90419b9a833ce3ead
Greg Zaitsev <gregz@opentensor.dev>
2026-04-14 17:19:36 -0400

    Add error logging to try-runtime

--- pallets/subtensor/src/utils/try_state.rs
+++ pallets/subtensor/src/utils/try_state.rs
@@ -10,25 +10,22 @@ impl<T: Config> Pallet<T> {
         // Get the total currency issuance
         let currency_issuance = <T as Config>::Currency::total_issuance();
 
-        // Calculate the expected total issuance
-        let expected_total_issuance = currency_issuance;
-
         // Verify the diff between calculated TI and actual TI is less than delta
         //
         // These values can be off slightly due to float rounding errors.
         // They are corrected every runtime upgrade.
-        let delta = TaoBalance::ZERO;
         let total_issuance = Self::get_total_issuance();
 
-        let diff = if total_issuance > expected_total_issuance {
-            total_issuance.checked_sub(&expected_total_issuance)
-        } else {
-            expected_total_issuance.checked_sub(&total_issuance)
+        if currency_issuance != total_issuance {
+            log::error!(
+                "currency_issuance: {} != total_issuance: {}",
+                currency_issuance,
+                total_issuance
+            );
         }
-        .expect("LHS > RHS");
 
         ensure!(
-            diff == delta,
+            currency_issuance == total_issuance,
             "TotalIssuance diff greater than allowable delta",
         );
 
