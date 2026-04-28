d33bcf1d43425dfe6691cd0127fba7866036fa27
Aliaksandr Tsurko <ales@opentensor.dev>
2026-04-28 19:57:40 +0200

    Port crowdloan precompile tests to rust

--- precompiles/src/crowdloan.rs
+++ precompiles/src/crowdloan.rs
@@ -258,3 +258,418 @@ struct CrowdloanInfo {
     finalized: bool,
     contributors_count: u32,
 }
+
+#[cfg(test)]
+mod tests {
+    #![allow(clippy::expect_used, clippy::arithmetic_side_effects)]
+
+    use super::*;
+    use crate::PrecompileExt;
+    use crate::mock::{
+        AccountId, Runtime, RuntimeOrigin, System, addr_from_index, new_test_ext, precompiles,
+        selector_u32,
+    };
+    use pallet_evm::AddressMapping;
+    use precompile_utils::solidity::{codec::Address, encode_return_value, encode_with_selector};
+    use precompile_utils::testing::PrecompileTesterExt;
+    use sp_core::H160;
+
+    const CREATOR_DEPOSIT: u64 = 50;
+    const MIN_CONTRIBUTION: u64 = 10;
+    const CAP: u64 = 300;
+    const END: u32 = 50;
+    const ACCOUNT_BALANCE: u64 = 1_000;
+
+    fn mapped_account(address: H160) -> AccountId {
+        <Runtime as pallet_evm::Config>::AddressMapping::into_account_id(address)
+    }
+
+    fn fund_account(account: &AccountId, amount: u64) {
+        pallet_subtensor::Pallet::<Runtime>::add_balance_to_coldkey_account(account, amount.into());
+    }
+
+    fn get_crowdloan(caller: H160, crowdloan_id: u32, expected: CrowdloanInfo) {
+        let precompile_addr = addr_from_index(CrowdloanPrecompile::<Runtime>::INDEX);
+
+        precompiles::<CrowdloanPrecompile<Runtime>>()
+            .prepare_test(
+                caller,
+                precompile_addr,
+                encode_with_selector(selector_u32("getCrowdloan(uint32)"), (crowdloan_id,)),
+            )
+            .with_static_call(true)
+            .execute_returns_raw(encode_return_value(expected));
+    }
+
+    fn expected_crowdloan_info(crowdloan_id: u32) -> CrowdloanInfo {
+        let crowdloan = pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id)
+            .expect("crowdloan should exist");
+
+        CrowdloanInfo {
+            creator: H256::from_slice(crowdloan.creator.as_slice()),
+            deposit: u64::from(crowdloan.deposit),
+            min_contribution: u64::from(crowdloan.min_contribution),
+            end: crowdloan.end as u32,
+            cap: u64::from(crowdloan.cap),
+            funds_account: H256::from_slice(crowdloan.funds_account.as_slice()),
+            raised: u64::from(crowdloan.raised),
+            has_target_address: crowdloan.target_address.is_some(),
+            target_address: crowdloan
+                .target_address
+                .map(|account| H256::from_slice(account.as_slice()))
+                .unwrap_or_else(H256::zero),
+            finalized: crowdloan.finalized,
+            contributors_count: crowdloan.contributors_count,
+        }
+    }
+
+    fn create_crowdloan(caller: H160, target: H160) -> u32 {
+        let crowdloan_id = pallet_crowdloan::NextCrowdloanId::<Runtime>::get();
+        let precompile_addr = addr_from_index(CrowdloanPrecompile::<Runtime>::INDEX);
+
+        precompiles::<CrowdloanPrecompile<Runtime>>()
+            .prepare_test(
+                caller,
+                precompile_addr,
+                encode_with_selector(
+                    selector_u32("create(uint64,uint64,uint64,uint32,address)"),
+                    (CREATOR_DEPOSIT, MIN_CONTRIBUTION, CAP, END, Address(target)),
+                ),
+            )
+            .execute_returns(());
+        crowdloan_id
+    }
+
+    fn contribute(caller: H160, crowdloan_id: u32, amount: u64) {
+        let precompile_addr = addr_from_index(CrowdloanPrecompile::<Runtime>::INDEX);
+
+        precompiles::<CrowdloanPrecompile<Runtime>>()
+            .prepare_test(
+                caller,
+                precompile_addr,
+                encode_with_selector(
+                    selector_u32("contribute(uint32,uint64)"),
+                    (crowdloan_id, amount),
+                ),
+            )
+            .execute_returns(());
+    }
+
+    fn withdraw(caller: H160, crowdloan_id: u32) {
+        let precompile_addr = addr_from_index(CrowdloanPrecompile::<Runtime>::INDEX);
+
+        precompiles::<CrowdloanPrecompile<Runtime>>()
+            .prepare_test(
+                caller,
+                precompile_addr,
+                encode_with_selector(selector_u32("withdraw(uint32)"), (crowdloan_id,)),
+            )
+            .execute_returns(());
+    }
+
+    fn finalize(caller: H160, crowdloan_id: u32) {
+        let precompile_addr = addr_from_index(CrowdloanPrecompile::<Runtime>::INDEX);
+
+        precompiles::<CrowdloanPrecompile<Runtime>>()
+            .prepare_test(
+                caller,
+                precompile_addr,
+                encode_with_selector(selector_u32("finalize(uint32)"), (crowdloan_id,)),
+            )
+            .execute_returns(());
+    }
+
+    #[test]
+    fn crowdloan_precompile_reads_existing_pallet_crowdloan() {
+        new_test_ext().execute_with(|| {
+            let creator = AccountId::from([0x11; 32]);
+            let caller = addr_from_index(0x7001);
+            let crowdloan_id = pallet_crowdloan::NextCrowdloanId::<Runtime>::get();
+
+            fund_account(&creator, ACCOUNT_BALANCE);
+            pallet_crowdloan::Pallet::<Runtime>::create(
+                RuntimeOrigin::signed(creator),
+                CREATOR_DEPOSIT.into(),
+                MIN_CONTRIBUTION.into(),
+                CAP.into(),
+                END.into(),
+                None,
+                None,
+            )
+            .expect("direct crowdloan create should work");
+
+            get_crowdloan(caller, crowdloan_id, expected_crowdloan_info(crowdloan_id));
+        });
+    }
+
+    #[test]
+    fn crowdloan_precompile_creates_and_reads_crowdloan() {
+        new_test_ext().execute_with(|| {
+            let creator = addr_from_index(0x7002);
+            let target = addr_from_index(0x7003);
+            let creator_account = mapped_account(creator);
+            let target_account = mapped_account(target);
+
+            fund_account(&creator_account, ACCOUNT_BALANCE);
+
+            let crowdloan_id = create_crowdloan(creator, target);
+            let crowdloan = pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id)
+                .expect("crowdloan should exist");
+
+            assert_eq!(crowdloan.creator, creator_account);
+            assert_eq!(u64::from(crowdloan.deposit), CREATOR_DEPOSIT);
+            assert_eq!(u64::from(crowdloan.min_contribution), MIN_CONTRIBUTION);
+            assert_eq!(u64::from(crowdloan.cap), CAP);
+            assert_eq!(crowdloan.end, END as u64);
+            assert_eq!(u64::from(crowdloan.raised), CREATOR_DEPOSIT);
+            assert_eq!(crowdloan.target_address, Some(target_account));
+            assert!(!crowdloan.finalized);
+            assert_eq!(crowdloan.contributors_count, 1);
+            get_crowdloan(creator, crowdloan_id, expected_crowdloan_info(crowdloan_id));
+        });
+    }
+
+    #[test]
+    fn crowdloan_precompile_contributes_and_withdraws() {
+        new_test_ext().execute_with(|| {
+            let creator = addr_from_index(0x7004);
+            let contributor = addr_from_index(0x7005);
+            let target = addr_from_index(0x7006);
+            let creator_account = mapped_account(creator);
+            let contributor_account = mapped_account(contributor);
+            let contribution = 30_u64;
+
+            fund_account(&creator_account, ACCOUNT_BALANCE);
+            fund_account(&contributor_account, ACCOUNT_BALANCE);
+
+            let crowdloan_id = create_crowdloan(creator, target);
+            contribute(contributor, crowdloan_id, contribution);
+
+            let crowdloan = pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id)
+                .expect("crowdloan should exist");
+            assert_eq!(u64::from(crowdloan.raised), CREATOR_DEPOSIT + contribution);
+            assert_eq!(crowdloan.contributors_count, 2);
+            assert_eq!(
+                pallet_crowdloan::Contributions::<Runtime>::get(
+                    crowdloan_id,
+                    &contributor_account,
+                ),
+                Some(contribution.into()),
+            );
+            get_crowdloan(creator, crowdloan_id, expected_crowdloan_info(crowdloan_id));
+
+            withdraw(contributor, crowdloan_id);
+
+            let crowdloan = pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id)
+                .expect("crowdloan should exist");
+            assert_eq!(u64::from(crowdloan.raised), CREATOR_DEPOSIT);
+            assert_eq!(crowdloan.contributors_count, 1);
+            assert_eq!(
+                pallet_crowdloan::Contributions::<Runtime>::get(
+                    crowdloan_id,
+                    &contributor_account,
+                ),
+                None,
+            );
+            get_crowdloan(creator, crowdloan_id, expected_crowdloan_info(crowdloan_id));
+        });
+    }
+
+    #[test]
+    fn crowdloan_precompile_contributes_and_withdraws_from_pallet_crowdloan() {
+        new_test_ext().execute_with(|| {
+            let creator = AccountId::from([0x22; 32]);
+            let contributor = addr_from_index(0x7016);
+            let contributor_account = mapped_account(contributor);
+            let crowdloan_id = pallet_crowdloan::NextCrowdloanId::<Runtime>::get();
+            let contribution = 30_u64;
+
+            fund_account(&creator, ACCOUNT_BALANCE);
+            fund_account(&contributor_account, ACCOUNT_BALANCE);
+            pallet_crowdloan::Pallet::<Runtime>::create(
+                RuntimeOrigin::signed(creator),
+                CREATOR_DEPOSIT.into(),
+                MIN_CONTRIBUTION.into(),
+                CAP.into(),
+                END.into(),
+                None,
+                None,
+            )
+            .expect("direct crowdloan create should work");
+
+            contribute(contributor, crowdloan_id, contribution);
+
+            let crowdloan = pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id)
+                .expect("crowdloan should exist");
+            assert_eq!(u64::from(crowdloan.raised), CREATOR_DEPOSIT + contribution);
+            assert_eq!(crowdloan.contributors_count, 2);
+            get_crowdloan(contributor, crowdloan_id, expected_crowdloan_info(crowdloan_id));
+
+            withdraw(contributor, crowdloan_id);
+
+            let crowdloan = pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id)
+                .expect("crowdloan should exist");
+            assert_eq!(u64::from(crowdloan.raised), CREATOR_DEPOSIT);
+            assert_eq!(crowdloan.contributors_count, 1);
+            assert_eq!(
+                pallet_crowdloan::Contributions::<Runtime>::get(
+                    crowdloan_id,
+                    &contributor_account,
+                ),
+                None,
+            );
+            get_crowdloan(contributor, crowdloan_id, expected_crowdloan_info(crowdloan_id));
+        });
+    }
+
+    #[test]
+    fn crowdloan_precompile_finalizes_capped_crowdloan() {
+        new_test_ext().execute_with(|| {
+            let creator = addr_from_index(0x7007);
+            let contributor = addr_from_index(0x7008);
+            let target = addr_from_index(0x7009);
+            let creator_account = mapped_account(creator);
+            let contributor_account = mapped_account(contributor);
+            let target_account = mapped_account(target);
+
+            fund_account(&creator_account, ACCOUNT_BALANCE);
+            fund_account(&contributor_account, ACCOUNT_BALANCE);
+
+            let crowdloan_id = create_crowdloan(creator, target);
+            contribute(contributor, crowdloan_id, CAP - CREATOR_DEPOSIT);
+            System::set_block_number(END.into());
+            finalize(creator, crowdloan_id);
+
+            let crowdloan = pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id)
+                .expect("crowdloan should exist");
+            assert!(crowdloan.finalized);
+            assert_eq!(
+                pallet_balances::Pallet::<Runtime>::free_balance(&target_account),
+                CAP.into(),
+            );
+            get_crowdloan(creator, crowdloan_id, expected_crowdloan_info(crowdloan_id));
+        });
+    }
+
+    #[test]
+    fn crowdloan_precompile_refunds_and_dissolves_crowdloan() {
+        new_test_ext().execute_with(|| {
+            let creator = addr_from_index(0x7010);
+            let first = addr_from_index(0x7011);
+            let second = addr_from_index(0x7012);
+            let target = addr_from_index(0x7013);
+            let creator_account = mapped_account(creator);
+            let first_account = mapped_account(first);
+            let second_account = mapped_account(second);
+            let contribution = 30_u64;
+
+            fund_account(&creator_account, ACCOUNT_BALANCE);
+            fund_account(&first_account, ACCOUNT_BALANCE);
+            fund_account(&second_account, ACCOUNT_BALANCE);
+
+            let crowdloan_id = create_crowdloan(creator, target);
+            contribute(first, crowdloan_id, contribution);
+            contribute(second, crowdloan_id, contribution);
+            System::set_block_number(END.into());
+            let precompile_addr = addr_from_index(CrowdloanPrecompile::<Runtime>::INDEX);
+
+            precompiles::<CrowdloanPrecompile<Runtime>>()
+                .prepare_test(
+                    creator,
+                    precompile_addr,
+                    encode_with_selector(selector_u32("refund(uint32)"), (crowdloan_id,)),
+                )
+                .execute_returns(());
+
+            let crowdloan = pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id)
+                .expect("crowdloan should exist");
+            assert_eq!(u64::from(crowdloan.raised), CREATOR_DEPOSIT);
+            assert_eq!(crowdloan.contributors_count, 1);
+            get_crowdloan(creator, crowdloan_id, expected_crowdloan_info(crowdloan_id));
+
+            precompiles::<CrowdloanPrecompile<Runtime>>()
+                .prepare_test(
+                    creator,
+                    precompile_addr,
+                    encode_with_selector(selector_u32("dissolve(uint32)"), (crowdloan_id,)),
+                )
+                .execute_returns(());
+
+            assert!(pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id).is_none());
+        });
+    }
+
+    #[test]
+    fn crowdloan_precompile_updates_crowdloan_terms() {
+        new_test_ext().execute_with(|| {
+            let creator = addr_from_index(0x7014);
+            let target = addr_from_index(0x7015);
+            let creator_account = mapped_account(creator);
+            let new_min_contribution = 20_u64;
+            let new_end = 80_u32;
+            let new_cap = 400_u64;
+
+            fund_account(&creator_account, ACCOUNT_BALANCE);
+
+            let crowdloan_id = create_crowdloan(creator, target);
+            let precompiles = precompiles::<CrowdloanPrecompile<Runtime>>();
+            let precompile_addr = addr_from_index(CrowdloanPrecompile::<Runtime>::INDEX);
+
+            precompiles
+                .prepare_test(
+                    creator,
+                    precompile_addr,
+                    encode_with_selector(
+                        selector_u32("updateMinContribution(uint32,uint64)"),
+                        (crowdloan_id, new_min_contribution),
+                    ),
+                )
+                .execute_returns(());
+            assert_eq!(
+                u64::from(
+                    pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id)
+                        .expect("crowdloan should exist")
+                        .min_contribution,
+                ),
+                new_min_contribution,
+            );
+
+            precompiles
+                .prepare_test(
+                    creator,
+                    precompile_addr,
+                    encode_with_selector(
+                        selector_u32("updateEnd(uint32,uint32)"),
+                        (crowdloan_id, new_end),
+                    ),
+                )
+                .execute_returns(());
+            assert_eq!(
+                pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id)
+                    .expect("crowdloan should exist")
+                    .end,
+                new_end as u64,
+            );
+
+            precompiles
+                .prepare_test(
+                    creator,
+                    precompile_addr,
+                    encode_with_selector(
+                        selector_u32("updateCap(uint32,uint64)"),
+                        (crowdloan_id, new_cap),
+                    ),
+                )
+                .execute_returns(());
+            assert_eq!(
+                u64::from(
+                    pallet_crowdloan::Crowdloans::<Runtime>::get(crowdloan_id)
+                        .expect("crowdloan should exist")
+                        .cap,
+                ),
+                new_cap,
+            );
+            get_crowdloan(creator, crowdloan_id, expected_crowdloan_info(crowdloan_id));
+        });
+    }
+}
