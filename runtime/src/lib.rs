4bd5a0db07466c2099fc206f16f8fffa17116c95
Loris Moulin <contact@lorismoulin.com>
2026-05-10 23:57:25 -0300

    Restore MaxScheduledPerBlock

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -884,7 +884,7 @@ impl CommitmentsInterface for CommitmentsI {
 parameter_types! {
     pub MaximumSchedulerWeight: Weight = Perbill::from_percent(80) *
         BlockWeights::get().max_block;
-    pub const MaxScheduledPerBlock: u32 = 70;
+    pub const MaxScheduledPerBlock: u32 = 50;
 }
 
 /// Used the compare the privilege of an origin inside the scheduler.
