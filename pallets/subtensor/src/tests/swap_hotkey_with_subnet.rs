70bd1b5298c06bc900fde6f2cf959b9c936a4e03
Greg Zaitsev <gregz@opentensor.dev>
2026-06-11 14:13:12 -0400

    merge main

--- pallets/subtensor/src/tests/swap_hotkey_with_subnet.rs
+++ pallets/subtensor/src/tests/swap_hotkey_with_subnet.rs
@@ -907,14 +907,13 @@ fn test_swap_owner_new_hotkey_already_exists() {
         let old_hotkey = U256::from(1);
         let new_hotkey = U256::from(2);
         let coldkey = U256::from(3);
-        let another_coldkey = U256::from(4);
 
         let netuid = add_dynamic_network(&new_hotkey, &coldkey);
         add_balance_to_coldkey_account(&coldkey, 1_000_000_000_000_u64.into());
 
         // Initialize Owner for old_hotkey and new_hotkey
         Owner::<Test>::insert(old_hotkey, coldkey);
-        Owner::<Test>::insert(new_hotkey, another_coldkey);
+        Owner::<Test>::insert(new_hotkey, coldkey);
 
         // Perform the swap
         System::set_block_number(System::block_number() + HotkeySwapOnSubnetInterval::get());
