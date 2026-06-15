b42d8d26249017778e79248a685a04607e45d857
open-junius <zhou@opentensor.dev>
2026-06-15 17:41:14 +0800

    update reads

--- precompiles/src/alpha.rs
+++ precompiles/src/alpha.rs
@@ -117,10 +117,9 @@ where
         tao: u64,
     ) -> EvmResult<U256> {
         // SubnetMechanism + swap simulation reads
-        handle.record_db_reads::<R>(1)?;
-        let order = pallet_subtensor::GetAlphaForTao::<R>::with_amount(tao);
+        handle.record_db_reads::<R>(9)?;
 
-        handle.record_db_reads::<R>(8)?;
+        let order = pallet_subtensor::GetAlphaForTao::<R>::with_amount(tao);
         let swap_result =
             <pallet_subtensor_swap::Pallet<R> as SwapHandler>::sim_swap(netuid.into(), order)
                 .map_err(|e| PrecompileFailure::Error {
@@ -137,7 +136,7 @@ where
         alpha: u64,
     ) -> EvmResult<U256> {
         // SubnetMechanism + swap simulation reads
-        handle.record_db_reads::<R>(2)?;
+        handle.record_db_reads::<R>(9)?;
 
         let order = pallet_subtensor::GetTaoForAlpha::<R>::with_amount(alpha);
         let swap_result =
